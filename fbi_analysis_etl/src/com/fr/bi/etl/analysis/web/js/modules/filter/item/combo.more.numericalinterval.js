9125297d098d39437fa0db835954e2777ca2c867
ponyxiaohuihui <zihaihuizhou@hotmail.com>
2016-05-03 09:51:20 +0800

    抄个大于等于的下拉框，汇总再汇总用

---
+++ fbi_analysis_etl/src/com/fr/bi/etl/analysis/web/js/modules/filter/item/combo.more.numericalinterval.js
@@ -0,0 +1,108 @@
+BI.NumericalIntervalMoreCombo = BI.inherit(BI.Widget, {
+    constants: {
+        expandClass: "combo-expand",
+        tgap: 2
+    },
+    _defaultConfig: function () {
+        var conf = BI.NumericalIntervalMoreCombo.superclass._defaultConfig.apply(this, arguments);
+        return BI.extend(conf, {
+            baseCls: "bi-numerical-interval-combo",
+            items: [{
+                text: ">(" + BI.i18nText("BI-More_Than") + ")",
+                value: 0
+            }, {
+                text: "≥(" + BI.i18nText("More_Than_And_Equal") + ")",
+                value: 1
+            }],
+            height:30,
+            adjustLength: -1,
+            offsetStyle: "left"
+        })
+    },
+    _init: function () {
+        var self = this, o = this.options;
+        BI.NumericalIntervalMoreCombo.superclass._init.apply(this, arguments);
+
+        if (BI.isNumeric(o.value)) {
+            o.items[o.value].selected = true
+        } else {
+            o.items[0].selected = true
+        }
+
+        this.trigger = BI.createWidget({
+            type: "bi.numerical_more_interval_trigger",
+            height: o.height-2,
+            value: o.value
+        })
+        this.popup = BI.createWidget({
+            type: "bi.numerical-interval-popup",
+            value: o.value,
+            items: o.items
+        })
+        this.combo = BI.createWidget({
+            element: this.element,
+            type: "bi.combo",
+            trigger: "click,hover",
+            isNeedAdjustHeight: false,
+            isNeedAdjustWidth: false,
+            offsetStyle: o.offsetStyle,
+            adjustLength: o.adjustLength,
+            el: self.trigger,
+            popup: {
+                el: self.popup,
+                stopPropagation:false,
+                tgap: self.constants.tgap
+            }
+
+        })
+
+        this.combo.on(BI.Combo.EVENT_EXPAND, function () {
+            self.trigger.hover();
+        })
+
+        this.combo.on(BI.Controller.EVENT_CHANGE,function(){
+            self.fireEvent(BI.Controller.EVENT_CHANGE,arguments);
+        })
+
+        this.combo.on(BI.Combo.EVENT_COLLAPSE, function () {
+            self.trigger.dishover();
+        })
+
+        this.popup.on(BI.NumericalIntervalPopup.EVENT_CHANGE, function () {
+            var value = self.popup.getValue();
+            self.combo.hideView();
+            self.trigger.setValue(value[0]);
+            self.fireEvent(BI.NumericalIntervalCombo.EVENT_CHANGE);
+        })
+
+        this.popup.on(BI.Controller.EVENT_CHANGE,function(){
+            self.fireEvent(BI.Controller.EVENT_CHANGE,arguments);
+        })
+
+    },
+
+    setEnable:function(b){
+        this.combo.setEnable(b);
+    },
+
+    setTitle: function (v) {
+        this.trigger.setTitle(v);
+    },
+
+    getValue: function () {
+        return this.popup.getValue();
+    },
+
+    setValue: function (v) {
+        var o = this.options, self = this;
+        v = BI.isArray(v) ? v[0] : v;
+        BI.each(o.items, function (i, item) {
+            if (v === item.value) {
+                self.popup.setValue(item.value);
+                self.trigger.setValue(item.value);
+            }
+        })
+    }
+})
+BI.NumericalIntervalCombo.EVENT_CHANGE = "EVENT_CHANGE";
+$.shortcut("bi.numerical_interval_more_combo", BI.NumericalIntervalMoreCombo);
