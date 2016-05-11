cf52e6cabc3f182079ef9b710fc43d0a6258194e
wuk <kary@fineReport.com>
2016-05-11 11:04:26 +0800

    后台

--- fbi_conf/src/com/fr/bi/conf/data/source/operator/add/rowcal/correspondperiod/CorrespondMonthperiodRowCalculatorOperator.java
+++ fbi_conf/src/com/fr/bi/conf/data/source/operator/add/rowcal/correspondperiod/CorrespondMonthperiodRowCalculatorOperator.java
@@ -6,7 +6,7 @@ import com.fr.bi.base.key.BIKey;
 /**
  * Created by 小灰灰 on 2016/5/9.
  */
-public class CorrespondMonthperiodRowCalculatorOperator extends CorrespondperiodRowCalculatorOperator {
+public class CorrespondMonthPeriodRowCalculatorOperator extends CorrespondPeriodRowCalculatorOperator {
     @BICoreField
     private static final String XML_TAG="CorrespondMonthPeriodRowCalculatorOperator";
     @BICoreField
--- fbi_conf/src/com/fr/bi/conf/data/source/operator/add/rowcal/correspondperiod/CorrespondperiodRowCalculatorOperator.java
+++ fbi_conf/src/com/fr/bi/conf/data/source/operator/add/rowcal/correspondperiod/CorrespondperiodRowCalculatorOperator.java
@@ -14,7 +14,7 @@ import com.fr.bi.stable.engine.cal.ResultDealer;
  * @author Daniel
  *
  */
-public class CorrespondperiodRowCalculatorOperator extends RowCalculatorOperator {
+public class CorrespondPeriodRowCalculatorOperator extends RowCalculatorOperator {
 
 	/**
 	 * 
