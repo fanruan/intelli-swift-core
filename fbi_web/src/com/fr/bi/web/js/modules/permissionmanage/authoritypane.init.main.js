0630ab35702263dc6893e8f83d52809c0da91775
wuk <kary@fineReport.com>
2016-05-14 17:22:36 +0800

    tab嵌套

--- fbi_web/src/com/fr/bi/web/js/modules/permissionmanage/authoritypane.init.main.js
+++ fbi_web/src/com/fr/bi/web/js/modules/permissionmanage/authoritypane.init.main.js
@@ -1,39 +1,40 @@
 /**
  * Created by wuk on 16/4/29.
- * 初始页面,分批量和单独编辑两种情况
+ * 初始页面,分批量和单独编辑两种情况显示
  */
-BI.AuthorityPaneInitMain = BI.inherit(BI.Widget, {
+BI.authorityPaneInitMain = BI.inherit(BI.Widget, {
     _defaultConfig: function () {
-        return BI.extend(BI.AuthorityPaneInitMain.superclass._defaultConfig.apply(this, arguments), {
+        return BI.extend(BI.authorityPaneInitMain.superclass._defaultConfig.apply(this, arguments), {
             baseCls: '',
             items: []
         })
     },
     _init: function () {
-        BI.AuthorityPaneInitMain.superclass._init.apply(this, arguments);
+        BI.authorityPaneInitMain.superclass._init.apply(this, arguments);
         this.packageIDs =[];
         this.tab = BI.createWidget({
             type: "bi.tab",
             element: this.element,
             tab: null,
-            defaultShowIndex: BI.AuthorityPaneInitMain.SelectPane.paneSingleShow,
+            defaultShowIndex: BI.authorityPaneInitMain.SelectPane.paneSingleShow,
             cardCreator: BI.bind(this._createTabs, this)
         });
     },
     _createTabs: function (type) {
+        alert('初始界面:'+type);
         var self=this;
         switch (type) {
-            case BI.AuthorityPaneInitMain.SelectPane.paneSingleShow:
+            case BI.authorityPaneInitMain.SelectPane.paneSingleShow:
                 this.paneSingleShow = BI.createWidget({
                     type: "bi.authority_pane_single"
                 });
                 return this.paneSingleShow;
-            case BI.AuthorityPaneInitMain.SelectPane.paneMultiShow:
+            case BI.authorityPaneInitMain.SelectPane.paneMultiShow:
                 this.paneMultiShow = BI.createWidget({
                     type: "bi.authority_pane_multi"
                 });
                 this.paneMultiShow.on(BI.AuthorityPaneMulti.EVENT_CHANGE, function () {
-                    self.fireEvent(BI.AuthorityPaneInitMain.EVENT_CHANGE);
+                    self.fireEvent(BI.authorityPaneInitMain.EVENT_CHANGE);
                 });
                 return this.paneMultiShow;
         }
@@ -46,10 +47,10 @@ BI.AuthorityPaneInitMain = BI.inherit(BI.Widget, {
     _switchPane: function (v) {
         this.tab.setSelect(v);
         switch (v) {
-            case BI.AuthorityPaneInitMain.SelectPane.paneSingleShow:
+            case BI.authorityPaneInitMain.SelectPane.paneSingleShow:
                 this.paneSingleShow.populate();
                 break;
-            case BI.AuthorityPaneInitMain.SelectPane.paneMultiShow:
+            case BI.authorityPaneInitMain.SelectPane.paneMultiShow:
                 this.paneMultiShow.populate(this.packageIDs);
                 break;
         }
@@ -57,9 +58,9 @@ BI.AuthorityPaneInitMain = BI.inherit(BI.Widget, {
 
 });
 
-BI.AuthorityPaneInitMain.EVENT_CHANGE = "EVENT_CHANGE";
-BI.AuthorityPaneInitMain.SelectPane = {
+BI.authorityPaneInitMain.EVENT_CHANGE = "EVENT_CHANGE";
+BI.authorityPaneInitMain.SelectPane = {
     paneSingleShow:BI.Selection.Single,
     paneMultiShow: BI.Selection.Multi
 };
-$.shortcut('bi.authority_pane_init_main', BI.AuthorityPaneInitMain)
+$.shortcut('bi.authority_pane_init_main', BI.authorityPaneInitMain)
