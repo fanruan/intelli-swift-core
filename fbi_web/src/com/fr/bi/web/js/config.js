00b2d248e6cf735445a068a5a32ebfe39250f630
root <guy@finereport.com>
2016-05-19 09:55:51 +0800

    各位的居中布局用错了场景，先变回去了

--- fbi_web/src/com/fr/bi/web/js/config.js
+++ fbi_web/src/com/fr/bi/web/js/config.js
@@ -1,13 +1,13 @@
 //工程配置
 $(function () {
     BI.isSupportFlex = BI.isSupportCss3("flex");
-    BI.Plugin.registerWidget("bi.center_adapt", function (ob) {
-        if (BI.isSupportFlex) {
-            return BI.extend({}, ob, {type: "bi.flexbox_center_adapt"});
-        } else {
-            return ob;
-        }
-    });
+    //BI.Plugin.registerWidget("bi.center_adapt", function (ob) {
+    //    if (BI.isSupportFlex) {
+    //        return BI.extend({}, ob, {type: "bi.flexbox_center_adapt"});
+    //    } else {
+    //        return ob;
+    //    }
+    //});
     BI.Plugin.registerWidget("bi.float_center_adapt", function (ob) {
         if (BI.isSupportFlex) {
             return BI.extend({}, ob, {type: "bi.flexbox_center_adapt"});
