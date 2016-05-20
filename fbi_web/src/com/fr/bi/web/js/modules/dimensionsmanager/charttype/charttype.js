ee2037fbcb002dd9d202a89ea848674ab54d4e99
root <guy@finereport.com>
2016-05-20 16:35:14 +0800

    chart类型整理

--- fbi_web/src/com/fr/bi/web/js/modules/dimensionsmanager/charttype/charttype.js
+++ fbi_web/src/com/fr/bi/web/js/modules/dimensionsmanager/charttype/charttype.js
@@ -5,26 +5,6 @@
  */
 BI.ChartType = BI.inherit(BI.Widget, {
 
-    constants: {
-        sort: {
-            TABLE: 0,
-            AXIS: 1,
-            BAR: 2,
-            LINE: 7,
-            AREA: 3,
-            COMBINE: 4,
-            PIE: 8,
-            DONUT: 9,
-            MAP: 10,
-            GIS_MAP: 11,
-            DASHBOARD: 12,
-            BUBBLE: 5,
-            SCATTER: 13,
-            RADAR: 6,
-            FUNNEL: 14
-        }
-    },
-
     _defaultConfig: function () {
         return BI.extend(BI.ChartType.superclass._defaultConfig.apply(this, arguments), {
             baseCls: "bi-chart-type"
@@ -35,87 +15,56 @@ BI.ChartType = BI.inherit(BI.Widget, {
         BI.ChartType.superclass._init.apply(this, arguments);
         var self = this, o = this.options;
 
-        this.multiTypeChart = BI.map([
-            BICst.TABLE_TYPE,
-            BICst.AXIS_CHART_TYPE,
-            BICst.BAR_CHART_TYPE,
-            BICst.AREA_CHART_TYPE,
-            BICst.COMBINE_CHART_TYPE,
-            BICst.BUBBLE_CHART_TYPE,
-            BICst.RADAR_CHART_TYPE
-        ], function(idx, item){
-            return BI.createWidget({
-                type: "bi.table_chart_combo",
-                width: 40,
-                height: 34,
-                items: item
-            });
-        });
-
-        var singleTypeChart = BI.createWidgets(BICst.SINGLE_TYPE_CHART, {
-            type: "bi.icon_button",
-            width: 40,
-            height: 34,
-            iconHeight: 25,
-            iconWidth: 25,
-            extraCls: "chart-type-icon"
-        });
-
-        this.singleGroup = BI.createWidget({
-            type: "bi.button_group",
-            items: singleTypeChart,
+        this.buttonTree = BI.createWidget({
+            type: "bi.button_tree",
+            element: this.element,
+            items: this._formatItems(BI.deepClone(BICst.DASHBOARD_WIDGETS[0])),
             layouts: [{
-                type: "bi.default"
+                type: "bi.horizontal",
+                scrollx: false,
+                scrollable: false,
+                vgap: 3,
+                hgap: 3
             }]
         });
-
-        this.singleGroup.on(BI.ButtonGroup.EVENT_CHANGE, function (v) {
-            self.setValue(v);
+        this.buttonTree.on(BI.ButtonTree.EVENT_CHANGE, function () {
             self.fireEvent(BI.ChartType.EVENT_CHANGE, arguments);
-        });
-
-        BI.each(this.multiTypeChart, function(idx, widget){
-            widget.on(BI.TableChartCombo.EVENT_CHANGE, function(v){
-                self.setValue(v);
-                self.fireEvent(BI.ChartType.EVENT_CHANGE, arguments);
-            });
-        });
-
-        var sorted = [];
-        var all = BI.concat(this.multiTypeChart, singleTypeChart);
-        BI.each(this.constants.sort, function(idx, v){
-            sorted[sorted.length] = all[v];
-        });
-
-        BI.createWidget({
-            type: "bi.horizontal",
-            element: this.element,
-            scrollx: false,
-            scrollable: false,
-            items: sorted,
-            vgap: 3,
-            hgap: 3
         })
     },
 
-    getValue: function () {
-        var value = null;
-        BI.any(this.multiTypeChart, function(idx, chart){
-            if(chart.isSelected()){
-                value = chart.getValue();
+    _formatItems: function (items) {
+        var self = this;
+        var result = [];
+        BI.each(items, function (i, item) {
+            if (BI.isNotEmptyArray(item.children)) {
+                BI.each(item.children, function (i, child) {
+                    child.iconClass = child.cls;
+                });
+                result.push({
+                    type: "bi.icon_combo",
+                    cls: "chart-type-combo",
+                    width: 40,
+                    iconClass: item.cls,
+                    items: item.children
+                });
+            } else {
+                result.push(BI.extend({
+                    type: "bi.icon_button",
+                    width: 40
+                }, item, {
+                    cls: item.cls + " chart-type-icon"
+                }));
             }
         });
-        if(BI.isNull(value)){
-            value = this.singleGroup.getValue()[0];
-        }
-        return value;
+        return result;
+    },
+
+    getValue: function () {
+        return this.buttonTree.getValue()[0];
     },
 
     setValue: function (v) {
-        BI.each(this.multiTypeChart, function(idx, chart){
-            chart.setValue(v);
-        });
-        this.singleGroup.setValue(v);
+        this.buttonTree.setValue(v);
     }
 });
 BI.ChartType.EVENT_CHANGE = "EVENT_CHANGE";
