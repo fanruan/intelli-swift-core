89394d430684a5cbb66a54f3a7c7f5cc22f70b05
wuk <kary@fineReport.com>
2016-05-23 20:46:32 +0800

    Merge branch 'dev' of http://www.finedevelop.com:2015/scm/bi/nuclear into dev
    # Conflicts:
    #	fbi_web/src/com/fr/bi/web/js/data/req/req.js
    #	fbi_web/src/com/fr/bi/web/js/modules/utils4conf.js

--- fbi_web/src/com/fr/bi/web/js/extend/update/singletable/update.singletable.setting.js
+++ fbi_web/src/com/fr/bi/web/js/extend/update/singletable/update.singletable.setting.js
@@ -65,22 +65,6 @@ BI.UpdateSingleTableSetting = BI.inherit(BI.Widget, {
 
         //定时设置
         var timeSetting = this._createTimeSetting();
-        this.immediateButton = BI.createWidget({
-            type: "bi.button",
-            text: BI.i18nText("BI-Update_Table_Immedi"),
-            height: 30,
-            handler: function () {
-                self.immediateButton.setEnable(false);
-                self.immediateButton.setText(BI.i18nText("BI-Cube_is_Generating"));
-                console.log('false');
-                BI.Utils.generateCubeByTable(self.model.table.id, function () {
-                    self._createCheckInterval();
-                });
-
-
-            }
-        });
-
         BI.createWidget({
             type: "bi.vertical",
             element: this.element,
@@ -100,7 +84,18 @@ BI.UpdateSingleTableSetting = BI.inherit(BI.Widget, {
                     el: this.updateType,
                     width: "fill"
                 }, {
-                    el: this.immediateButton,
+                    el: {
+                        type: "bi.button",
+                        text: BI.i18nText("BI-Update_Table_Immedi"),
+                        height: 30,
+                        handler: function() {
+                            self.immediateButton.setEnable(false);
+                            self.immediateButton.setText(BI.i18nText("BI-Cube_is_Generating"));
+                            BI.Utils.generateCubeByTable(self.model.table.id, function () {
+                                self._createCheckInterval();
+                            });
+                        }
+                    },
                     width: 105
                 }],
                 hgap: 5,
