f9f77091bde31305532427248845b157dcef6588
黑桃钢铁侠 <connery@finereport.com>
2016-06-06 10:43:21 +0800

    unsafe构造方法

--- fbi_base/test/com/fr/bi/stable/utils/program/BIConstructorUtilsTest.java
+++ fbi_base/test/com/fr/bi/stable/utils/program/BIConstructorUtilsTest.java
@@ -1,10 +1,23 @@
 package com.fr.bi.stable.utils.program;
 
+import com.fr.bi.common.persistent.writer.BIIgnore4Test;
+import junit.framework.TestCase;
+
 /**
  * This class created on 2016/6/6.
  *
  * @author Connery
  * @since 4.0
  */
-public class BIConstructorUtilsTest {
+public class BIConstructorUtilsTest extends TestCase {
+    public void testUnsafeConstructor() {
+        try {
+            BIIgnore4Test test = BIConstructorUtils.unsafeConstructObject(BIIgnore4Test.class);
+            assertTrue(test.getA() == null);
+            assertTrue(test.getB() == null);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertFalse(true);
+        }
+    }
 }
