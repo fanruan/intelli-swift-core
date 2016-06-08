c5af73f3b7831fedf884c3796d914ca57d8a54d2
kary <kary@fineReport.com>
2016-06-08 15:01:11 +0800

    cube

--- fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/CubeTaskBuild.java
+++ fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/CubeTaskBuild.java
@@ -3,12 +3,6 @@ package com.fr.bi.web.conf.services.cubetask;
 import com.finebi.cube.conf.BICubeManagerProvider;
 import com.finebi.cube.conf.CubeBuildStuff;
 import com.finebi.cube.conf.CubeGenerationManager;
-import com.finebi.cube.conf.pack.imp.BIPackageTableSourceConfigManager;
-import com.fr.bi.base.BIUser;
-import com.fr.bi.cal.generate.BuildCubeTask;
-import com.fr.bi.stable.data.source.CubeTableSource;
-
-import java.util.Set;
 
 /**
  * Created by wuk on 16/5/30.
@@ -18,8 +12,6 @@ public class CubeTaskBuild {
     private static BICubeManagerProvider cubeManager= CubeGenerationManager.getCubeManager();
 
     public static void CubeBuild(long userId,CubeBuildStuff cubeBuildStuff){
-        BIPackageTableSourceConfigManager biPackageFindTableSourceConfigManager=new BIPackageTableSourceConfigManager();
-        Set<CubeTableSource> cubeTableSources = biPackageFindTableSourceConfigManager.getTableSources4Genrate(userId);
-        cubeManager.addTask(new BuildCubeTask(new BIUser(userId),cubeBuildStuff),userId);
+//        cubeManager.addTask(new BuildCubeTask(new BIUser(userId),cubeBuildStuff),userId);
     }
 }
