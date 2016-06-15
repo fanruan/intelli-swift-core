a387164cc5b704881abfb04ef69975c1dfe0710f
kary <kary@fineReport.com>
2016-06-15 14:53:27 +0800

    bug fix

--- fbi_engine_generate/src/com/fr/bi/cal/generate/BuildCubeTask.java
+++ fbi_engine_generate/src/com/fr/bi/cal/generate/BuildCubeTask.java
@@ -80,16 +80,13 @@ public class BuildCubeTask implements CubeTask {
 
     @Override
     public void end() {
-//        Future<String> result = finishObserver.getOperationResult();
         try {
             BICubeConfigureCenter.getPackageManager().finishGenerateCubes(biUser.getUserId());
             BICubeConfigureCenter.getTableRelationManager().finishGenerateCubes(biUser.getUserId(), cubeBuildStuff.getTableRelationSet());
-//            BILogger.getLogger().info(result.get());
         } catch (Exception e) {
             BILogger.getLogger().error(e.getMessage(), e);
         } finally {
             try {
-//                BICubeConfigureCenter.getPackageManager().endBuildingCube(biUser.getUserId());
                 cube.addVersion(System.currentTimeMillis());
             } catch (Exception e) {
                 e.printStackTrace();
