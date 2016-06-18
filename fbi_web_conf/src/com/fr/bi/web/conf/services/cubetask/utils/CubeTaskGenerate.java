b64c2c2a314f1e90a5af5b888828000f9f27f45d
wuk <kary@fineReport.com>
2016-06-18 18:37:26 +0800

    cube

--- fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/utils/CubeTaskGenerate.java
+++ fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/utils/CubeTaskGenerate.java
@@ -5,9 +5,12 @@ import com.finebi.cube.conf.BICubeManagerProvider;
 import com.finebi.cube.conf.CubeBuildStuff;
 import com.finebi.cube.conf.CubeGenerationManager;
 import com.finebi.cube.conf.table.BIBusinessTable;
+import com.finebi.cube.impl.conf.CubeBuildStuffManager;
 import com.finebi.cube.impl.conf.CubeBuildStuffManagerIncremental;
+import com.finebi.cube.impl.conf.CubeBuildStuffManagerSingleTable;
 import com.fr.bi.base.BIUser;
 import com.fr.bi.cal.generate.BuildCubeTask;
+import com.fr.bi.stable.data.BITableID;
 
 import java.util.Set;
 
@@ -17,8 +20,9 @@ import java.util.Set;
 public class CubeTaskGenerate {
 
     private static BICubeManagerProvider cubeManager = CubeGenerationManager.getCubeManager();
-
-    public static void CubeBuild(long userId, CubeBuildStuff cubeBuildStuff) {
+    
+    public static void CubeBuild(long userId, BITableID biTableID) {
+        CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManagerSingleTable( new BIBusinessTable(biTableID),userId);
         cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff), userId);
     }
 
@@ -26,6 +30,8 @@ public class CubeTaskGenerate {
         BICubeConfigureCenter.getPackageManager().getPackages4CubeGenerate(userId);
         Set<BIBusinessTable> newTables = BICubeGenerateTool.getTables4CubeGenerate(userId);
         CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManagerIncremental(newTables, userId);
+        CubeBuildStuff cubeBuildStuff2 = new CubeBuildStuffManager(new BIUser(userId));
         cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff), userId);
+//        cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff2), userId);
     }
 }
