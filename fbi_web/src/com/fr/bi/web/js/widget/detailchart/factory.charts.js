96c687000cba8101a010864a05080b5df53bbe66
windy storm <1374721899@qq.com>
2016-06-21 09:16:53 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/widget/detailchart/factory.charts.js
+++ fbi_web/src/com/fr/bi/web/js/widget/detailchart/factory.charts.js
@@ -86,27 +86,173 @@ BI.ChartCombineFormatItemFactory = {
 
     combineConfig: function(type){
         var config = {
-            plotOptions:{
-                marker:{
-                    symbol:'circle'
-                },
+            "plotOptions": {
+                "rotatable": false,
+                "startAngle": 0,
+                "borderRadius": 0,
+                "endAngle": 360,
+                "innerRadius": "0.0%",
 
-                dataLabels:{
-                    enabled:true,
-                    formatter:function(){return this.seriesName;}
+                "layout": "horizontal",
+                "hinge": "rgb(101,107,109)",
+                "dataLabels":{
+                    "style": "{color: #d4dadd, fontSize: 9pt}",
+                    "formatter": {
+                        "identifier": "${VALUE}",
+                        "valueFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "seriesFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "percentFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##%') : arguments[0]}",
+                        "categoryFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}"
+                    },
+                    "align": "inside",
+                    "enabled": false
                 },
+                "percentageLabel": {
+                    "formatter": {
+                        "identifier": "${PERCENT}",
+                        "valueFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "seriesFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "percentFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##%') : arguments[0]}",
+                        "categoryFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}"
+                    },
+                    "style": {"fontFamily": "Verdana", "color": "rgba(51,51,51,1.0)", "fontSize": "14pt", "fontWeight": "bold"},
+                    "align": "bottom",
+                    "enabled": true
+                },
+                "valueLabel": {
+                    "formatter": {
+                        "identifier": "${SERIES}${VALUE}",
+                        "valueFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "seriesFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "percentFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##%') : arguments[0]}",
+                        "categoryFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}"
+                    },
+                    "backgroundColor": "rgb(255,255,0)",
+                    "style": {
+                        "fontFamily": "Verdana",
+                        "color": "rgba(51,51,51,1.0)",
+                        "fontSize": "8pt",
+                        "fontWeight": ""
+                    },
+                    "align": "inside",
+                    "enabled": true
+                },
+                "hingeBackgroundColor": "rgb(220,242,249)",
+                "seriesLabel": {
+                    "formatter": {
+                        "identifier": "${CATEGORY}",
+                        "valueFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "seriesFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "percentFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##%') : arguments[0]}",
+                        "categoryFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}"
+                    },
+                    "style": {
+                        "fontFamily": "Verdana",
+                        "color": "rgba(51,51,51,1.0)",
+                        "fontSize": "10pt",
+                        "fontWeight": ""
+                    },
+                    "align": "bottom",
+                    "enabled": true
+                },
+                "style": "pointer",
+                "paneBackgroundColor": "rgb(252,252,252)",
+                "needle": "rgb(229,113,90)",
 
-                tooltip:{
-                    enabled:true,
-                    formatter:function(){return this.seriesName;},
-                    follow:false
+
+                "large": false,
+                "connectNulls": false,
+                "shadow": true,
+                "curve": false,
+                "sizeBy": "area",
+                "tooltip": {
+                    "formatter": {
+                        "identifier": "${SERIES}${X}${Y}${SIZE}{CATEGORY}${SERIES}${VALUE}",
+                        "valueFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "seriesFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "percentFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##%') : arguments[0]}",
+                        "categoryFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "xFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                        "sizeFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '#.##') : arguments[0]}",
+                        "yFormat": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}"
+                    },
+                    "shared": false,
+                    "padding": 5,
+                    "backgroundColor": "rgba(0,0,0,0.4980392156862745)",
+                    "borderColor": "rgb(0,0,0)",
+                    "shadow": false,
+                    "borderRadius": 2,
+                    "borderWidth": 0,
+                    "follow": false,
+                    "enabled": true,
+                    "animation": true
+                },
+                "maxSize": 70,
+                "fillColorOpacity": 0.5,
+                "marker": {"symbol": "circle", "radius": 4.5, "enabled": true},
+                "step": false,
+                "force": false,
+                "minSize": 15,
+                "displayNegative": true,
+                "categoryGap": "16.0%",
+                "borderColor": "rgb(255,255,255)",
+                "borderWidth": 1,
+                "gap": "22.0%",
+                "animation": true,
+                "lineWidth": 2,
+
+                bubble:{
+                    "large": false,
+                    "connectNulls": false,
+                    "shadow": true,
+                    "curve": false,
+                    "sizeBy": "area",
+                    "maxSize": 60,
+                    "minSize": 12,
+                    "lineWidth": 0,
+                    "animation": true,
+                    "fillColorOpacity": 0.699999988079071,
+                    "marker": {
+                        "symbol": "circle",
+                        "radius": 28.39695010101295,
+                        "enabled": true
+                    }
                 }
             },
-
-            legend:{
-                enabled:true
+            dataSheet: {
+                enabled: false,
+                "borderColor": "rgb(0,0,0)",
+                "borderWidth": 1,
+                "formatter": "function(){return window.FR ? FR.contentFormat(arguments[0], '') : arguments[0]}",
+                style:{"fontFamily":"宋体","color":"rgba(0,0,0,1.0)","fontSize":"9pt","fontWeight":""}
             },
-            "chartType": "areaMap"
+            "borderColor": "rgb(238,238,238)",
+            "shadow": false,
+            "legend": {
+                "borderColor": "rgb(204,204,204)",
+                "borderRadius": 0,
+                "shadow": false,
+                "borderWidth": 0,
+                "style": {"fontFamily": "微软雅黑", "color": "rgba(102,102,102,1.0)", "fontSize": "11pt", "fontWeight": ""},
+                "position": "right",
+                "enabled": true
+            },
+            "zoom": {"zoomType": "xy", "zoomTool": {"visible": false, "resize": true, "from": "", "to": ""}},
+            "plotBorderColor": "rgba(255,255,255,0)",
+            "tools": {
+                "hidden": true,
+                "toImage": {"enabled": true},
+                "sort": {"enabled": true},
+                "enabled": false,
+                "fullScreen": {"enabled": true}
+            },
+            "plotBorderWidth": 0,
+            "colors": ["rgb(99,178,238)", "rgb(118,218,145)"],
+            "borderRadius": 0,
+            "borderWidth": 0,
+            "style": "normal",
+            "plotShadow": false,
+            "plotBorderRadius": 0
         };
         switch (type) {
             case BICst.WIDGET.BAR:
