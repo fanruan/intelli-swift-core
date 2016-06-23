fd5155a5c616978f707f78be517383a895ecadea
黑桃钢铁侠 <connery@finereport.com>
2016-06-23 14:05:32 +0800

    优化接口

---
+++ fbi_analysis_api/test/com/finebi/analysis/api/SelectionTest.java
@@ -0,0 +1,93 @@
+package com.finebi.analysis.api;
+
+import com.finebi.analysis.api.criteria.CriteriaBuilder;
+import com.finebi.analysis.api.criteria.CriteriaQuery;
+import com.finebi.analysis.api.criteria.Join;
+import com.finebi.analysis.api.criteria.Root;
+import com.finebi.analysis.api.criteria.PlainTable;
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+
+/**
+ * This class created on 2016/6/23.
+ *
+ * @author Connery
+ * @since 4.0
+ */
+public class SelectionTest extends TestCase {
+    /**
+     * Detail:
+     * Author:Connery
+     * Date:2016/6/23
+     */
+    public void testSelect() {
+        try {
+            PlainTable target= EasyMock.createMock(PlainTable.class);
+            CriteriaBuilder cb = generateCB();
+            CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
+            Root<PlainTable> root = query.from(target);
+            query.select(target);
+            executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    /**
+     * Detail:
+     * Author:Connery
+     * Date:2016/6/23
+     */
+    public void testJoin() {
+        try {
+            PlainTable tar = EasyMock.createMock(PlainTable.class);
+            PlainTable src = EasyMock.createMock(PlainTable.class);
+            CriteriaBuilder cb = generateCB();
+            CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
+            Root<PlainTable> root = query.from(src);
+            Join join = root.join(src);
+            join.on(cb.equal(tar.getColumn("id"), src.getColumn("id")));
+            query.select(tar);
+            executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+    /**
+     * Detail:
+     * Author:Connery
+     * Date:2016/6/23
+     */
+    public void testMutiJoin() {
+        try {
+            PlainTable one = EasyMock.createMock(PlainTable.class);
+            PlainTable two = EasyMock.createMock(PlainTable.class);
+            PlainTable three = EasyMock.createMock(PlainTable.class);
+
+            CriteriaBuilder cb = generateCB();
+            CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
+            Root<PlainTable> root = query.from(two);
+            Join join = root.join(two);
+            join.on(cb.equal(one.getColumn("id"), two.getColumn("id"))).join(three);
+            query.select(one);
+            executeQuery(query);
+        } catch (Exception e) {
+            e.printStackTrace();
+            assertTrue(false);
+        }
+    }
+
+    private void executeQuery(CriteriaQuery query) {
+
+    }
+
+    private CriteriaBuilder generateCB() {
+        CriteriaBuilder cb = EasyMock.createMock(CriteriaBuilder.class);
+//        cb.createQuery(PlainTable.class);
+//        CriteriaQuery<PlainTable> query=EasyMock.createMock(CriteriaQuery.class);
+//        query.from()
+        return cb;
+    }
+}
