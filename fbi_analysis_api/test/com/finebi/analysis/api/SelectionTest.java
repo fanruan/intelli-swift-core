b0f05208835a15e842453ac184e2b090a27758e5
黑桃钢铁侠 <connery@finereport.com>
2016-06-23 15:36:03 +0800

    注释demo

--- fbi_analysis_api/test/com/finebi/analysis/api/SelectionTest.java
+++ fbi_analysis_api/test/com/finebi/analysis/api/SelectionTest.java
@@ -1,10 +1,6 @@
 package com.finebi.analysis.api;
 
-import com.finebi.analysis.api.criteria.CriteriaBuilder;
-import com.finebi.analysis.api.criteria.CriteriaQuery;
-import com.finebi.analysis.api.criteria.Join;
-import com.finebi.analysis.api.criteria.Root;
-import com.finebi.analysis.api.criteria.PlainTable;
+import com.finebi.analysis.api.criteria.*;
 import junit.framework.TestCase;
 import org.easymock.EasyMock;
 
@@ -16,6 +12,7 @@ import org.easymock.EasyMock;
  */
 public class SelectionTest extends TestCase {
     /**
+     * 普通查询
      * Detail:
      * Author:Connery
      * Date:2016/6/23
@@ -23,11 +20,17 @@ public class SelectionTest extends TestCase {
     public void testSelect() {
         try {
             PlainTable target = EasyMock.createMock(PlainTable.class);
+            PlainResultMate resultMate = EasyMock.createMock(PlainResultMate.class);
+
             CriteriaBuilder cb = generateCB();
-            CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
-            Root<PlainTable> root = query.from(target);
-            query.select(target);
-            executeQuery(query);
+
+            CriteriaQuery<PlainTable> query = cb.createQuery();
+            //查询的结果元数据
+            query.select(resultMate);
+
+            //查询的表，获得根
+            query.from(target);
+            Object result = executeQuery(query);
         } catch (Exception e) {
             e.printStackTrace();
             assertTrue(false);
@@ -35,6 +38,7 @@ public class SelectionTest extends TestCase {
     }
 
     /**
+     * 关联的使用
      * Detail:
      * Author:Connery
      * Date:2016/6/23
@@ -43,11 +47,20 @@ public class SelectionTest extends TestCase {
         try {
             PlainTable tar = EasyMock.createMock(PlainTable.class);
             PlainTable src = EasyMock.createMock(PlainTable.class);
+            PlainResultMate resultMate = EasyMock.createMock(PlainResultMate.class);
             CriteriaBuilder cb = generateCB();
-            CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
+            CriteriaQuery<PlainTable> query = cb.createQuery();
+
+            //查询的结果元数据
+            query.select(resultMate);
+
+            //查询的表，获得根
             Root<PlainTable> root = query.from(src);
+
+            //对查询根进行关联操作
             Join join = root.join(src);
             join.on(cb.equal(tar.getColumn("id"), src.getColumn("id")));
+            //获得结果
             Object result = executeQuery(query);
         } catch (Exception e) {
             e.printStackTrace();
@@ -70,6 +83,7 @@ public class SelectionTest extends TestCase {
             CriteriaQuery<PlainTable> query = cb.createQuery(PlainTable.class);
             Root<PlainTable> root = query.from(two);
             Join join = root.join(two);
+            //对关联进行再次关联操作
             join.on(cb.equal(one.getColumn("id"), two.getColumn("id"))).join(three);
             query.select(one);
             Object result = executeQuery(query);
