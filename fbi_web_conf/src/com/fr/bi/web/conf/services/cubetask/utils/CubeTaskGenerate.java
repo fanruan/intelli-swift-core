c16afd397a3cd01c6a97aaa041c7ae1fa96594fa
kary <kary@fineReport.com>
2016-06-24 15:29:49 +0800

    cube bug

--- fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/utils/CubeTaskGenerate.java
+++ fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/utils/CubeTaskGenerate.java
@@ -1,21 +1,15 @@
 package com.fr.bi.web.conf.services.cubetask.utils;
 
-import com.finebi.cube.conf.BICubeConfigureCenter;
 import com.finebi.cube.conf.BICubeManagerProvider;
 import com.finebi.cube.conf.CubeBuildStuff;
 import com.finebi.cube.conf.CubeGenerationManager;
 import com.finebi.cube.conf.table.BIBusinessTable;
 import com.finebi.cube.impl.conf.CubeBuildStuffManager;
-import com.finebi.cube.impl.conf.CubeBuildStuffManagerIncremental;
 import com.finebi.cube.impl.conf.CubeBuildStuffManagerSingleTable;
 import com.fr.bi.base.BIUser;
 import com.fr.bi.cal.generate.BuildCubeTask;
-import com.fr.bi.conf.provider.BIConfigureManagerCenter;
 import com.fr.bi.stable.data.BITableID;
-import com.fr.bi.stable.engine.CubeTask;
 import com.fr.bi.stable.utils.code.BILogger;
-
-import java.util.Set;
 
 /**
  * Created by wuk on 16/5/30.
@@ -30,27 +24,33 @@ public class CubeTaskGenerate {
     }
 
     public static void CubeBuild(long userId) {
-        BICubeConfigureCenter.getPackageManager().getPackages4CubeGenerate(userId);
-        Set<BIBusinessTable> newTables = BICubeGenerateTool.getTables4CubeGenerate(userId);
-/*若有新增表，增量更新，否则进行全量*/
-        if (newTables.size() != 0) {
-            CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManagerIncremental(newTables, userId);
-            CubeTask task = new BuildCubeTask(new BIUser(userId), cubeBuildStuff);
-            if (!cubeBuildStuff.preConditionsCheck()) {
-                BILogger.getLogger().error("cube生成的前置条件无法满足，请确认硬盘空间足够且数据连接正常！");
-                BIConfigureManagerCenter.getLogManager().logEnd(userId);
-                return;
-            }
-            cubeManager.addTask(task, userId);
-        } else {
-            CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManager(new BIUser(userId));
-            if (!cubeBuildStuff.preConditionsCheck()) {
-                BILogger.getLogger().error("cube生成的前置条件无法满足，请确认硬盘空间足够且数据连接正常！");
-                BIConfigureManagerCenter.getLogManager().logEnd(userId);
-                return;
-            }
-            cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff), userId);
+//        Set<BIBusinessTable> newTables = BICubeGenerateTool.getTables4CubeGenerate(userId);
+///*若有新增表，增量更新，否则进行全量*/
+//        if (newTables.size() != 0) {
+//            CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManagerIncremental(newTables, userId);
+//            CubeTask task = new BuildCubeTask(new BIUser(userId), cubeBuildStuff);
+//            if (!cubeBuildStuff.preConditionsCheck()) {
+//                BILogger.getLogger().error("cube生成的前置条件无法满足，请确认硬盘空间足够且数据连接正常！");
+//                BIConfigureManagerCenter.getLogManager().logEnd(userId);
+//                return;
+//            }
+//            cubeManager.addTask(task, userId);
+//        } else {
+//            CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManager(new BIUser(userId));
+//            if (!cubeBuildStuff.preConditionsCheck()) {
+//                BILogger.getLogger().error("cube生成的前置条件无法满足，请确认硬盘空间足够且数据连接正常！");
+//                BIConfigureManagerCenter.getLogManager().logEnd(userId);
+//                return;
+//            }
+//            cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff), userId);
+//        }
+        CubeBuildStuff cubeBuildStuff = new CubeBuildStuffManager(new BIUser(userId));
+        if (!cubeBuildStuff.preConditionsCheck()) {
+            BILogger.getLogger().error("cube生成的前置条件无法满足，请确认硬盘空间足够且数据连接正常！");
+//            BIConfigureManagerCenter.getLogManager().logEnd(userId);
+//            return;
         }
+        cubeManager.addTask(new BuildCubeTask(new BIUser(userId), cubeBuildStuff), userId);
 
     }
 }
