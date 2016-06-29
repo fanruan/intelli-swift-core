579df45ff932fecedd1cf1085346a86e2b6e6f36
kary <kary@fineReport.com>
2016-06-29 13:59:11 +0800

    bug fix

--- fbi_engine_generate/src/com/fr/bi/cal/generate/timerTask/BICubeTimeTaskCreatorManager.java
+++ fbi_engine_generate/src/com/fr/bi/cal/generate/timerTask/BICubeTimeTaskCreatorManager.java
@@ -2,6 +2,8 @@ package com.fr.bi.cal.generate.timerTask;
 
 import com.finebi.cube.conf.CubeBuildStuff;
 import com.finebi.cube.conf.CubeGenerationManager;
+import com.finebi.cube.conf.datasource.BIBusinessTableSource;
+import com.finebi.cube.conf.datasource.BusinessTableSourceService;
 import com.finebi.cube.conf.table.BIBusinessTable;
 import com.finebi.cube.impl.conf.CubeBuildStuffManager;
 import com.finebi.cube.impl.conf.CubeBuildStuffManagerSingleTable;
@@ -32,8 +34,11 @@ public class BICubeTimeTaskCreatorManager implements BICubeTimeTaskCreator {
                 }
             } else {
                 for (TimeFrequency frequency : allTimeTaskMap.get(keys).getTimeList()) {
-                    BIBusinessTable table = new BIBusinessTable(new BITableID(keys));
-                    timerList.add(addSingleTableTask(frequency, table, userId));
+                    BusinessTableSourceService tableSourceService = new BIBusinessTableSource();
+                    if (tableSourceService.containBusinessTable(new BITableID(keys))) {
+                        BIBusinessTable table = new BIBusinessTable(new BITableID(keys));
+                        timerList.add(addSingleTableTask(frequency, table, userId));
+                    }
                 }
 
             }
