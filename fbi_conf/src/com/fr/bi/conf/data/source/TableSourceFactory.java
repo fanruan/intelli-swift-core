c5b721465e17996250193d9905e91417825cfc08
young <young@finereport.com>
2016-07-05 11:20:45 +0800

    bug

--- fbi_conf/src/com/fr/bi/conf/data/source/TableSourceFactory.java
+++ fbi_conf/src/com/fr/bi/conf/data/source/TableSourceFactory.java
@@ -1,18 +1,32 @@
 package com.fr.bi.conf.data.source;
 
+import com.fr.bi.stable.constant.DBConstant;
 import com.fr.bi.stable.data.source.AbstractTableSource;
 import com.fr.json.JSONObject;
 import com.fr.stable.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 小灰灰 on 2015/12/21.
  */
 public class TableSourceFactory {
+
+    public static final Map<String, Class> SOURCES = new HashMap<String, Class>() {
+        {
+            put(DBConstant.CONNECTION.ETL_CONNECTION, ETLTableSource.class);
+            put(DBConstant.CONNECTION.EXCEL_CONNECTION, ExcelTableSource.class);
+            put(DBConstant.CONNECTION.SERVER_CONNECTION, ServerTableSource.class);
+            put(DBConstant.CONNECTION.SQL_CONNECTION, SQLTableSource.class);
+        }
+    };
+
     public static AbstractTableSource createTableSource(JSONObject jo, long userId) throws Exception {
         String connectionName = jo.optString("connection_name", StringUtils.EMPTY);
         AbstractTableSource tableSource;
-        if(TableSourceMap.SOURCES.get(connectionName) != null) {
-            tableSource = (AbstractTableSource) TableSourceMap.SOURCES.get(connectionName).newInstance();
+        if(SOURCES.get(connectionName) != null) {
+            tableSource = (AbstractTableSource) SOURCES.get(connectionName).newInstance();
             tableSource.parseJSON(jo, userId);
         } else {
             tableSource = new DBTableSource();
