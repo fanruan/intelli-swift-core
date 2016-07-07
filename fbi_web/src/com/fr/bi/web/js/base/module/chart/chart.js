c1c4cf302d5186e39042b4852225534c082872bd
git <guy@finereport.com>
2016-07-07 11:42:38 +0800

    bug

--- fbi_web/src/com/fr/bi/web/js/base/module/chart/chart.js
+++ fbi_web/src/com/fr/bi/web/js/base/module/chart/chart.js
@@ -26,7 +26,7 @@ BI.Chart = BI.inherit(BI.Pane, {
                 self.vanCharts.resize();
             }
         }, 0);
-        BI.Resizers.add(this.getName(), function () {
+        BI.Resizers.add(this.getName(), function (e) {
             if (BI.isWindow(e.target) && self.element.is(":visible")) {
                 var newW = self.element.width(), newH = self.element.height();
                 if (newW > 0 && newH > 0 && (width !== newW || height !== newH)) {
