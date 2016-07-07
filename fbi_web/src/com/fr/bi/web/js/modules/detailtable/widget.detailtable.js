8e29cf8b4c0380fd093cc0d9330f59b01ff670ac
git <guy@finereport.com>
2016-07-07 17:54:31 +0800

    add

--- fbi_web/src/com/fr/bi/web/js/modules/detailtable/widget.detailtable.js
+++ fbi_web/src/com/fr/bi/web/js/modules/detailtable/widget.detailtable.js
@@ -133,6 +133,7 @@ BI.DetailTable = BI.inherit(BI.Pane, {
 
                 self.pager.setAllPages(Math.ceil(row / size));
                 self.pager.setValue(vPage);
+                self.table.attr("columnSize", self._getColumnSize([header]));
                 callback(items, [header]);
             } catch (e) {
                 self.errorPane.setErrorInfo("error happens during populate chart: " + e);
@@ -250,7 +251,6 @@ BI.DetailTable = BI.inherit(BI.Pane, {
     populate: function () {
         var self = this;
         this._onPageChange(BICst.TABLE_PAGE_OPERATOR.REFRESH, function (items, header) {
-            self.table.attr("columnSize", self._getColumnSize(header));
             self.table.attr("isNeedFreeze", self._isNeedFreeze());
             self.table.attr("freezeCols", self._getFreezeCols());
             self.table.populate(items, header);
