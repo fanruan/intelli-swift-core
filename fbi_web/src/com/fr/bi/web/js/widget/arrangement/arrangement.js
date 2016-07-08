ad6f66c13b8ce0a2df8da52a18979abc636320ac
git <guy@finereport.com>
2016-07-08 18:43:27 +0800

    add

--- fbi_web/src/com/fr/bi/web/js/widget/arrangement/arrangement.js
+++ fbi_web/src/com/fr/bi/web/js/widget/arrangement/arrangement.js
@@ -2220,7 +2220,7 @@ BI.Arrangement = BI.inherit(BI.Widget, {
 
     setRegionPosition: function (name, position) {
         var self = this, o = this.options;
-        var offset = this._getScrollOffset();
+        //var offset = this._getScrollOffset();
         //position = {
         //    left: position.left + offset.left,
         //    top: position.top + offset.top
