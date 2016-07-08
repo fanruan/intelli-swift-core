0ff3cb7fca53df760ec9d08095823f3e75d2ac28
wuk <kary@fineReport.com>
2016-07-08 09:01:35 +0800

    junit

---
+++ junit_test/code/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Created by wuk on 16/6/23.
+ */
+package code;
+
+public class Main {
+    private String name = "";
+
+    public Main(String _name){
+        name = _name;
+    }
+    public String info(){
+        return name;
+    }
+}
+
