2bcc9a2605845a81902fa2653ed40087cb80d68a
pony <pony@finereport.com>
2016-07-11 16:57:33 +0800

    bug

--- fbi_analysis_etl/src/com/fr/bi/etl/analysis/manager/SingleUserETLTableCubeManager.java
+++ fbi_analysis_etl/src/com/fr/bi/etl/analysis/manager/SingleUserETLTableCubeManager.java
@@ -79,6 +79,7 @@ public class SingleUserETLTableCubeManager implements Release {
 								long version = 0;
 								if (!tq.isEmpty()){
 									version = tq.get().getTableIndex().getTableVersion(new IndexKey(StringUtils.EMPTY));
+									tq.releaseObject();
 								}
 								if(data.check(version)){
 									return;
@@ -112,6 +113,7 @@ public class SingleUserETLTableCubeManager implements Release {
             return false;
         } else {
             long version = tq.get().getTableIndex().getTableVersion(new IndexKey(StringUtils.EMPTY));
+            tq.releaseObject();
             return new UserETLUpdateTask(source).check(version);
         }
     }
