11710422610b8319dcc7a4341e63aad0c9c77bb3
git <guy@finereport.com>
2016-07-11 10:26:46 +0800

    优化

--- fbi_web/src/com/fr/bi/web/js/data/source/source.js
+++ fbi_web/src/com/fr/bi/web/js/data/source/source.js
@@ -1,13 +1,3 @@
 Data.Source = BISource = {
 
-    getTableById: function (tId) {
-        return Pool.source[tId];
-    },
-
-    addTable: function (table) {
-        if (BI.isNull(table) || BI.isNull(table.id)) {
-            return;
-        }
-        Pool.source[table.id] = table;
-    }
 };
