8cc6c53199f1bf0e7b7e95096c7ab583f83c60fe
Roy <roy@RoydeMacBook-Pro.local>
2016-07-11 20:07:29 +0800

    cube路径空格处理

--- fbi_cube_base/src/com/finebi/cube/location/BICubeLocation.java
+++ fbi_cube_base/src/com/finebi/cube/location/BICubeLocation.java
@@ -9,6 +9,7 @@ import com.finebi.cube.data.output.ICubeWriterBuilder;
 import com.fr.bi.stable.utils.program.BINonValueUtils;
 import com.fr.bi.stable.utils.program.BIStringUtils;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -24,9 +25,8 @@ public class BICubeLocation implements ICubeResourceLocation, Cloneable {
 
     public BICubeLocation(String baseLocation, String childLocation) throws URISyntaxException {
         BINonValueUtils.checkNull(baseLocation);
-//        File file = new File(attachFirstSlash(attachLastSlash(replaceSlash(baseLocation))));
-//        this.baseLocation = file.toURI();
-        this.baseLocation = new URI(attachFirstSlash(attachLastSlash(replaceSlash(baseLocation))));
+        File file = new File(attachFirstSlash(attachLastSlash(replaceSlash(baseLocation))));
+        this.baseLocation = new URI(attachLastSlash(file.toURI().getRawPath()));
         if (childLocation != null) {
             this.childLocation = new URI(BIStringUtils.cutStartSlash(replaceSlash(childLocation)));
         } else {
