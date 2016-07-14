3be89aa6eef0f56ed8f875aab2c0b5b6fbbd3ef7
young <young@finereport.com>
2016-07-14 10:20:43 +0800

    指标样式 添加条件中的数量级

--- fbi_web/src/com/fr/bi/web/js/modules/targetstyle/conditionitem/item.targetstylecondition.js
+++ fbi_web/src/com/fr/bi/web/js/modules/targetstyle/conditionitem/item.targetstylecondition.js
@@ -2,13 +2,19 @@
  * Created by Young's on 2016/3/23.
  */
 BI.TargetStyleConditionItem = BI.inherit(BI.Widget, {
-    _defaultConfig: function(){
+
+    _constant: {
+        BUTTON_WIDTH: 25,
+        BUTTON_HEIGHT: 25
+    },
+
+    _defaultConfig: function () {
         return BI.extend(BI.TargetStyleConditionItem.superclass._defaultConfig.apply(this, arguments), {
             baseCls: "bi-target-style-condition-item"
         })
     },
 
-    _init: function(){
+    _init: function () {
         BI.TargetStyleConditionItem.superclass._init.apply(this, arguments);
         var self = this, o = this.options;
         var range = o.range, color = o.color;
@@ -21,14 +27,15 @@ BI.TargetStyleConditionItem = BI.inherit(BI.Widget, {
             closemax: range.closemax
         });
 
-        this.numberRange.on(BI.NumericalInterval.EVENT_CHANGE , function () {
+        this.numberRange.on(BI.NumericalInterval.EVENT_CHANGE, function () {
             self.fireEvent(BI.TargetStyleConditionItem.EVENT_CHANGE);
         });
+        this.setNumLevel(o.numLevel);
 
         this.colorChooser = BI.createWidget({
             type: "bi.color_chooser",
-            width: 30,
-            height: 30
+            width: this._constant.BUTTON_WIDTH,
+            height: this._constant.BUTTON_HEIGHT
         });
 
         this.colorChooser.setValue(color);
@@ -36,8 +43,9 @@ BI.TargetStyleConditionItem = BI.inherit(BI.Widget, {
         this.deleteIcon = BI.createWidget({
             type: "bi.icon_button",
             cls: "data-link-remove-font",
-            width: 30,
-            height: 30,
+            width: this._constant.BUTTON_WIDTH,
+            height: this._constant.BUTTON_HEIGHT,
+            title: BI.i18nText("BI-Remove"),
             handler: function () {
                 o.onRemoveCondition(o.cid);
             }
@@ -55,11 +63,31 @@ BI.TargetStyleConditionItem = BI.inherit(BI.Widget, {
             type: "bi.left",
             element: this.element,
             items: [this.numberRange, this.colorChooser, this.deleteIcon],
-            hgap: 5
+            hgap: 10
         })
     },
 
-    getValue: function(){
+    _getNumLevelText: function (numLevel) {
+        var numTip = "";
+        BI.some(BICst.TARGET_STYLE_LEVEL, function (i, level) {
+            if (level.value === numLevel) {
+                numTip = level.text;
+                return true;
+            }
+        });
+        return numTip;
+    },
+
+    setNumLevel: function (numLevel) {
+        if (numLevel === BICst.TARGET_STYLE.NUM_LEVEL.NORMAL) {
+            this.numberRange.hideNumTip();
+            return;
+        }
+        this.numberRange.setNumTip(this._getNumLevelText(numLevel));
+        this.numberRange.showNumTip();
+    },
+
+    getValue: function () {
         return {
             range: this.numberRange.getValue(),
             color: this.colorChooser.getValue(),
@@ -67,7 +95,7 @@ BI.TargetStyleConditionItem = BI.inherit(BI.Widget, {
         }
     },
 
-    setValue: function(v){
+    setValue: function (v) {
         this.numberRange.setValue(v.range);
         this.colorChooser.setValue(v.color);
     },
