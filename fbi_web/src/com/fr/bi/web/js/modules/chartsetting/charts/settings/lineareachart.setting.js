57a1c9568877b7442949c8a0cfdccadcdf7601de
windy <1374721899@qq.com>
2016-07-19 13:39:24 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/lineareachart.setting.js
+++ fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/lineareachart.setting.js
@@ -380,16 +380,16 @@ BI.LineAreaChartSetting = BI.inherit(BI.Widget, {
         });
 
         //空值连续nullContinue,之前去掉了，现在又要加回来
-        this.nullContinue = BI.createWidget({
-            type: "bi.multi_select_item",
-            value: BI.i18nText("BI-Null_Continue"),
-            invisible: BI.Utils.getWidgetTypeByID(o.wId) === BICst.WIDGET.COMBINE_CHART,
-            width: 115
-        });
-
-        this.nullContinue.on(BI.Controller.EVENT_CHANGE, function(){
-            self.fireEvent(BI.LineAreaChartSetting.EVENT_CHANGE);
-        });
+        //this.nullContinue = BI.createWidget({
+        //    type: "bi.multi_select_item",
+        //    value: BI.i18nText("BI-Null_Continue"),
+        //    invisible: BI.Utils.getWidgetTypeByID(o.wId) === BICst.WIDGET.COMBINE_CHART,
+        //    width: 115
+        //});
+        //
+        //this.nullContinue.on(BI.Controller.EVENT_CHANGE, function(){
+        //    self.fireEvent(BI.LineAreaChartSetting.EVENT_CHANGE);
+        //});
 
         var showElement = BI.createWidget({
             type: "bi.horizontal_adapt",
@@ -424,9 +424,6 @@ BI.LineAreaChartSetting = BI.inherit(BI.Widget, {
                 }, {
                     type: "bi.center_adapt",
                     items: [this.showZoom]
-                }, {
-                    type: "bi.center_adapt",
-                    items: [this.nullContinue]
                 }], {
                     height: this.constant.SINGLE_LINE_HEIGHT
                 }),
@@ -666,7 +663,6 @@ BI.LineAreaChartSetting = BI.inherit(BI.Widget, {
         this.showDataTable.setSelected(BI.Utils.getWSShowDataTableByID(wId));
         this.gridLine.setSelected(BI.Utils.getWSShowGridLineByID(wId));
         this.showZoom.setSelected(BI.Utils.getWSShowZoomByID(wId));
-        this.nullContinue.setSelected(BI.Utils.getWSNullContinueByID(wId));
 
         this.isShowTitleLY.isSelected() ? this.editTitleLY.setVisible(true) : this.editTitleLY.setVisible(false);
         this.isShowTitleRY.isSelected() ? this.editTitleRY.setVisible(true) : this.editTitleRY.setVisible(false);
@@ -698,8 +694,7 @@ BI.LineAreaChartSetting = BI.inherit(BI.Widget, {
             show_data_label: this.showDataLabel.isSelected(),
             show_data_table: this.showDataTable.isSelected(),
             show_grid_line: this.gridLine.isSelected(),
-            show_zoom: this.showZoom.isSelected(),
-            null_continue: this.nullContinue.isSelected()
+            show_zoom: this.showZoom.isSelected()
         }
     },
 
@@ -728,7 +723,6 @@ BI.LineAreaChartSetting = BI.inherit(BI.Widget, {
         this.showDataTable.setSelected(v.show_data_table);
         this.gridLine.setSelected(v.show_grid_line);
         this.showZoom.setSelected(v.show_zoom);
-        this.nullContinue.setSelected(v.null_continue);
     }
 });
 BI.LineAreaChartSetting.EVENT_CHANGE = "EVENT_CHANGE";
