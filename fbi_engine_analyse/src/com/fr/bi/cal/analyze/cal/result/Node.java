1d605fdf369fe5c60bf5c5016ac4cdecbd7adfcb
young <young@finereport.com>
2016-07-26 21:24:44 +0800

    丢精度的坑导致的其他问题。。。

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/result/Node.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/result/Node.java
@@ -1311,7 +1311,8 @@ public class Node extends BIField implements BINode {
         }
         JSONArray summary = new JSONArray();
         for (int i = 0; i < keys.length; i++) {
-            summary.put(GeneralUtils.objectToString(this.getSummaryValue(keys[i])));
+            String value = GeneralUtils.objectToString(this.getSummaryValue(keys[i]));
+            summary.put(GeneralUtils.objectToNumber(value));
         }
         jo.put("s", summary);
         jo.put("x", getTotalLengthWithSummary());
