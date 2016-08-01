1e7180c81c759c61e31d563ebe60333027b181ec
windy <1374721899@qq.com>
2016-08-01 13:47:05 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/tablechartmanager/chartdisplay/chartdisplay.js
+++ fbi_web/src/com/fr/bi/web/js/modules/tablechartmanager/chartdisplay/chartdisplay.js
@@ -300,7 +300,7 @@ BI.ChartDisplay = BI.inherit(BI.Pane, {
                 self.errorPane.setVisible(true);
                 return;
             }
-            //try {
+            try {
                 var dimensionIds = BI.Utils.getAllDimDimensionIDs(o.wId);
                 var lnglat = BI.Utils.getDimensionPositionByID(dimensionIds[0]);
 
@@ -359,10 +359,10 @@ BI.ChartDisplay = BI.inherit(BI.Pane, {
                     lnglat: BI.isNotNull(lnglat) ? lnglat.type : lnglat
                 });
                 selectedTab.populate(data, op, types);
-            //} catch (e) {
-            //    self.errorPane.setErrorInfo("error happens during populate chart: " + e);
-            //    self.errorPane.setVisible(true);
-            //}
+            } catch (e) {
+                self.errorPane.setErrorInfo("error happens during populate chart: " + e);
+                self.errorPane.setVisible(true);
+            }
         });
     },
 
