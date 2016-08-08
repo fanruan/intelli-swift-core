1f93b043c4417cdfc162a91ba7e93cb04c4214c2
young <young@finereport.com>
2016-08-08 10:30:27 +0800

    搜索问题

--- fbi_web/src/com/fr/bi/web/js/modules/permissionmanage/addrole/searcher.batchaddrole.js
+++ fbi_web/src/com/fr/bi/web/js/modules/permissionmanage/addrole/searcher.batchaddrole.js
@@ -135,7 +135,7 @@ BI.BatchAddRoleSearcher = BI.inherit(BI.Widget, {
         });
         var items = [];
         BI.each(sortedRoles, function(i, role) {
-            var roleName = role.text || (role.department_name  + ", " + role.post_name);
+            var roleName = role.text || (role.department_name  + "," + role.post_name);
             items.push({
                 type: "bi.text_button",
                 cls: "role-item",
