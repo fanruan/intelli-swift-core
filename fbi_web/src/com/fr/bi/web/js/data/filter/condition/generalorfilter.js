182d1365e94acf5c53e045117e1f2d1cde7a0f86
windy <1374721899@qq.com>
2016-08-22 15:31:27 +0800

    添加且或空条件过滤

---
+++ fbi_web/src/com/fr/bi/web/js/data/filter/condition/generalorfilter.js
@@ -0,0 +1,23 @@
+;
+!(function(){
+    BI.GeneralOrFilter = function(filter){
+        if(BI.has(filter, "filter_value")){
+            var filter_value = filter.filter_value || [];
+            this.childs = BI.map(filter_value, function(idx, fv){
+                return BI.FilterFactory.parseFilter(fv);
+            })
+        }
+    };
+    BI.GeneralOrFilter.prototype = {
+        constructor: BI.GeneralOrFilter,
+
+        isQualified: function(value, array){
+            var res = BI.find(this.childs, function(idx, child){
+                if(BI.isNotNull(child) && !child.isQualified(value, array)){
+                    return true;
+                }
+            });
+            return BI.isNull(res);
+        }
+    }
+})();
--- fbi_web/src/com/fr/bi/web/js/data/filter/filter.factory.js
+++ fbi_web/src/com/fr/bi/web/js/data/filter/filter.factory.js
@@ -1,66 +1,27 @@
-BI.FilterValueFactory = {
+BI.FilterFactory = {
 
-    parseFilterValue: function(filter){
-        var filterValue = null;
-        if (BI.has(filter, "filter_type")) {
-            var type = filter.filter_type;
-            switch (type) {
-                case BICst.DIMENSION_FILTER_NUMBER.BELONG_VALUE:
-                    filterValue = new BI.NumberInRangeFilterValue(filter.filter_value);
+    parseFilter: function(filter){
+        var filter = null;
+        if(BI.has(filter, "filter_type")){
+            switch (filter.filter_type){
+                case BICst.FILTER_TYPE.AND:
+                    filter = new GeneralANDDimensionFilter();
                     break;
-                case BICst.DIMENSION_FILTER_NUMBER.NOT_BELONG_VALUE:
-                    filterValue = new BI.NumberNotInRangeFilterValue(filter.filter_value);
+                case BICst.FILTER_TYPE.OR:
+                    filter = new GeneralORDimensionFilter();
                     break;
-                case BICst.DIMENSION_FILTER_NUMBER.MORE_THAN_AVG:
-                    filterValue = new BI.NumberLargeThanOrEqualFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_NUMBER.LESS_THAN_AVG:
-                    filterValue = new BI.NumberLessThanFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_NUMBER.IS_NULL:
-                    filterValue = new BI.NumberNullFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_NUMBER.NOT_NULL:
-                    filterValue = new BI.NumberNotNullFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_NUMBER.TOP_N:
-                    filterValue = new BI.NumberKthFilterValue(filter.filter_value);
-                    break;
-                case BICst.TARGET_FILTER_NUMBER.EQUAL_TO:
-                    filterValue = new BI.NumberEqualFilterValue(filter.filter_value);
-                    break;
-                case BICst.TARGET_FILTER_NUMBER.NOT_EQUAL_TO:
-                    filterValue = new BI.NumberNotEqualFilterValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.BELONG_VALUE:
-                    filterValue = new BI.StringInFilterValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.NOT_BELONG_VALUE:
-                    filterValue = new BI.StringNotInFilterValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.CONTAIN:
-                    filterValue = new BI.StringContainValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.NOT_CONTAIN:
-                    filterValue = new BI.StringNotContainValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.IS_NULL:
-                    filterValue = new BI.StringNullFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.NOT_NULL:
-                    filterValue = new BI.StringNotNullFilterValue();
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.BEGIN_WITH:
-                    filterValue = new BI.StringStartWithFilterValue(filter.filter_value);
-                    break;
-                case BICst.DIMENSION_FILTER_STRING.END_WITH:
-                    filterValue = new BI.StringEndWithFilterValue(filter.filter_value);
+                case BICst.FILTER_TYPE.EMPTY_CONDITION:
+                    filter = new EmptyDimensionTargetValueFilter();
                     break;
                 default:
+                    filter = new DimensionTargetValueFilter();
                     break;
             }
         }
-        return filterValue;
+        if (filter != null){
+            filter.parseJSON(jo, userId);
+        }
+        return filter;
     }
 
 };
