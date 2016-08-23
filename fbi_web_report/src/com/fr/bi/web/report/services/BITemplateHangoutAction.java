76fd58b22afb59cba275dad139f7a1dc825b72e1
young <young@finereport.com>
2016-08-23 12:04:35 +0800

    bug

--- fbi_web_report/src/com/fr/bi/web/report/services/BITemplateHangoutAction.java
+++ fbi_web_report/src/com/fr/bi/web/report/services/BITemplateHangoutAction.java
@@ -2,7 +2,9 @@ package com.fr.bi.web.report.services;
 
 import com.fr.bi.fs.BIDAOUtils;
 import com.fr.bi.fs.BIReportNode;
+import com.fr.bi.stable.utils.code.BILogger;
 import com.fr.fs.web.service.ServiceUtils;
+import com.fr.json.JSONObject;
 import com.fr.web.core.ActionNoSessionCMD;
 import com.fr.web.utils.WebUtils;
 
@@ -19,9 +21,16 @@ public class BITemplateHangoutAction extends ActionNoSessionCMD {
         long userId = ServiceUtils.getCurrentUserID(req);
         String id = WebUtils.getHTTPRequestParameter(req, "id");
         String status = WebUtils.getHTTPRequestParameter(req, "status");
-        BIReportNode reportNode = BIDAOUtils.findByID(Long.parseLong(id), userId);
-        reportNode.setStatus(Integer.parseInt(status));
-        BIDAOUtils.saveOrUpDate(reportNode, userId);
+        JSONObject jo = new JSONObject();
+        try {
+            BIReportNode reportNode = BIDAOUtils.findByID(Long.parseLong(id), userId);
+            reportNode.setStatus(Integer.parseInt(status));
+            BIDAOUtils.saveOrUpDate(reportNode, userId);
+            jo.put("result", true);
+        } catch (Exception e) {
+            BILogger.getLogger().error(e.getMessage());
+        }
+        WebUtils.printAsJSON(res, jo);
     }
 
     @Override
