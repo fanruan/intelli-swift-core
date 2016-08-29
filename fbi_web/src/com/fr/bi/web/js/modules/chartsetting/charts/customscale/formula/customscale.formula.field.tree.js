d5596d07854080c5602d57238340d48571243557
imp <imp@finereport.com>
2016-08-29 19:03:25 +0800

    custom scale

--- fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/customscale/formula/customscale.formula.field.tree.js
+++ fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/customscale/formula/customscale.formula.field.tree.js
@@ -6,113 +6,107 @@ BI.CustomScaleFormulaFieldTree = BI.inherit(BI.Widget, {
     _defaultConfig: function () {
         return BI.extend(BI.CustomScaleFormulaFieldTree.superclass._defaultConfig.apply(this, arguments), {
             baseCls: "custom-scale-formula-field-tree",
-            chooseType: 0
+            chooseType: 0,
+            items: {}
         })
     },
 
     _init: function () {
         BI.CustomScaleFormulaFieldTree.superclass._init.apply(this, arguments);
-        this.populate()
+        this.populate(this.options.items)
     },
 
-    _createOneAxis: function (nodes, view, axisOne) {
+    _createOneAxis: function (nodes, targets, axisOne) {
         var self = this;
-        BI.each(view[BICst.REGION.TARGET1], function (idx, item) {
-            var id = BICst.REGION.TARGET1 + idx;
+        BI.each(targets[BICst.REGION.TARGET1], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisOne + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item))
         });
 
         return nodes
     },
 
-    _createTwoAxis: function (nodes, view, axisOne, axisTwo) {
+    _createTwoAxis: function (nodes, targets, axisOne, axisTwo) {
         var self = this;
-        BI.each(view[BICst.REGION.TARGET1], function (idx, item) {
-            var id = BICst.REGION.TARGET1 + idx;
+        BI.each(targets[BICst.REGION.TARGET1], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisOne + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item))
         });
 
-        BI.each(view[BICst.REGION.TARGET2], function (idx, item) {
-            var id = BICst.REGION.TARGET2 + idx;
+        BI.each(targets[BICst.REGION.TARGET2], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisTwo + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item))
         });
-        debugger;
         return nodes
     },
 
-    _createThreeAxis: function (nodes, view, axisOne, axisTwo, axisThree) {
+    _createThreeAxis: function (nodes, targets, axisOne, axisTwo, axisThree) {
         var self = this;
-        BI.each(view[BICst.REGION.TARGET1], function (idx, item) {
-            var id = BICst.REGION.TARGET1 + idx;
+        BI.each(targets[BICst.REGION.TARGET1], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisOne + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item))
         });
 
-        BI.each(view[BICst.REGION.TARGET2], function (idx, item) {
-            var id = BICst.REGION.TARGET2 + idx;
+        BI.each(targets[BICst.REGION.TARGET2], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisTwo + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item))
         });
 
-        BI.each(view[BICst.REGION.TARGET3], function (idx, item) {
-            var id = BICst.REGION.TARGET3 + idx;
+        BI.each(targets[BICst.REGION.TARGET3], function (idx, item) {
             nodes.push({
                 type: "bi.custom_scale_formula_field_tree_mid_leaf",
-                id: id,
+                id: item,
                 pId: nodes[0].id,
                 text: axisThree + (idx + 1),
                 value: item
             });
-            nodes = BI.concat(nodes, self._createLeaves(id))
+            nodes = BI.concat(nodes, self._createLeaves(item, item))
         });
 
         return nodes
     },
 
-    _createLeaves: function (v) {
+    _createLeaves: function (pId) {
         var leaves = [];
         var texts = [BI.i18nText("BI-DOT_MAX"), BI.i18nText("BI-DOT_MIN"), BI.i18nText("BI-DOT_AVERAGE")];
 
         BI.each(texts, function (idx, text) {
             leaves.push({
                 type: "bi.tree_text_leaf_item",
-                pId: v,
-                id: BI.UUID(),
+                pId: pId,
+                id: pId + text,
                 text: text,
-                value: v + text,
+                value: pId + text,
                 lgap: 48
             })
         });
@@ -120,16 +114,8 @@ BI.CustomScaleFormulaFieldTree = BI.inherit(BI.Widget, {
         return leaves
     },
 
-    populate: function () {
-        var self = this, o = this.options;
-        // var type = BI.Utils.getWidgetTypeByID(o.wId);
-        // var view = BI.Utils.getWidgetViewByID(o.wId);
-
-        var type = 8;
-        var view = {
-            30000: [1, 2, 3],
-            40000: [5, 10, 7]
-        };
+    populate: function (usedTargets) {
+        var self = this;
 
         this.empty();
 
@@ -144,14 +130,14 @@ BI.CustomScaleFormulaFieldTree = BI.inherit(BI.Widget, {
             }
         ];
 
-        switch (type) {
+        switch (usedTargets.type) {
             case BICst.WIDGET.AXIS:
             case BICst.WIDGET.ACCUMULATE_AXIS:
             case BICst.WIDGET.LINE:
             case BICst.WIDGET.AREA:
             case BICst.WIDGET.ACCUMULATE_AREA:
             case BICst.WIDGET.COMBINE_CHART:
-                nodes = this._createTwoAxis(nodes, view, BI.i18nText("BI-Left_Value_Axis"), BI.i18nText("BI-Right_Value_Axis"));
+                nodes = this._createTwoAxis(nodes, usedTargets, BI.i18nText("BI-Left_Value_Axis"), BI.i18nText("BI-Right_Value_Axis"));
                 break;
             case BICst.WIDGET.BAR:
             case BICst.WIDGET.PERCENT_ACCUMULATE_AXIS:
@@ -160,23 +146,23 @@ BI.CustomScaleFormulaFieldTree = BI.inherit(BI.Widget, {
             case BICst.WIDGET.FALL_AXIS:
             case BICst.WIDGET.RADAR:
             case BICst.WIDGET.ACCUMULATE_RADAR:
-                nodes = this._createOneAxis(nodes, view, BI.i18nText("BI-Value_Axis"));
+                nodes = this._createOneAxis(nodes, usedTargets, BI.i18nText("BI-Value_Axis"));
                 break;
             case BICst.WIDGET.COMPARE_AXIS:
             case BICst.WIDGET.COMPARE_AREA:
-                nodes = this._createTwoAxis(nodes, view, BI.i18nText("BI-Positive_Value_Axis"), BI.i18nText("BI-Negative_Value_Axis"));
+                nodes = this._createTwoAxis(nodes, usedTargets, BI.i18nText("BI-Positive_Value_Axis"), BI.i18nText("BI-Negative_Value_Axis"));
                 break;
             case BICst.WIDGET.COMPARE_BAR:
-                nodes = this._createTwoAxis(nodes, view, BI.i18nText("BI-Value_Axis_One"), BI.i18nText("BI-Value_Axis_Two"));
+                nodes = this._createTwoAxis(nodes, usedTargets, BI.i18nText("BI-Value_Axis_One"), BI.i18nText("BI-Value_Axis_Two"));
                 break;
             case BICst.WIDGET.RANGE_AREA:
-                nodes = this._createTwoAxis(nodes, view, BI.i18nText("BI-Low_Value_Axis"), BI.i18nText("BI-High_Value_Axis"));
+                nodes = this._createTwoAxis(nodes, usedTargets, BI.i18nText("BI-Low_Value_Axis"), BI.i18nText("BI-High_Value_Axis"));
                 break;
             case BICst.WIDGET.MULTI_AXIS_COMBINE_CHART:
-                nodes = this._createThreeAxis(nodes, view, BI.i18nText("BI-Left_Value_Axis"), BI.i18nText("BI-Right_Value_Axis_One"), BI.i18nText("BI-Right_Value_Axis_Two"));
+                nodes = this._createThreeAxis(nodes, usedTargets, BI.i18nText("BI-Left_Value_Axis"), BI.i18nText("BI-Right_Value_Axis_One"), BI.i18nText("BI-Right_Value_Axis_Two"));
                 break;
             case BICst.WIDGET.BUBBLE:
-                nodes = this._createTwoAxis(nodes, view, BI.i18nText("BI-Y_Value_Axis"), BI.i18nText("BI-X_Value_Axis"));
+                nodes = this._createTwoAxis(nodes, usedTargets, BI.i18nText("BI-Y_Value_Axis"), BI.i18nText("BI-X_Value_Axis"));
                 break;
         }
 
@@ -200,8 +186,11 @@ BI.CustomScaleFormulaFieldTree = BI.inherit(BI.Widget, {
     },
 
     setValue: function (v) {
-        v = BI.isArray(v) ? v : [v];
-        this.tree.setValue(v)
+        this.fieldTree.setValue(v)
+    },
+
+    getAllLeaves: function () {
+        return this.fieldTree.getAllLeaves()
     }
 });
 BI.CustomScaleFormulaFieldTree.EVENT_CHANGE = "EVENT_CHANGE";
--- fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/customscale/formula/customscale.formula.js
+++ fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/customscale/formula/customscale.formula.js
@@ -5,20 +5,26 @@
 BI.CustomScaleFormula = BI.inherit(BI.Widget, {
     _defaultConfig: function () {
         return BI.extend(BI.CustomScaleFormula.superclass._defaultConfig.apply(this, arguments), {
-            baseCls: "bi-custom-scale-formula"
+            baseCls: "bi-custom-scale-formula",
+            items: {
+                type: 14,
+                30000: [BI.UUID(), BI.UUID()]
+            }
         })
     },
 
     _init: function () {
         BI.CustomScaleFormula.superclass._init.apply(this, arguments);
-        var self = this;
+        var self = this, o = this.options;
+        self.validation = 'valid';
 
         this.formulaTree = BI.createWidget({
             type: "bi.custom_scale_formula_field_tree",
-            cls: "bi-custom-formula-field-pane"
+            cls: "bi-custom-formula-field-pane",
+            items: o.items
         });
 
-        this.formulaTree.on(BI.FormulaFieldTree.EVENT_CHANGE, function () {
+        this.formulaTree.on(BI.CustomScaleFormulaFieldTree.EVENT_CHANGE, function () {
             var v = self.formulaTree.getValue();
             self.formulaEditor.insertField(self.fieldValueTextMap[v[0]]);
         });
@@ -29,13 +35,14 @@ BI.CustomScaleFormula = BI.inherit(BI.Widget, {
             cls: "symbol-group-column"
         });
 
-        this.symbolGroup.on(BI.CustomScaleFormula.EVENT_CHANGE, function (v) {
-            self.formulaTree.insertOperator(v)
+        this.symbolGroup.on(BI.SymbolGroup.EVENT_CHANGE, function (v) {
+            self.formulaEditor.insertOperator(v)
         });
 
         this.formulaEditor = BI.createWidget({
             type: "bi.formula",
-            tipType:"warning"
+            showHint: false,
+            tipType: "warning"
         });
 
         this.formulaEditor.on(BI.FormulaEditor.EVENT_CHANGE, function () {
@@ -101,7 +108,177 @@ BI.CustomScaleFormula = BI.inherit(BI.Widget, {
                 self.formulaEditor.insertField(value);
             }
         });
+
+        this.populate(o.items)
+    },
+
+    _createTargetMap: function (state, items, targetType, text, axis) {
+        var fieldMap = {};
+        if(state){
+            BI.each(items[targetType], function (idx, item) {
+                fieldMap["max(" + axis + (idx + 1) + ")"] = item + text[0];
+                fieldMap["min(" + axis + (idx + 1) + ")"] = item + text[1];
+                fieldMap["average(" + axis + (idx + 1) + ")"] = item + text[2]
+            });
+        }else{
+            BI.each(items[targetType], function (idx, item) {
+                fieldMap[item + text[0]] = "max(" + axis + (idx + 1) + ")";
+                fieldMap[item + text[1]] = "min(" + axis + (idx + 1) + ")";
+                fieldMap[item + text[2]] = "average(" + axis + (idx + 1) + ")"
+            });
+        }
+
+        return fieldMap
+    },
+
+    _createOneTargetMap: function (state, text, items, axisOne) {
+        return this._createTargetMap(state, items, BICst.REGION.TARGET1, text, axisOne)
+    },
+
+    _createTwoTargetsMap: function (state, text, items, axisOne, axisTwo) {
+        var fieldMap1 = this._createTargetMap(state, items, BICst.REGION.TARGET1, text, axisOne);
+        var fieldMap2 = this._createTargetMap(state, items, BICst.REGION.TARGET2, text, axisTwo);
+
+        return BI.extend(fieldMap1, fieldMap2)
+    },
+
+    _createThreeTargetsMap: function (state, text, items, axisOne, axisTwo, axisThree) {
+        var fieldMap1 = this._createTargetMap(state, items, BICst.REGION.TARGET1, text, axisOne);
+        var fieldMap2 = this._createTargetMap(state, items, BICst.REGION.TARGET2, text, axisTwo);
+        var fieldMap3 = this._createTargetMap(state, items, BICst.REGION.TARGET3, text, axisThree);
+
+        return BI.extend(fieldMap1, fieldMap2, fieldMap3)
+    },
+
+    _createFieldTextValueMap: function (items) {
+        var text = [BI.i18nText("BI-DOT_MAX"), BI.i18nText("BI-DOT_MIN"), BI.i18nText("BI-DOT_AVERAGE")];
+
+        switch (items.type) {
+            case BICst.WIDGET.AXIS:
+            case BICst.WIDGET.ACCUMULATE_AXIS:
+            case BICst.WIDGET.LINE:
+            case BICst.WIDGET.AREA:
+            case BICst.WIDGET.ACCUMULATE_AREA:
+            case BICst.WIDGET.COMBINE_CHART:
+                return this._createTwoTargetsMap(true, text, items, BI.i18nText("BI-Left_Value_Axis"),
+                    BI.i18nText("BI-Right_Value_Axis"));
+                break;
+            case BICst.WIDGET.BAR:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AXIS:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AREA:
+            case BICst.WIDGET.ACCUMULATE_BAR:
+            case BICst.WIDGET.FALL_AXIS:
+            case BICst.WIDGET.RADAR:
+            case BICst.WIDGET.ACCUMULATE_RADAR:
+                return this._createOneTargetMap(true, text, items, BI.i18nText("BI-Value_Axis"));
+                break;
+            case BICst.WIDGET.COMPARE_AXIS:
+            case BICst.WIDGET.COMPARE_AREA:
+                return this._createTwoTargetsMap(true, text, items, BI.i18nText("BI-Positive_Value_Axis"),
+                    BI.i18nText("BI-Negative_Value_Axis"));
+                break;
+            case BICst.WIDGET.COMPARE_BAR:
+                return this._createTwoTargetsMap(true, text, items, BI.i18nText("BI-Value_Axis_One"),
+                    BI.i18nText("BI-Value_Axis_Two"));
+                break;
+            case BICst.WIDGET.RANGE_AREA:
+                return this._createTwoTargetsMap(true, text, items, BI.i18nText("BI-Low_Value_Axis"),
+                    BI.i18nText("BI-High_Value_Axis"));
+                break;
+            case BICst.WIDGET.MULTI_AXIS_COMBINE_CHART:
+                return this._createThreeTargetsMap(true, text, items, BI.i18nText("BI-Left_Value_Axis"),
+                    BI.i18nText("BI-Right_Value_Axis_One"), BI.i18nText("BI-Right_Value_Axis_Two"));
+                break;
+            case BICst.WIDGET.BUBBLE:
+                return this._createTwoTargetsMap(true, text, items, BI.i18nText("BI-Y_Value_Axis"),
+                    BI.i18nText("BI-X_Value_Axis"));
+                break;
+        }
+    },
+
+    _createFieldValueTextMap: function (items) {
+        var text = [BI.i18nText("BI-DOT_MAX"), BI.i18nText("BI-DOT_MIN"), BI.i18nText("BI-DOT_AVERAGE")];
+
+        switch (items.type) {
+            case BICst.WIDGET.AXIS:
+            case BICst.WIDGET.ACCUMULATE_AXIS:
+            case BICst.WIDGET.LINE:
+            case BICst.WIDGET.AREA:
+            case BICst.WIDGET.ACCUMULATE_AREA:
+            case BICst.WIDGET.COMBINE_CHART:
+                return this._createTwoTargetsMap(false, text, items, BI.i18nText("BI-Left_Value_Axis"),
+                    BI.i18nText("BI-Right_Value_Axis"));
+                break;
+            case BICst.WIDGET.BAR:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AXIS:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AREA:
+            case BICst.WIDGET.ACCUMULATE_BAR:
+            case BICst.WIDGET.FALL_AXIS:
+            case BICst.WIDGET.RADAR:
+            case BICst.WIDGET.ACCUMULATE_RADAR:
+                return this._createOneTargetMap(false, text, items, BI.i18nText("BI-Value_Axis"));
+                break;
+            case BICst.WIDGET.COMPARE_AXIS:
+            case BICst.WIDGET.COMPARE_AREA:
+                return this._createTwoTargetsMap(false, text, items, BI.i18nText("BI-Positive_Value_Axis"),
+                    BI.i18nText("BI-Negative_Value_Axis"));
+                break;
+            case BICst.WIDGET.COMPARE_BAR:
+                return this._createTwoTargetsMap(false, text, items, BI.i18nText("BI-Value_Axis_One"),
+                    BI.i18nText("BI-Value_Axis_Two"));
+                break;
+            case BICst.WIDGET.RANGE_AREA:
+                return this._createTwoTargetsMap(false, text, items, BI.i18nText("BI-Low_Value_Axis"),
+                    BI.i18nText("BI-High_Value_Axis"));
+                break;
+            case BICst.WIDGET.MULTI_AXIS_COMBINE_CHART:
+                return this._createThreeTargetsMap(false, text, items, BI.i18nText("BI-Left_Value_Axis"),
+                    BI.i18nText("BI-Right_Value_Axis_One"), BI.i18nText("BI-Right_Value_Axis_Two"));
+                break;
+            case BICst.WIDGET.BUBBLE:
+                return this._createTwoTargetsMap(false, text, items, BI.i18nText("BI-Y_Value_Axis"),
+                    BI.i18nText("BI-X_Value_Axis"));
+                break;
+        }
+    },
+
+    _bindDragEvent: function () {
+        var self = this;
+        BI.each(self.formulaTree.getAllLeaves(), function (i, node) {
+            node.element.draggable({
+                cursorAt: {top: 5, left: 5},
+                helper: function () {
+                    var hint = BI.createWidget({
+                        type: "bi.helper",
+                        value: node.getValue(),
+                        text: self.fieldValueTextMap[node.getValue()]
+                    });
+                    BI.createWidget({
+                        element: self.element,
+                        type: "bi.default",
+                        items: [hint]
+                    });
+                    hint.element.attr({text: self.fieldValueTextMap[node.getValue()]});
+                    return hint.element;
+
+                }
+            })
+        });
+    },
+
+    getValue: function () {
+
+    },
+
+    populate: function (items) {
+        this.options.items = items;
+        this.fieldTextValueMap = this._createFieldTextValueMap(items);
+        this.fieldValueTextMap = this._createFieldValueTextMap(items);
+        this.formulaEditor.setFieldTextValueMap(this.fieldTextValueMap);
+        this.formulaTree.populate(items);
+        this._bindDragEvent();
     }
+
 });
 BI.CustomScaleFormula.EVENT_CHANGE = 'EVENT_CHANGE';
 $.shortcut("bi.custom_scale_formula", BI.CustomScaleFormula);
