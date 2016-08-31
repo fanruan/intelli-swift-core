bad3007789496c9b1326f3d885d31a714ee2bdcf
young <young@finereport.com>
2016-08-31 19:35:58 +0800

    1、etl结束获取字段 2、读关联转义的时候遇到数据连接失败的继续

--- fbi_web/src/com/fr/bi/web/js/modules/onepackage/etl/statistic/widget.statistic.js
+++ fbi_web/src/com/fr/bi/web/js/modules/onepackage/etl/statistic/widget.statistic.js
@@ -175,7 +175,21 @@ BI.GroupStatistic = BI.inherit(BI.Widget, {
 
         this.save.on(BI.Button.EVENT_CHANGE,function(){
             self._checkMasker();
-            self.fireEvent(BI.GroupStatistic.EVENT_SAVE, self.model.getValue());
+            var data = self.model.getValue();
+            var mask = BI.createWidget({
+                type: "bi.loading_mask",
+                masker: self.element,
+                text: BI.i18nText("BI-Loading")
+            });
+            BI.Utils.getTablesDetailInfoByTables([data], function (sourceTables) {
+                var table = sourceTables[0];
+                if(BI.isNotNull(table)) {
+                    data.fields = table.fields;
+                }
+                self.fireEvent(BI.GroupStatistic.EVENT_SAVE, data);
+            }, function() {
+                mask.destroy();
+            });
         });
 
         return BI.createWidget({
