fe20bd33b4d45c7a8c60a9e4d2b24801bdd36ed9
fay <lfhlifenghao@gmail.com>
2016-09-02 17:58:43 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/datalabel/condition/bubble/item.multifield.js
+++ fbi_web/src/com/fr/bi/web/js/modules/datalabel/condition/bubble/item.multifield.js
@@ -7,9 +7,9 @@ BI.BubbleMultiFieldFilterItem = BI.inherit(BI.AbstractDataLabelFilterItem, {
         CONTAINER_HEIGHT: 40,
         BUTTON_HEIGHT: 30,
         COMBO_WIDTH: 120,
-        FIELD_NAME_BUTTON_WIDTH: 53,
+        FIELD_NAME_BUTTON_WIDTH: 57,
         TEXT_BUTTON_H_GAP: 10,
-        INPUT_WIDTH: 230,
+        INPUT_WIDTH: 240,
         LABEL_WIDTH: 30
     },
 
