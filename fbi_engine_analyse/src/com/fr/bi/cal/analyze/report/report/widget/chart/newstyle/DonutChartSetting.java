87bc282c2a5184ad655823944a003a37d7952c7c
windy <1374721899@qq.com>
2016-09-12 15:24:58 +0800

    图表样式放到后台

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/newstyle/DonutChartSetting.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/newstyle/DonutChartSetting.java
@@ -8,18 +8,38 @@ import com.fr.json.JSONObject;
  * Created by User on 2016/8/31.
  */
 public class DonutChartSetting extends BIAbstractChartSetting {
-    @Override
-    public JSONObject formatItems(JSONArray data, JSONArray types) throws JSONException {
-        return super.formatItems(data, types);
-    }
 
-    @Override
-    public JSONObject getConvertedDataAndSettings(JSONArray data, JSONArray types, JSONObject options) throws JSONException {
-        return null;
+    private JSONObject config;
+
+    public DonutChartSetting() {
+        this.config = new JSONObject();
     }
 
     @Override
     public JSONObject formatConfig(JSONObject options, JSONArray data) throws JSONException {
-        return null;
+        JSONObject plotOptions = this.config.getJSONObject("plotOptions");
+        this.config.put("color", options.getJSONArray("chart_color"));
+        this.config.put("style", this.formatChartStyle(options.getInt("chart_style")));
+        this.formatChartLegend(options.optInt("chart_legend"));
+        plotOptions.optJSONObject("dataLabels").put("enabled", options.optBoolean("show_data_label"));
+        plotOptions.put("innerRadius", "50.0%");
+        this.config.put("chartType", "pie");
+        plotOptions.getJSONObject("dataLabels").put("align", "outside").put("connectorWidth", "outside").getJSONObject("formatter").put("identifier", "${VALUE}${PERCENT}");
+        for(int i = 0; i < data.length(); i++){
+            JSONArray da = data.getJSONObject(i).getJSONArray("data");
+            for(int j = 0; j < da.length(); j++){
+                JSONObject num = da.getJSONObject(j);
+                num.put("y", this.formatXYDataWithMagnify(num.getDouble("y"), 1));
+            }
+        }
+        return this.config.put("series", data);
+    }
+
+    @Override
+    public JSONObject getConvertedDataAndSettings(JSONArray data, JSONArray types, JSONObject options) throws JSONException {
+        JSONObject configAndData = this.formatItems(data, types, options);
+        this.config = configAndData.getJSONObject("config");
+        JSONArray items = configAndData.getJSONArray("result");
+        return this.formatConfig(options, items);
     }
 }
--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/newstyle/RadarChartSetting.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/newstyle/RadarChartSetting.java
@@ -1,25 +1,12 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.newstyle;
 
-import com.fr.json.JSONArray;
 import com.fr.json.JSONException;
-import com.fr.json.JSONObject;
 
 /**
  * Created by User on 2016/8/31.
  */
-public class RadarChartSetting extends BIAbstractChartSetting {
-    @Override
-    public JSONObject formatItems(JSONArray data, JSONArray types) throws JSONException {
-        return super.formatItems(data, types);
-    }
+public class RadarChartSetting extends BIAbstractRadarChartSetting {
 
-    @Override
-    public JSONObject getConvertedDataAndSettings(JSONArray data, JSONArray types, JSONObject options) throws JSONException {
-        return null;
-    }
-
-    @Override
-    public JSONObject formatConfig(JSONObject options, JSONArray data) throws JSONException {
-        return null;
+    public RadarChartSetting() throws JSONException {
     }
 }
