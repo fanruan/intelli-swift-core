03d0a88d19ad457fc58eb97f70ba53a94331da5d
kary <kary@fineReport.com>
2016-09-12 09:19:24 +0800

    no message

--- fbi_base/src/com/finebi/cube/retry/RetryPolicy.java
+++
@@ -1,8 +0,0 @@
-package com.finebi.cube.retry;
-
-/**
- * Created by Sean on 2015/5/7.
- */
-public interface RetryPolicy {
-    boolean allowRetry(int currentCount, long elapsedTimeMs, Sleeper sleeper);
-}
