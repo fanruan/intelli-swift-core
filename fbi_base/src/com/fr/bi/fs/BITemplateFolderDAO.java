1508675e5530390ac21ddd9f27413a80fa095a35
sunmondong <sunmondong@finereport.com>
2016-09-21 10:25:27 +0800

    添加表迁移方法测试

--- fbi_base/src/com/fr/bi/fs/BITemplateFolderDAO.java
+++ fbi_base/src/com/fr/bi/fs/BITemplateFolderDAO.java
@@ -1,5 +1,6 @@
 package com.fr.bi.fs;
 
+import java.util.BitSet;
 import java.util.List;
 
 public interface  BITemplateFolderDAO {
@@ -19,5 +20,6 @@ public interface  BITemplateFolderDAO {
     public List<BIReportNode> findTemplateByParentId (String parentId) throws Exception;
 
     public List<BITemplateFolderNode> findByName(String name) throws Exception;
+
 
 }
