293f3196f97cf5ae82a09bdc07541122b496b1bd
windy <1374721899@qq.com>
2016-09-28 11:53:25 +0800

    tracker bug图交互

--- fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/percentchart.setting.js
+++ fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/percentchart.setting.js
@@ -60,7 +60,7 @@ BI.PercentChartsSetting = BI.inherit(BI.AbstractChartSetting, {
                 cls: "detail-style",
                 items: BI.createItems([{
                     type: "bi.label",
-                    text: BI.i18nText("BI-Chart_Color"),
+                    text: BI.i18nText("BI-Color_Setting"),
                     cls: "attr-names"
                 }, {
                     el: {
