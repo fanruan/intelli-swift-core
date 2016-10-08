e921bba406c53e4014fb907edbb3353b38ed1757
fay <lfhlifenghao@gmail.com>
2016-10-08 16:53:14 +0800

    update

--- fbi_web/src/com/fr/bi/web/cross/demo/mvc/js/detail/mvc.treelabel.js
+++ fbi_web/src/com/fr/bi/web/cross/demo/mvc/js/detail/mvc.treelabel.js
@@ -61,7 +61,28 @@ TreeLabelView = BI.inherit(BI.View, {
                         });
                         ids = temp;
                     }
-                    callback({items: result});
+                    //callback({items: result});
+                    callback({
+                        items: [
+                            {id: 1, value: "南京", text: "南京", pId: null},
+                            {id: 2, value: "苏州", text: "苏州", pId: null},
+                            {id: 3, value: "无锡", text: "无锡", pId: null},
+                            {id: 11, value: "玄武区", text: "玄武区", pId: 1},
+                            {id: 21, value: "工业园区", text: "工业园区", pId: 2},
+                            {id: 31, value: "南长区", text: "南长区", pId: 3},
+                            {id: 12, value: "江宁区", text: "江宁区", pId: 1},
+                            {id: 111, value: "40", text: "40", pId: 11},
+                            {id: 112, value: "60", text: "60", pId: 11},
+                            {id: 211, value: "60", text: "60", pId: 21},
+                            {id: 121, value: "40", text: "40", pId: 12},
+                            {id: 1111, value: "70-89", text: "70-89", pId: 111},
+                            {id: 1112, value: "89-100", text: "89-100", pId: 111},
+                            {id: 2111, value: "70-89", text: "70-89", pId: 211},
+                            {id: 2112, value: "89-100", text: "89-100", pId: 211},
+                            {id: 1121, value: "70-89", text: "70-89", pId: 112},
+                            {id: 1122, value: "89-100", text: "89-100", pId: 112}
+                        ]
+                    });
                 } else {
                     callback({
                         items: [
