04c39714ed32e9afd36a318c47b38ecdf968c8f3
connery <mitiskysean@gmail.com>
2016-10-09 11:08:20 +0800

    log

--- fbi_base/src/com/finebi/cube/common/log/BILoggerFactory.java
+++ fbi_base/src/com/finebi/cube/common/log/BILoggerFactory.java
@@ -1,58 +1,34 @@
 package com.finebi.cube.common.log;
 
-import com.fr.bi.manager.PerformancePlugManager;
-import com.fr.bi.stable.utils.code.BIPrintUtils;
-import com.fr.bi.stable.utils.time.BIDateUtils;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * BI日志输出
  */
 public class BILoggerFactory {
-    boolean verbose = true;
-    public static BILoggerFactory logger = null;
 
-    public static BILoggerFactory getLogger() {
-        if (logger != null) {
-            return logger;
-        }
-        synchronized (BILoggerFactory.class) {
-            if (logger == null) {
-                logger = new BILoggerFactory();
+
+    public static Map<Class, BILogger> loggerMap = new HashMap<Class, BILogger>();
+
+    public static BILogger getLogger(Class clazz) {
+        if (loggerMap.containsKey(clazz)) {
+            return loggerMap.get(clazz);
+        } else {
+            synchronized (BILoggerFactory.class) {
+                if (!loggerMap.containsKey(clazz)) {
+                    BILogger logger = new BILogger(LoggerFactory.getLogger(clazz));
+                    loggerMap.put(clazz, logger);
+                }
+                return loggerMap.get(clazz);
             }
         }
-        return logger;
     }
 
-    public void error(String message) {
-        errorOut(message);
+    public static BILogger getLogger() {
+        return getLogger(BILogger.class);
     }
 
-    public void error(String message, Throwable e) {
-        errorOut(message);
-        String out = BIPrintUtils.outputException(e);
-        errorOut(out);
-    }
-
-    private void errorOut(String message) {
-        if (PerformancePlugManager.getInstance().useStandardOutError()) {
-            standardOut(message);
-        } else {
-            System.err.println(BIDateUtils.getCurrentDateTime() + ": " + message);
-        }
-    }
-
-    public void info(String message) {
-        standardOut(message);
-    }
-
-    public void standardOut(String message) {
-        System.out.println(BIDateUtils.getCurrentDateTime() + ": " + message);
-    }
-
-
-    public void debug(String message) {
-        if (verbose) {
-            System.out.println(BIDateUtils.getCurrentDateTime() + ": " + message);
-        }
-    }
 }
