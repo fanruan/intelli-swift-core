a9e2d70e94041cd00a5f4c2fca7857ccae770c39
windy <1374721899@qq.com>
2016-10-11 15:06:03 +0800

    文本提示

--- fbi_web/src/com/fr/bi/web/js/modules/fieldrelationsettingwithpreview/fieldrelationsettingwithpreviewpopup.js
+++ fbi_web/src/com/fr/bi/web/js/modules/fieldrelationsettingwithpreview/fieldrelationsettingwithpreviewpopup.js
@@ -53,7 +53,8 @@ BI.FieldRelationSettingWithPreviewPopup = BI.inherit(BI.BarPopoverSection, {
             textHeight: 35,
             textAlign: "left",
             cls: "detail-table-path-setting-tip-label",
-            value: BI.i18nText("BI-Detail_Setting_Path")
+            value: BI.i18nText("BI-Detail_Setting_Path"),
+            title: BI.i18nText("BI-Detail_Setting_Path")
         });
 
 
@@ -74,7 +75,8 @@ BI.FieldRelationSettingWithPreviewPopup = BI.inherit(BI.BarPopoverSection, {
             textHeight: 35,
             textAlign: "left",
             cls: "detail-table-path-setting-tip-label",
-            value: BI.i18nText("BI-Check_If_The_Relation_Below_Is_You_Need")
+            value: BI.i18nText("BI-Check_If_The_Relation_Below_Is_You_Need"),
+            title: BI.i18nText("BI-Check_If_The_Relation_Below_Is_You_Need")
         });
 
         this.tabPane = BI.createWidget({
