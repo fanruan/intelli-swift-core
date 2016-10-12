db710a2038d74bd38708565a29baf9c8bbbfd043
windy <1374721899@qq.com>
2016-10-12 19:22:24 +0800

    BI风格添加全局样式中的属性

---
+++ fbi_conf/src/com/fr/bi/conf/fs/tablechartstyle/BIChartFontStyleAttr.java
@@ -0,0 +1,25 @@
+package com.fr.bi.conf.fs.tablechartstyle;
+
+/**
+ * Created by windy on 2016/10/12.
+ */
+public class BIChartFontStyleAttr extends BIAbstractFontStyleAttr{
+
+    public BIChartFontStyleAttr() {
+    }
+
+    public BIChartFontStyleAttr(String color, String fontStyle, String fontWidget) {
+        super(color, fontStyle, fontWidget);
+    }
+
+    @Override
+    public String getTag() {
+        return "ChartFont";
+    }
+
+    @Override
+    public String toString() {
+        return "{" + "color:\"" + this.getColor() + "\", font-style: \"" + this.getFontStyle() +
+                "\",font-weight:\"" + this.getFontWidget() + "\"}";
+    }
+}
--- fbi_web_base/src/com/fr/bi/web/base/fs/BIFSSetConfigAction.java
+++ fbi_web_base/src/com/fr/bi/web/base/fs/BIFSSetConfigAction.java
@@ -2,8 +2,11 @@ package com.fr.bi.web.base.fs;
 
 import com.fr.base.ChartPreStyleServerManager;
 import com.fr.base.FRContext;
+import com.fr.bi.conf.fs.BIChartStyleAttr;
 import com.fr.bi.conf.fs.FBIConfig;
+import com.fr.bi.conf.fs.tablechartstyle.*;
 import com.fr.bi.web.base.AbstractBIBaseAction;
+import com.fr.json.JSONObject;
 import com.fr.web.utils.WebUtils;
 
 import javax.servlet.http.HttpServletRequest;
@@ -29,7 +32,19 @@ public class BIFSSetConfigAction extends AbstractBIBaseAction {
     @Override
     public void actionCMDPrivilegePassed(HttpServletRequest req, HttpServletResponse res)
             throws Exception {
-        FBIConfig.getInstance().getChartStyleAttr().setChartStyle(Integer.parseInt(WebUtils.getHTTPRequestParameter(req, "chartStyle")));
+        JSONObject mainBackground = new JSONObject(WebUtils.getHTTPRequestParameter(req, "mainBackground"));
+        JSONObject widgetBackground = new JSONObject(WebUtils.getHTTPRequestParameter(req, "widgetBackground"));
+        JSONObject titleBackground = new JSONObject(WebUtils.getHTTPRequestParameter(req, "titleBackground"));
+        JSONObject titleFont = new JSONObject(WebUtils.getHTTPRequestParameter(req, "titleFont"));
+        JSONObject chartFont = new JSONObject(WebUtils.getHTTPRequestParameter(req, "chartFont"));
+        BIChartStyleAttr chartStyleAttr = FBIConfig.getInstance().getChartStyleAttr();
+        chartStyleAttr.setChartStyle(Integer.parseInt(WebUtils.getHTTPRequestParameter(req, "chartStyle")));
+        chartStyleAttr.setControlTheme(WebUtils.getHTTPRequestParameter(req, "controlTheme"));
+        chartStyleAttr.setChartFont(new BIChartFontStyleAttr(chartFont.optString("color", ""),chartFont.optString("font-style", ""),chartFont.optString("font-widget", "")));
+        chartStyleAttr.setTitleFont(new BITitleFontStyleAttr(titleFont.optString("color", ""),titleFont.optString("font-style", ""),titleFont.optString("font-widget", ""), titleFont.optString("text-align", "")));
+        chartStyleAttr.setMainBackground(new BIMainBackgroundAttr(mainBackground.optString("value", ""), mainBackground.optInt("type", 1)));
+        chartStyleAttr.setTitleBackground(new BITitleBackgroundAttr(titleBackground.optString("value", ""), titleBackground.optInt("type", 1)));
+        chartStyleAttr.setWidgetBackground(new BIWidgetBackgroundAttr(widgetBackground.optString("value", ""), widgetBackground.optInt("type", 1)));
 //        FBIConfig.getInstance().getChartStyleAttr().setDefaultStyle(Integer.parseInt(WebUtils.getHTTPRequestParameter(req, "defaultStyle")));
         ChartPreStyleServerManager.getInstance().setCurrentStyle(WebUtils.getHTTPRequestParameter(req, "defaultColor"));
 
