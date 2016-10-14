ef2558ae8fbe466e9f75e459028cf9ebfd696ad6
imp <imp@finereport.com>
2016-10-14 16:47:46 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/comparecolumnchart.setting.js
+++ fbi_web/src/com/fr/bi/web/js/modules/chartsetting/charts/settings/comparecolumnchart.setting.js
@@ -15,13 +15,83 @@ BI.CompareColumnChartsSetting = BI.inherit(BI.AbstractChartSetting, {
         BI.CompareColumnChartsSetting.superclass._init.apply(this, arguments);
         var self = this, o = this.options, constant = BI.AbstractChartSetting;
 
-        this.widgetSetting = BI.createWidget({
-            type: "bi.widget_block_setting",
-            wId: o.wId
+        //显示组件标题
+        this.showTitle = BI.createWidget({
+            type: "bi.multi_select_item",
+            value: BI.i18nText("BI-Show_Chart_Title"),
+            cls: "attr-names",
+            logic: {
+                dynamic: true
+            }
+        });
+        this.showTitle.on(BI.Controller.EVENT_CHANGE, function () {
+            self.widgetTitle.setVisible(this.isSelected());
+            self.fireEvent(BI.GroupTableSetting.EVENT_CHANGE);
         });
 
-        this.widgetSetting.on(BI.WidgetBlockSetting.EVENT_CHANGE, function() {
-            self.fireEvent(BI.CompareColumnChartsSetting.EVENT_CHANGE)
+        //组件标题
+        this.title = BI.createWidget({
+            type: "bi.sign_editor",
+            cls: "title-input",
+            width: 120
+        });
+
+        this.title.on(BI.SignEditor.EVENT_CHANGE, function () {
+            self.fireEvent(BI.GroupTableSetting.EVENT_CHANGE)
+        });
+
+        //详细设置
+        this.titleDetailSettting = BI.createWidget({
+            type: "bi.show_title_detailed_setting_combo"
+        });
+
+        this.titleDetailSettting.on(BI.ShowTitleDetailedSettingCombo.EVENT_CHANGE, function () {
+            self.fireEvent(BI.GroupTableSetting.EVENT_CHANGE)
+        });
+
+        this.widgetTitle = BI.createWidget({
+            type: "bi.left",
+            items: [this.title, this.titleDetailSettting],
+            hgap: constant.SIMPLE_H_GAP
+        });
+
+        //组件背景
+        this.widgetBackground = BI.createWidget({
+            type: "bi.global_style_index_background"
+        });
+        this.widgetBackground.on(BI.GlobalStyleIndexBackground.EVENT_CHANGE, function () {
+            self.fireEvent(BI.GroupTableSetting.EVENT_CHANGE);
+        });
+
+        var widgetTitle = BI.createWidget({
+            type: "bi.left",
+            cls: "single-line-settings",
+            items: BI.createItems([{
+                type: "bi.label",
+                text: BI.i18nText("BI-Component_Widget"),
+                cls: "line-title",
+            }, {
+                type: "bi.label",
+                text: BI.i18nText("BI-Title"),
+                cls: "line-title",
+                lgap: 38
+            }, {
+                type: "bi.vertical_adapt",
+                items: [this.showTitle]
+            }, {
+                type: "bi.vertical_adapt",
+                items: [this.widgetTitle]
+            }, {
+                type: "bi.label",
+                text: BI.i18nText("BI-Background"),
+                cls: "line-title",
+            }, {
+                type: "bi.vertical_adapt",
+                items: [this.widgetBackground]
+            }], {
+                height: constant.SINGLE_LINE_HEIGHT
+            }),
+            hgap: constant.SIMPLE_H_GAP
         });
 
         this.colorSelect = BI.createWidget({
@@ -645,7 +715,7 @@ BI.CompareColumnChartsSetting = BI.inherit(BI.AbstractChartSetting, {
         BI.createWidget({
             type: "bi.vertical",
             element: this.element,
-            items: [this.widgetSetting, this.tableStyle, this.lYAxis, this.rYAxis, this.xAxis, this.showElement, this.otherAttr, modelChange],
+            items: [widgetTitle, this.tableStyle, this.lYAxis, this.rYAxis, this.xAxis, this.showElement, this.otherAttr, modelChange],
             hgap: 10
         })
     },
