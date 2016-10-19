7a87db3367cd2a711034f051ff5317dcc0c1b16a
pony <pony@finereport.com>
2016-10-19 10:58:20 +0800

    ..

--- fbi_cube_base/src/com/finebi/cube/data/disk/reader/primitive/BIPrimitiveNIOReaderBuilder.java
+++ fbi_cube_base/src/com/finebi/cube/data/disk/reader/primitive/BIPrimitiveNIOReaderBuilder.java
@@ -2,9 +2,6 @@ package com.finebi.cube.data.disk.reader.primitive;
 
 import com.finebi.cube.data.disk.reader.BIBasicNIOReaderBuilder;
 import com.finebi.cube.data.input.primitive.ICubePrimitiveReader;
-import com.fr.bi.stable.io.newio.NIOConstant;
-
-import java.io.File;
 
 /**
  * This class created on 2016/3/16.
@@ -13,7 +10,4 @@ import java.io.File;
  * @since 4.0
  */
 public abstract class BIPrimitiveNIOReaderBuilder<T extends ICubePrimitiveReader> extends BIBasicNIOReaderBuilder<T> {
-    boolean isSingleFile(File file){
-        return file!= null && file.exists() && file.length() < NIOConstant.SINGLE_FILE_LENGTH && file.length() > 0;
-    }
 }
