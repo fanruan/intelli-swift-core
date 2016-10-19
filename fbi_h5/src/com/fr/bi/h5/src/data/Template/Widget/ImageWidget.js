ac96f194d4195eaa555f899926fd5010bbf1f6bf
young <young@finereport.com>
2016-10-19 19:57:21 +0800

    无JIRA任务 H5图片组件

--- fbi_h5/src/com/fr/bi/h5/src/data/Template/Widget/ImageWidget.js
+++ fbi_h5/src/com/fr/bi/h5/src/data/Template/Widget/ImageWidget.js
@@ -8,6 +8,28 @@ class ImageWidget extends AbstractWidget{
     constructor($widget, ...props) {
         super($widget, ...props);
     }
+
+    getSizeMode() {
+        const size = this.$widget.get('size');
+        switch (size) {
+            case BICst.IMAGE_RESIZE_MODE.ORIGINAL:
+                return 'contain';
+            case BICst.IMAGE_RESIZE_MODE.EQUAL:
+                return 'cover';
+            case BICst.IMAGE_RESIZE_MODE.STRETCH:
+                return 'stretch';
+            default:
+                return 'contain'
+        }
+    }
+
+    getSrc() {
+        return this.$widget.get('src');
+    }
+
+    getHref() {
+        return this.$widget.get('href');
+    }
 }
 
 export default ImageWidget;
