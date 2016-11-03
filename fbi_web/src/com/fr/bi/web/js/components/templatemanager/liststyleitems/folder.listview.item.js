c6f4b27318d90d23f5bba8fba881d1a2c53907cd
windy <1374721899@qq.com>
2016-11-03 09:11:09 +0800

    BI-1900 分析模板列表文件夹

--- fbi_web/src/com/fr/bi/web/js/components/templatemanager/liststyleitems/folder.listview.item.js
+++ fbi_web/src/com/fr/bi/web/js/components/templatemanager/liststyleitems/folder.listview.item.js
@@ -4,7 +4,7 @@
  * @class BI.FolderListViewItem
  * @extends BI.Single
  */
-BI.FolderListViewItem = BI.inherit(BI.Single, {
+BI.FolderListViewItem = BI.inherit(BI.BasicButton, {
 
     constants: {
         minGap: 10,
@@ -12,8 +12,9 @@ BI.FolderListViewItem = BI.inherit(BI.Single, {
     },
     _defaultConfig: function () {
         return BI.extend(BI.FolderListViewItem.superclass._defaultConfig.apply(this, arguments), {
-            baseCls: "bi-template-manager-folder-item bi-list-item",
+            baseCls: "bi-template-manager-folder-item bi-list-item cursor-pointer",
             height: 40,
+            disableSelected: true,
             validationChecker: BI.emptyFn,
             id: null,
             value: null
@@ -24,7 +25,8 @@ BI.FolderListViewItem = BI.inherit(BI.Single, {
         BI.FolderListViewItem.superclass._init.apply(this, arguments);
         var self = this, o = this.options;
         this.checkbox = BI.createWidget({
-            type: "bi.checkbox"
+            type: "bi.checkbox",
+            stopPropagation: true
         });
         this.checkbox.on(BI.Controller.EVENT_CHANGE, function () {
             arguments[2] = self;
@@ -132,12 +134,15 @@ BI.FolderListViewItem = BI.inherit(BI.Single, {
                 width: 230
             }, {
                 el: this.blankSpace,
-                width: "fill"
+                width: 50
+            }, {
+                type: "bi.vertical_adapt",
+                hgap: 20,
+                items: [renameIcon, deleteIcon]
             }, {
                 el: {
                     type: "bi.left_right_vertical_adapt",
                     items: {
-                        left: [renameIcon, deleteIcon],
                         right: [timeText]
                     },
                     llgap: 20,
@@ -146,6 +151,12 @@ BI.FolderListViewItem = BI.inherit(BI.Single, {
                 width: 320
             }]
         });
+    },
+
+    doClick: function(){
+        var self = this, o = this.options;
+        BI.FolderListViewItem.superclass.doClick.apply(this, arguments);
+        o.onClickItem.apply(self, arguments);
     },
 
     isSelected: function () {
