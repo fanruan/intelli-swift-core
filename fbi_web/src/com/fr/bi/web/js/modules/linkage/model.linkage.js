bfe3947b205b8b08ada9e33e3b4c4d9541a22e11
fay <lfhlifenghao@gmail.com>
2016-11-08 09:22:02 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/linkage/model.linkage.js
+++ fbi_web/src/com/fr/bi/web/js/modules/linkage/model.linkage.js
@@ -113,8 +113,8 @@ BI.LinkageModel = BI.inherit(FR.OB, {
     },
 
     _isRelationsIntersect: function (relation1, relation2) {
-        var all1 = relation1.parents.concat(relation1.current);
-        var all2 = relation2.children.concat(relation2.parents).concat(relation2.current);
+        var all1 = [relation1.current];
+        var all2 = relation2.children.concat(relation2.current);
         var intersection = BI.intersection(all1, all2);
         return intersection.length > 0;
     },
