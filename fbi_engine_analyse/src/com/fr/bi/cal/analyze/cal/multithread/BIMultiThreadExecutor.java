b1b17c1792eb1946948042d54cc4c2ba6859b4f8
roy <ray@finereport.com>
2016-11-15 19:35:45 +0800

    1.螺旋分析文件没有释放，没有删除cube 2.螺旋分析多线程计算时的count数没有正确计算bug，导致没有删除文件

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/multithread/BIMultiThreadExecutor.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/multithread/BIMultiThreadExecutor.java
@@ -1,5 +1,12 @@
 package com.fr.bi.cal.analyze.cal.multithread;
 
+import com.finebi.cube.common.log.BILoggerFactory;
+import com.fr.bi.cal.analyze.session.BISession;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by 小灰灰 on 2016/8/8.
  */
@@ -8,10 +15,12 @@ public class BIMultiThreadExecutor {
     private static final int MOD = SIZE - 1;
     private MergeSummaryCallList[] lists = new MergeSummaryCallList[SIZE];
     private int index = 0;
+    private ExecutorService executorService;
     public BIMultiThreadExecutor() {
+        executorService = Executors.newFixedThreadPool(SIZE);
         for (int i = 0; i < SIZE; i ++){
             MergeSummaryCallList list = new MergeSummaryCallList();
-            MultiThreadManagerImpl.getInstance().getExecutorService().submit(list);
+            executorService.submit(list);
             lists[i] = list;
         }
     }
@@ -21,11 +30,22 @@ public class BIMultiThreadExecutor {
         index++;
     }
 
-    public void awaitExecutor(){
+    public void awaitExecutor(final BISession session){
         for (MergeSummaryCallList list : lists){
+            list.add(new BISingleThreadCal() {
+                @Override
+                public void cal() {
+                    session.getLoader().releaseCurrentThread();
+                }
+            });
             list.finish();
         }
         lists = null;
-        MultiThreadManagerImpl.getInstance().awaitExecutor();
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            BILoggerFactory.getLogger().error(e.getMessage(), e);
+        }
     }
 }
