8025bf027535267e9309d3256a0cc4a6d7ee2dae
windy <1374721899@qq.com>
2016-11-17 10:46:30 +0800

    update

--- fbi_web/src/com/fr/bi/web/js/modules/base/combos/dimension/dimension/abstract.dimension.combo.js
+++ fbi_web/src/com/fr/bi/web/js/modules/base/combos/dimension/dimension/abstract.dimension.combo.js
@@ -46,7 +46,7 @@ BI.AbstractDimensionCombo = BI.inherit(BI.AbstractDimensionTargetCombo, {
         var dimensionMap = BI.Utils.getDimensionMapByDimensionID(o.dId);
         var tIds = BI.Utils.getAllTargetDimensionIDs(BI.Utils.getWidgetIDByDimensionID(o.dId));
         var res = BI.find(tIds, function(idx, tId){
-            return !BI.Utils.isCalculateTargetByDimensionID(tId) && !checkDimAndTarRelationValidInCurrentPaths(dId, tId);
+            return !BI.Utils.isCalculateTargetByDimensionID(tId) && !checkDimAndTarRelationValidInCurrentPaths(o.dId, tId);
         });
         return BI.isNull(res);
 
