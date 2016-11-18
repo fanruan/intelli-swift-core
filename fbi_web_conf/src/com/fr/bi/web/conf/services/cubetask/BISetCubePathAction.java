2a16ff53bb2047da912811b77dfdf8595d34e219
young <young@finereport.com>
2016-11-18 11:36:29 +0800

    BI-2160 修复：移动cube文件前释放所有文件

--- fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/BISetCubePathAction.java
+++ fbi_web_conf/src/com/fr/bi/web/conf/services/cubetask/BISetCubePathAction.java
@@ -1,10 +1,13 @@
 package com.fr.bi.web.conf.services.cubetask;
 
+import com.finebi.cube.data.disk.BICubeDiskPrimitiveDiscovery;
 import com.fr.bi.conf.provider.BIConfigureManagerCenter;
 import com.finebi.cube.common.log.BILoggerFactory;
+import com.fr.bi.stable.utils.file.BIFileUtils;
 import com.fr.bi.util.BIConfigurePathUtils;
 import com.fr.bi.web.conf.AbstractBIConfigureAction;
 import com.fr.fs.web.service.ServiceUtils;
+import com.fr.general.ComparatorUtils;
 import com.fr.json.JSONObject;
 import com.fr.stable.StringUtils;
 import com.fr.web.utils.WebUtils;
@@ -32,11 +35,21 @@ public class BISetCubePathAction extends AbstractBIConfigureAction {
         if (StringUtils.isEmpty(fileName)) {
             return;
         }
+        String oPath = BIConfigureManagerCenter.getCubeConfManager().getCubePath();
         String path = BIConfigurePathUtils.checkCubePath(fileName);
         if (!StringUtils.isEmpty(path)) {
             BIConfigureManagerCenter.getCubeConfManager().saveCubePath(fileName);
             try {
                 BIConfigureManagerCenter.getCubeConfManager().persistData(userId);
+                if (!ComparatorUtils.equals(oPath, path)) {
+                    try {
+                        //释放
+                        BICubeDiskPrimitiveDiscovery.getInstance().forceRelease();
+                        BIFileUtils.moveFile(oPath, path);
+                    } finally {
+                        BICubeDiskPrimitiveDiscovery.getInstance().finishRelease();
+                    }
+                }
             } catch (Exception e) {
                 BILoggerFactory.getLogger().error(e.getMessage(), e);
             }
