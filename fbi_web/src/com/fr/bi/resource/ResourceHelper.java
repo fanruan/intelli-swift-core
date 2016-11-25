1a01d92956b67efcf85e57441ad21469853d28ce
guy <919891171@qq.com>
2016-11-25 11:01:22 +0800

    add

--- fbi_web/src/com/fr/bi/resource/ResourceHelper.java
+++ fbi_web/src/com/fr/bi/resource/ResourceHelper.java
@@ -2269,7 +2269,6 @@ public class ResourceHelper {
                 "com/fr/bi/web/js/base/module/table/table.tree.js",
 
                 "com/fr/bi/web/js/base/module/table/table.fix.js",
-                "com/fr/bi/web/js/base/module/table/table.cache.js",
 
                 "com/fr/bi/web/js/base/module/layer/layer.floatbox.js",
                 "com/fr/bi/web/js/base/module/layer/layer.popup.js",
