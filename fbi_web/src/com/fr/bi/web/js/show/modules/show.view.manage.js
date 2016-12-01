f134907308b63263a6787062ccb319cdb3157743
zcf <chaofeng@finereport.com>
2016-12-01 15:40:54 +0800

    无JIRA任务 show面板维度切换改view层

--- fbi_web/src/com/fr/bi/web/js/show/modules/show.view.manage.js
+++ fbi_web/src/com/fr/bi/web/js/show/modules/show.view.manage.js
@@ -8,8 +8,47 @@ BIShow.Views = new (BI.inherit(BI.WRouter, {
         "/pane": "BIShow.PaneView",
         "/pane/:id/:type": "getWidget",
         "/pane/:id/:type/detail/:region/:dId": "getDimensionOrTarget",
+        "/pane/:id/:type/detail": "getDetail"
     },
-
+    getDetail: function (id, type) {
+        switch (BI.parseInt(type)) {
+            case BICst.WIDGET.TABLE:
+            case BICst.WIDGET.CROSS_TABLE:
+            case BICst.WIDGET.COMPLEX_TABLE:
+            case BICst.WIDGET.AXIS:
+            case BICst.WIDGET.ACCUMULATE_AXIS:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AXIS:
+            case BICst.WIDGET.COMPARE_AXIS:
+            case BICst.WIDGET.FALL_AXIS:
+            case BICst.WIDGET.BAR:
+            case BICst.WIDGET.ACCUMULATE_BAR:
+            case BICst.WIDGET.COMPARE_BAR:
+            case BICst.WIDGET.LINE:
+            case BICst.WIDGET.AREA:
+            case BICst.WIDGET.ACCUMULATE_AREA:
+            case BICst.WIDGET.PERCENT_ACCUMULATE_AREA:
+            case BICst.WIDGET.COMPARE_AREA:
+            case BICst.WIDGET.RANGE_AREA:
+            case BICst.WIDGET.COMBINE_CHART:
+            case BICst.WIDGET.MULTI_AXIS_COMBINE_CHART:
+            case BICst.WIDGET.PIE:
+            case BICst.WIDGET.DONUT:
+            case BICst.WIDGET.MAP:
+            case BICst.WIDGET.GIS_MAP:
+            case BICst.WIDGET.DASHBOARD:
+            case BICst.WIDGET.BUBBLE:
+            case BICst.WIDGET.FORCE_BUBBLE:
+            case BICst.WIDGET.SCATTER:
+            case BICst.WIDGET.RADAR:
+            case BICst.WIDGET.ACCUMULATE_RADAR:
+            case BICst.WIDGET.FUNNEL:
+                return "BIShow.DetailView";
+            // case BICst.WIDGET.DETAIL:
+            //     return "BIShow.DetailTableDetailView";
+            default:
+                return;
+        }
+    },
     getWidget: function (id, type) {
         var view = "";
         switch (BI.parseInt(type)) {
