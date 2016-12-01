0eed1959d3ef66ef8e606bab29e2369ec43262db
qbwang <wangqi101023@gmail.com>
2016-12-01 19:34:49 +0800

    修复BI4.0连接hive后选表并更新Cube，显示报错

--- fbi_conf/src/com/fr/bi/data/NormalExtractor.java
+++ fbi_conf/src/com/fr/bi/data/NormalExtractor.java
@@ -1,5 +1,8 @@
 package com.fr.bi.data;
 
+import com.fr.bi.stable.data.db.ICubeFieldSource;
+import com.fr.bi.stable.dbdealer.DBDealer;
+import com.fr.bi.stable.dbdealer.TimestampDealer;
 import com.fr.data.core.db.dialect.Dialect;
 
 import java.sql.Connection;
@@ -16,6 +19,10 @@ import java.sql.Statement;
 public class NormalExtractor extends DBExtractorImpl {
 
 
+    @Override
+    protected DBDealer dealWithDate(ICubeFieldSource field, int rsColumn) {
+        return new TimestampDealer(rsColumn);
+    }
 
     public Statement createStatement(Connection conn, Dialect dialect) throws SQLException {
         Statement stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
