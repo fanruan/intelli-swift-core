c89a1b28c61a6223c44025d190d880e503b1a85a
young <young@finereport.com>
2016-12-13 16:33:45 +0800

    调整

--- fbi_web/src/com/fr/bi/resource/ResourceHelper.java
+++ fbi_web/src/com/fr/bi/resource/ResourceHelper.java
@@ -1548,7 +1548,7 @@ public class ResourceHelper {
                 //one package
                 "com/fr/bi/web/js/modules/onepackage/widget.onepackage.js",
                 "com/fr/bi/web/js/modules/onepackage/widget.onepackage.model.js",
-                "com/fr/bi/web/js/modules/onepackage/widget.refreshtablefields.js",
+                "com/fr/bi/web/js/modules/onepackage/etl/refreshfields/widget.refreshtablefields.js",
 
                 //数据连接
                 "com/fr/bi/web/js/modules/datalink/widget.testlink.loading.js",
