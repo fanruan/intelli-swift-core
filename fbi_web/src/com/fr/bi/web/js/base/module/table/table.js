c2d8853bb12872dca92a715abdec9fbce1a30074
guy <guy@finereport.com>
2016-12-14 10:59:36 +0800

    改回来  不能改

--- fbi_web/src/com/fr/bi/web/js/base/module/table/table.js
+++ fbi_web/src/com/fr/bi/web/js/base/module/table/table.js
@@ -270,25 +270,25 @@ BI.Table = BI.inherit(BI.Widget, {
         this.topLeft = BI.createWidget({
             type: "bi.adaptive",
             cls: "top-left",
-            // scrollable: false,
+            scrollable: false,
             items: [this.scrollTopLeft]
         });
         this.topRight = BI.createWidget({
             type: "bi.adaptive",
             cls: "top-right",
-            // scrollable: false,
+            scrollable: false,
             items: [this.scrollTopRight]
         });
         this.bottomLeft = BI.createWidget({
             type: "bi.adaptive",
             cls: "bottom-left",
-            //scrollable: false,
+            // scrollable: false,
             items: [this.scrollBottomLeft]
         });
         this.bottomRight = BI.createWidget({
             type: "bi.adaptive",
             cls: "bottom-right",
-            //scrollable: false,
+            scrollable: false,
             items: [this.scrollBottomRight]
         });
 
@@ -1052,7 +1052,7 @@ BI.Table = BI.inherit(BI.Widget, {
             type: "bi.adaptive",
             cls: "bottom-right",
             element: this.element,
-            // scrollable: false,
+            scrollable: false,
             items: [this.scrollBottomRight]
         });
 
