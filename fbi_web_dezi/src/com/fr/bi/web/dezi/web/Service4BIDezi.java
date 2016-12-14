29123cec322415081f8072141aa73c888f2959f0
imp <imp@finereport.com>
2016-12-14 20:00:17 +0800

    update

--- fbi_web_dezi/src/com/fr/bi/web/dezi/web/Service4BIDezi.java
+++ fbi_web_dezi/src/com/fr/bi/web/dezi/web/Service4BIDezi.java
@@ -47,7 +47,7 @@ public class Service4BIDezi implements Service {
             new BIExcelExportAction(),
 
             new BIGlobalExportAction(),
-            
+
             new BIStartGenerateTempCubeAction(),
             new BIGetTempCubeGeneratingStatusAction(),
 
