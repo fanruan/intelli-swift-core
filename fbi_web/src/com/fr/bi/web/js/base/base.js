696dba63b22f558fa1333a44e825d3120c8955c2
guy <guy@finereport.com>
2016-12-15 17:21:24 +0800

    代码整理

--- fbi_web/src/com/fr/bi/web/js/base/base.js
+++ fbi_web/src/com/fr/bi/web/js/base/base.js
@@ -1000,23 +1000,23 @@ if (!window.BI) {
     //浏览器相关方法
     _.extend(BI, {
         isIE: function () {
-            return $.browser.msie;
+            return /(msie|trident)/i.test(navigator.userAgent.toLowerCase());
         },
 
         isChrome: function () {
-            return $.browser.chrome;
+            return /(chrome)/i.test(navigator.userAgent.toLowerCase());
         },
 
         isFireFox: function () {
-            return $.browser.mozilla;
+            return /(firefox)/i.test(navigator.userAgent.toLowerCase());
         },
 
         isOpera: function () {
-            return $.browser.opera;
+            return /(opera)/i.test(navigator.userAgent.toLowerCase());
         },
 
         isSafari: function () {
-            return $.browser.safari;
+            return /(safari)/i.test(navigator.userAgent.toLowerCase());
         },
 
         isKhtml: function () {
