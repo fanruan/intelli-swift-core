e616a2c3fdca4e3444bf93a2612bd45ecc2a1f46
CL <824983662@qq.com>
2017-01-06 10:19:43 +0800

    滚动条事件函数将当前的iframe注册进去，导致在tab关闭时，iframe不释放，内存越来越大，导致前端操作卡，现在已经将滚动条注册事件部分注释掉，待王川重新实现

--- fbi_web/src/com/fr/bi/web/js/third/jquery.mCustomScrollbar.js
+++ fbi_web/src/com/fr/bi/web/js/third/jquery.mCustomScrollbar.js
@@ -1095,7 +1095,7 @@ and dependencies (minified).
 			if(o.advanced.extraDraggableSelectors){sel.add($(o.advanced.extraDraggableSelectors));}
 			if(d.bindEvents){ /* check if events are bound */
 				/* unbind namespaced events from document/selectors */
-				$(document).add($(!_canAccessIFrame() || top.document)).unbind("."+namespace);
+				//$(document).add($(!_canAccessIFrame() || top.document)).unbind("."+namespace);
 				sel.each(function(){
 					$(this).unbind("."+namespace);
 				});
@@ -1275,9 +1275,9 @@ and dependencies (minified).
 				_onTouchend(e);
 			});
 			if(iframe.length){
-				iframe.each(function(){
+				/*iframe.each(function(){
 					$(this).load(function(){
-						/* bind events on accessible iframes */
+						/!* bind events on accessible iframes *!/
 						if(_canAccessIFrame(this)){
 							$(this.contentDocument || this.contentWindow.document).bind(events[0],function(e){
 								_onTouchstart(e);
@@ -1289,7 +1289,7 @@ and dependencies (minified).
 							});
 						}
 					});
-				});
+				});*/
 			}
 			function _onTouchstart(e){
 				if(!_pointerTouch(e) || touchActive || _coordinates(e)[2]){touchable=0; return;}
@@ -1458,16 +1458,16 @@ and dependencies (minified).
 				mCSB_dragger=[$("#mCSB_"+d.idx+"_dragger_vertical"),$("#mCSB_"+d.idx+"_dragger_horizontal")],
 				iframe=$("#mCSB_"+d.idx+"_container").find("iframe");
 			if(iframe.length){
-				iframe.each(function(){
+				/*iframe.each(function(){
 					$(this).load(function(){
-						/* bind events on accessible iframes */
+						/!* bind events on accessible iframes *!/
 						if(_canAccessIFrame(this)){
 							$(this.contentDocument || this.contentWindow.document).bind("mousewheel."+namespace,function(e,delta){
 								_onMousewheel(e,delta);
 							});
 						}
 					});
-				});
+				});*/
 			}
 			mCustomScrollBox.bind("mousewheel."+namespace,function(e,delta){
 				_onMousewheel(e,delta);
@@ -1513,20 +1513,20 @@ and dependencies (minified).
 		
 		/* checks if iframe can be accessed */
 		_canAccessIFrame=function(iframe){
-			var html=null;
+		/*	var html=null;
 			if(!iframe){
 				try{
 					var doc=top.document;
 					html=doc.body.innerHTML;
-				}catch(err){/* do nothing */}
+				}catch(err){/!* do nothing *!/}
 				return(html!==null);
 			}else{
 				try{
 					var doc=iframe.contentDocument || iframe.contentWindow.document;
 					html=doc.body.innerHTML;
-				}catch(err){/* do nothing */}
+				}catch(err){/!* do nothing *!/}
 				return(html!==null);
-			}
+			}*/
 		},
 		/* -------------------- */
 		
@@ -1689,16 +1689,16 @@ and dependencies (minified).
 				iframe=mCSB_container.find("iframe"),
 				events=["blur."+namespace+" keydown."+namespace+" keyup."+namespace];
 			if(iframe.length){
-				iframe.each(function(){
+				/*iframe.each(function(){
 					$(this).load(function(){
-						/* bind events on accessible iframes */
+						/!* bind events on accessible iframes *!/
 						if(_canAccessIFrame(this)){
 							$(this.contentDocument || this.contentWindow.document).bind(events[0],function(e){
 								_onKeyboard(e);
 							});
 						}
 					});
-				});
+				});*/
 			}
 			mCustomScrollBox.attr("tabindex","0").bind(events[0],function(e){
 				_onKeyboard(e);
