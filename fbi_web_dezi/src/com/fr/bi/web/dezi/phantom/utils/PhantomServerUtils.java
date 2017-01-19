c9186844a9459fcbb0dda116ccdf29f8614fe075
imp <imp@fanruan.com>
2017-01-19 22:55:44 +0800

    update

--- fbi_web_dezi/src/com/fr/bi/web/dezi/phantom/utils/PhantomServerUtils.java
+++ fbi_web_dezi/src/com/fr/bi/web/dezi/phantom/utils/PhantomServerUtils.java
@@ -109,7 +109,7 @@ public class PhantomServerUtils {
         if (isLinux32() || isLinux64()){
             PhantomServerUtils.setAuthority(exe);
         }else if (OperatingSystem.isMacOS()){
-            PhantomServerUtils.setAuthority(exe);
+            exe += "-mac";
         }else if (isUnix32() || isUnix64()){
             PhantomServerUtils.setAuthority(exe);
         }
