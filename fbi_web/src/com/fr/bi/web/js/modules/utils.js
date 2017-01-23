56b2e4cb520aed348b32b0595fd6d75d11eedfc5
windy <1374721899@qq.com>
2017-01-23 10:05:15 +0800

    BI-3192 带空值的数值维度，点击空值的那个指标联动，却把不是空值的全部过滤出来了

--- fbi_web/src/com/fr/bi/web/js/modules/utils.js
+++ fbi_web/src/com/fr/bi/web/js/modules/utils.js
@@ -2576,15 +2576,23 @@
                     };
                 }
                 if (groupType === BICst.GROUP.ID_GROUP) {
-                    return {
-                        filter_type: BICst.TARGET_FILTER_NUMBER.BELONG_VALUE,
-                        filter_value: {
-                            min: BI.parseFloat(value),
-                            max: BI.parseFloat(value),
-                            closemin: true,
-                            closemax: true
-                        },
-                        _src: {field_id: BI.Utils.getFieldIDByDimensionID(dId)}
+                    if(BI.isNull(value) || BI.isEmptyString(value)){
+                        return {
+                            filter_type: BICst.TARGET_FILTER_NUMBER.IS_NULL,
+                            filter_value: {},
+                            _src: {field_id: BI.Utils.getFieldIDByDimensionID(dId)}
+                        };
+                    }else{
+                        return {
+                            filter_type: BICst.TARGET_FILTER_NUMBER.BELONG_VALUE,
+                            filter_value: {
+                                min: BI.parseFloat(value),
+                                max: BI.parseFloat(value),
+                                closemin: true,
+                                closemax: true
+                            },
+                            _src: {field_id: BI.Utils.getFieldIDByDimensionID(dId)}
+                        }
                     }
                 }
                 var groupNodes = groupValue.group_nodes, useOther = groupValue.use_other;
@@ -2619,6 +2627,18 @@
                         filter_type: BICst.FILTER_TYPE.AND,
                         filter_value: vs
                     };
+                } else if (BI.isNumeric(value)) {
+                    //自定义分组后不勾选剩余值分组到其他
+                    return {
+                        filter_type: BICst.TARGET_FILTER_NUMBER.BELONG_VALUE,
+                        filter_value: {
+                            min: BI.parseFloat(value),
+                            max: BI.parseFloat(value),
+                            closemin: true,
+                            closemax: true
+                        },
+                        _src: {field_id: BI.Utils.getFieldIDByDimensionID(dId)}
+                    }
                 }
             }
 
