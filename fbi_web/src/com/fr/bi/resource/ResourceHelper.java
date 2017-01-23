1ff3b6a73efcfc458a18dfb23e73c6d02a7718b7
windy <1374721899@qq.com>
2017-01-23 16:00:05 +0800

    无JIRA任务 删react所有

--- fbi_web/src/com/fr/bi/resource/ResourceHelper.java
+++ fbi_web/src/com/fr/bi/resource/ResourceHelper.java
@@ -764,12 +764,6 @@ public class ResourceHelper {
                 //excelview
                 "com/fr/bi/web/js/extend/excelview/excelview.cell.js",
                 "com/fr/bi/web/js/extend/excelview/excelview.js",
-
-
-                //react明细表
-//                "com/fr/bi/web/js/modules/detailtablereact/widget.detailtablereact.js",
-                //react汇总表
-//                "com/fr/bi/web/js/modules/summarytablereact/widget.summarytablereact.js",
         };
     }
 
@@ -931,10 +925,7 @@ public class ResourceHelper {
 
                 //excelview
                 "com/fr/bi/web/css/extend/excelview/excelview.cell.css",
-                "com/fr/bi/web/css/extend/excelview/excelview.css",
-
-                //react明细表
-//                "com/fr/bi/web/css/modules/detailtablereact/widget.detailtablereact.css",
+                "com/fr/bi/web/css/extend/excelview/excelview.css"
         };
     }
 
@@ -2083,11 +2074,7 @@ public class ResourceHelper {
 
                 //分享给我的
                 "com/fr/bi/web/css/components/share/sharetome.css",
-                "com/fr/bi/web/css/components/share/sharetome.singleuser.css",
-
-
-                //react控件
-//                "com/fr/bi/web/css/components/components.react.css",
+                "com/fr/bi/web/css/components/share/sharetome.singleuser.css"
 
         };
     }
@@ -2858,11 +2845,7 @@ public class ResourceHelper {
 
                 //分享给我
                 "com/fr/bi/web/js/components/share/sharetome.js",
-                "com/fr/bi/web/js/components/share/sharetome.singleuser.js",
-
-
-                //react控件
-//                "com/fr/bi/web/js/components/components.react.js",
+                "com/fr/bi/web/js/components/share/sharetome.singleuser.js"
         };
     }
 
