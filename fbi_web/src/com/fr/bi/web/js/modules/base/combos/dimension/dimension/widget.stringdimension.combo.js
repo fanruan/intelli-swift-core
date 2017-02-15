bdfddb9366f138ecd3bf57df122c53d501b4de08
windy <1374721899@qq.com>
2017-02-15 13:45:35 +0800

    BI-2047

--- fbi_web/src/com/fr/bi/web/js/modules/base/combos/dimension/dimension/widget.stringdimension.combo.js
+++ fbi_web/src/com/fr/bi/web/js/modules/base/combos/dimension/dimension/widget.stringdimension.combo.js
@@ -25,6 +25,7 @@ BI.DimensionStringCombo = BI.inherit(BI.AbstractDimensionCombo, {
 
 
     defaultItems: function () {
+        var showFieldDisabled = this.checkShowFieldDisabled();
         return [
             [{
                 el: {
@@ -69,9 +70,9 @@ BI.DimensionStringCombo = BI.inherit(BI.AbstractDimensionCombo, {
                 text: BI.i18nText("BI-Show_Field"),
                 value: BICst.DIMENSION_STRING_COMBO.SHOW_FIELD,
                 cls: BI.Utils.isDimensionUsable(this.options.dId) ? "widget-combo-show-title-font" : "",
-                disabled: this.checkShowFieldDisabled(),
+                disabled: showFieldDisabled,
                 tipType: "success",
-                title: BI.i18nText("BI-Show_Field")
+                title: showFieldDisabled ? BI.i18nText("BI-For_Chart_Multi_Targets_Then_Forbid_Select_Dimension") : BI.i18nText("BI-Show_Field")
             }],
             [{
                 text: BI.i18nText("BI-Rename"),
