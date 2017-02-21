775e8805d8b5ba939d5c810a67d02c4e18f788bd
pony <pony@finereport.com>
2017-02-21 14:09:12 +0800

    pmd

--- fbi_web_report/src/com/fr/bi/web/report/services/BIInitDeziPaneAction.java
+++ fbi_web_report/src/com/fr/bi/web/report/services/BIInitDeziPaneAction.java
@@ -98,7 +98,9 @@ public class BIInitDeziPaneAction extends ActionNoSessionCMD {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
+            if (this == o) {
+                return true;
+            }
             if (o == null || getClass() != o.getClass()) {
                 return false;
             }
