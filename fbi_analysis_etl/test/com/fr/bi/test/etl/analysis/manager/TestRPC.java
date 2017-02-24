a2210ab8d80102a3321fcc8cbdbcccba97119d3e
wang <wang@finereport.com>
2017-02-24 11:51:48 +0800

    删除DataSourcemanagerProxy

--- fbi_analysis_etl/test/com/fr/bi/test/etl/analysis/manager/TestRPC.java
+++ fbi_analysis_etl/test/com/fr/bi/test/etl/analysis/manager/TestRPC.java
@@ -7,8 +7,6 @@ import com.fr.bi.cal.generate.EmptyCubeTask;
 import com.fr.bi.conf.base.auth.BISystemAuthorityManager;
 import com.fr.bi.conf.base.datasource.BIConnectionManager;
 import com.fr.bi.conf.base.datasource.BIConnectionProvider;
-import com.fr.bi.conf.base.datasource.DatasourceManagerProxy;
-import com.fr.bi.conf.base.datasource.DatasourceManagerProxyProvider;
 import com.fr.bi.conf.manager.update.BIUpdateSettingManager;
 import com.fr.bi.conf.provider.BIAuthorityManageProvider;
 import com.fr.bi.conf.provider.BIUpdateFrequencyManagerProvider;
@@ -83,33 +81,6 @@ public class TestRPC extends TestCase {
         System.err.println(pp.toString());
 
     }
-
-    public void testDataSourceManager() {
-        DatasourceManagerProxy provider = new DatasourceManagerProxy(DatasourceManager.getProviderInstance());
-        provider.putConnection("s",new JDBCDatabaseConnection());
-        provider.putConnection("s1",new JDBCDatabaseConnection());
-
-        RPC.registerSkeleton(provider, 12345);
-        DatasourceManagerProxyProvider proxy = (DatasourceManagerProxyProvider) RPC.getProxy(DatasourceManagerProxy.class, "127.0.0.1", 12345);
-        StableFactory.registerMarkedObject(DatasourceManagerProxyProvider.XML_TAG,proxy);
-        DatasourceManagerProxyProvider pp = DatasourceManagerProxy.getDatasourceManager();
-        synchronized (pp) {
-            Iterator<String> iterator = pp.getConnectionNameIterator();
-            while (iterator.hasNext()) {
-                String name = iterator.next();
-                JDBCDatabaseConnection connection = pp.getConnection(name, JDBCDatabaseConnection.class);
-                BIDBUtils.dealWithJDBCConnection(connection);
-                System.out.println(name);
-            }
-            try {
-                MemoryConnection.getConnectionMap().clear();
-                pp.writeContextResource();
-            } catch (Exception e) {
-                BILoggerFactory.getLogger().error(e.getMessage(), e);
-            }
-        }
-    }
-
 
 
     public void testCubeTask() {
