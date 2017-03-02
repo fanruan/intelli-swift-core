125a0aaa4e2398b4845156c1eaf044d70420d1c4
eason-skx <eason.skx@gmail.com>
2017-03-02 17:02:11 +0800

    图表属性后台计算

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
@@ -1,162 +1,78 @@
 package com.fr.bi.cal.analyze.report.report.widget;
 
-import com.fr.bi.conf.report.widget.field.dimension.BIDimension;
 import com.fr.bi.conf.session.BISessionProvider;
-import com.fr.bi.field.target.target.BISummaryTarget;
-import com.fr.bi.stable.constant.BIReportConstant;
-import com.fr.bi.stable.operation.group.IGroup;
-import com.fr.bi.stable.operation.group.group.NoGroup;
-import com.fr.general.ComparatorUtils;
 import com.fr.json.JSONArray;
 import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
 import com.fr.web.core.SessionDealWith;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 /**
  * Created by User on 2016/4/25.
  */
-public class VanChartWidget extends TableWidget {
+public abstract class VanChartWidget extends TableWidget {
 
-    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+    private static final int STYLE_GRADULA = 2;
 
-    public BIDimension getCategoryDimension() {
-        List<String> dimensionIds = view.get(Integer.parseInt(BIReportConstant.REGION.DIMENSION1));
-        if (dimensionIds == null) {
-            return null;
-        }
-        for (BIDimension dimension : this.getDimensions()) {
-            if (dimensionIds.contains(dimension.getValue()) && dimension.isUsed()) {
-                return dimension;
-            }
-        }
-        return null;
+    public JSONObject createDataJSON(BISessionProvider session) throws Exception {
+
+        JSONObject data = super.createDataJSON(session).getJSONObject("data");
+
+        JSONArray series = this.createSeries(data);
+
+        return this.createOptions().put("series", series);
     }
 
-    public BIDimension getSeriesDimension() {
-        List<String> dimensionIds = view.get(Integer.parseInt(BIReportConstant.REGION.DIMENSION2));
-        if (dimensionIds == null) {
-            return null;
-        }
-        for (BIDimension dimension : this.getDimensions()) {
-            if (dimensionIds.contains(dimension.getValue()) && dimension.isUsed()) {
-                return dimension;
-            }
-        }
-        return null;
-    }
-
-    public BIDimension getDimensionById(String id) {
-        for (BIDimension dimension : this.getDimensions()) {
-            if (ComparatorUtils.equals(dimension.getValue(), id)) {
-                return dimension;
-            }
-        }
-        return null;
-    }
-
-    public BISummaryTarget getTargetById(String id) {
-        for (BISummaryTarget target : this.getTargets()) {
-            if (ComparatorUtils.equals(target.getValue(), id)) {
-                return target;
-            }
-        }
-        return null;
-    }
-
-    public boolean isDimensionUsable(String id) {
-        BIDimension dimension = getDimensionById(id);
-        if (dimension != null) {
-            return dimension.isUsed();
-        }
-        BISummaryTarget target = getTargetById(id);
-        if (target != null) {
-            return target.isUsed();
-        }
-        return false;
-    }
-
-    public List<BISummaryTarget> getUsedTargets() {
-        List<BISummaryTarget> targets = new ArrayList<BISummaryTarget>();
-        BISummaryTarget[] sourceTarget = this.getTargets();
-        for (int i = 0; i < sourceTarget.length; i++) {
-            if (sourceTarget[i].isUsed()) {
-                targets.add(sourceTarget[i]);
-            }
-        }
-        return targets;
-    }
-
-    public Integer getRegionTypeByDimension(BIDimension dimension) {
-        for (Map.Entry<Integer, List<String>> entry : view.entrySet()) {
-            if (entry.getKey() <= Integer.parseInt(BIReportConstant.REGION.DIMENSION2)) {
-                Integer key = entry.getKey();
-                List<String> dIds = entry.getValue();
-                if (dIds.contains(dimension.getValue())) {
-                    return key;
-                }
-            }
-        }
-        return null;
-    }
-
-    public Integer getRegionTypeByTarget(BISummaryTarget target) {
-        for (Map.Entry<Integer, List<String>> entry : view.entrySet()) {
-            if (entry.getKey() >= Integer.parseInt(BIReportConstant.REGION.TARGET1)) {
-                Integer key = entry.getKey();
-                List<String> dIds = entry.getValue();
-                if (dIds.contains(target.getValue())) {
-                    return key;
-                }
-            }
-        }
-        return null;
-    }
 
     public JSONObject getPostOptions(String sessionId) throws Exception {
-        JSONObject dataJSON = this.createDataJSON((BISessionProvider) SessionDealWith.getSessionIDInfor(sessionId));
-        JSONObject chartOptions = createOptions(dataJSON.optJSONObject("data"));
+        JSONObject chartOptions = this.createDataJSON((BISessionProvider) SessionDealWith.getSessionIDInfor(sessionId));
         JSONObject plotOptions = chartOptions.optJSONObject("plotOptions");
         plotOptions.put("animation", false);
         chartOptions.put("plotOptions", plotOptions);
         return chartOptions;
     }
 
-    public JSONObject createDataJSON(BISessionProvider session) throws Exception {
-        //用来做测试
-        return new JSONObject("{\n" +
-                "                    \"plotOptions\":{\n" +
-                "\n" +
-                "                    },\n" +
-                "                    \"chartType\": \"column\",\n" +
-                "\n" +
-                "                    \"series\": [\n" +
-                "                        {\n" +
-                "                            \"data\": [\n" +
-                "                                {\n" +
-                "                                    \"x\": \"孙林\",\n" +
-                "                                    \"y\": 140\n" +
-                "                                }\n" +
-                "                            ],\n" +
-                "                            \"name\": \"苹果汁\"\n" +
-                "                        }\n" +
-                "                    ]\n" +
-                "                };");
+
+    public abstract JSONArray createSeries(JSONObject data) throws JSONException;
+
+    public JSONObject createOptions() throws JSONException{
+        JSONObject options = JSONObject.create();
+        JSONObject settings = this.getChartSetting().getDetailChartSetting();
+
+        if(settings.has("chartColor")){
+            options.put("colors", settings.getJSONArray("chartColor"));
+        }
+
+        if(settings.getInt("chartStyle") == STYLE_GRADULA){
+            options.put("style", "gradual");
+        }
+
+
+        return options;
     }
 
-    public JSONObject createOptions(JSONObject data){
-        return JSONObject.create();
+    public String getSeriesType(){
+        return "column";
     }
 
-    protected JSONArray createXYSeries(JSONObject data) throws JSONException{
+    protected JSONArray createXYSeries(JSONObject originData) throws JSONException{
         JSONArray series = JSONArray.create();
-        BIDimension categoryDimension = this.getCategoryDimension();
-        BIDimension seriesDimension = this.getSeriesDimension();
-        IGroup categoryGroup = categoryDimension != null ? categoryDimension.getGroup() : new NoGroup();
-        IGroup seriesGroup = seriesDimension != null ? seriesDimension.getGroup() : new NoGroup();
-
+        String type = this.getSeriesType();
+        if (originData.has("t")) {
+            JSONObject top = originData.getJSONObject("t"), left = originData.getJSONObject("l");
+            JSONArray topC = top.getJSONArray("c"), leftC = left.getJSONArray("c");
+            for (int i = 0; i < topC.length(); i++) {
+                JSONObject tObj = topC.getJSONObject(i);
+                String name = tObj.getString("n");
+                JSONArray data = JSONArray.create();
+                for (int j = 0; j < leftC.length(); j++) {
+                    JSONObject lObj = leftC.getJSONObject(j);
+                    String x = lObj.getString("n");
+                    double y = lObj.getJSONObject("s").getJSONArray("c").getJSONObject(i).getJSONArray("s").getDouble(0);
+                    data.put(JSONObject.create().put("x", x).put("y", y));
+                }
+                series.put(JSONObject.create().put("data", data).put("name", name).put("type", type));
+            }
+        }
         return series;
     }
 
--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanColumnWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanColumnWidget.java
@@ -1,7 +1,16 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
+import com.fr.json.JSONArray;
+import com.fr.json.JSONException;
+import com.fr.json.JSONObject;
+
 /**
  * Created by eason on 2017/2/27.
  */
-public class VanColumnWidget {
+public class VanColumnWidget extends VanCartesianWidget{
+
+    public JSONArray createSeries(JSONObject data) throws JSONException{
+        return createXYSeries(data);
+    }
+
 }
