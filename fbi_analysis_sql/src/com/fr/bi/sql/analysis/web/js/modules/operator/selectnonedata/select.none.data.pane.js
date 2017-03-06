40bb5a658331e3c2bce43f167a376fe2e043b85d
pony <pony@finereport.com>
2017-03-06 10:04:11 +0800

    这个没用了

--- fbi_analysis_sql/src/com/fr/bi/sql/analysis/web/js/modules/operator/selectnonedata/select.none.data.pane.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 小灰灰 on 2016/4/15.
- */
-BI.AnalysisETLOperatorSelectNoneDataPane = BI.inherit(BI.Widget, {
-    populate : function (model, controller) {
-    }
-})
-$.shortcut(SQLCst.ANALYSIS_ETL_PAGES.SELECT_NONE_DATA +  SQLCst.ANALYSIS_TABLE_PANE, BI.AnalysisETLOperatorSelectNoneDataPane);
-$.shortcut(SQLCst.ANALYSIS_ETL_PAGES.SELECT_DATA +  SQLCst.ANALYSIS_TABLE_PANE, BI.AnalysisETLOperatorSelectNoneDataPane);
