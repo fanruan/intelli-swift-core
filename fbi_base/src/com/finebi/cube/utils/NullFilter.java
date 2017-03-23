35fdd493760afa862173282886637bec3ded7cce
lucifer <781562184@qq.com>
2017-03-23 15:42:16 +0800

    no message

--- fbi_base/src/com/finebi/cube/utils/NullFilter.java
+++ fbi_base/src/com/finebi/cube/utils/NullFilter.java
@@ -20,27 +20,4 @@ public class NullFilter {
         }
         return set;
     }
-
-    public static void main(String[] args) {
-        Set<String> set = new HashSet<String>();
-        List<String> list = new ArrayList<String>();
-        set.add("1");
-        set.add("2");
-        set.add(null);
-        set.add("3");
-        set.add(null);
-
-        list.add("1");
-        list.add("2");
-        list.add(null);
-        list.add("3");
-        list.add(null);
-
-        System.out.println(set.size());
-        System.out.println(list.size());
-
-        list = (List<String>) filterCollection(list);
-        System.out.println(filterCollection(set).size());
-        System.out.println(list.size());
-    }
 }
