3435bc41cc4eedac2f57764869385464f3aed55b
neil.wang <neil.wang@finereport.com>
2017-03-26 19:15:39 -0700

    demo

---
+++ fbi_base/test/com/fr/bi/common/persistent/writer/XMLRenameDemo.java
@@ -0,0 +1,41 @@
+package com.fr.bi.common.persistent.writer;
+
+import com.fr.bi.common.world.people.Person;
+import com.fr.bi.common.world.people.PersonOld1;
+import com.fr.bi.stable.utils.program.BIConstructorUtils;
+import junit.framework.TestCase;
+
+/**
+ * Created by neil on 17-3-26.
+ */
+public class XMLRenameDemo extends TestCase{
+
+    public void testDemoRename() throws Exception{
+        writeObjectToXml();
+        renameClassAndFieldName();
+        Object o = readNewObjectFromXml();
+
+        assertTrue(o instanceof Person);
+        assertTrue("ChenHe".equals(((Person)o).getName()));
+        assertTrue(35 == ((Person)o).getAge());
+        assertTrue(false == ((Person)o).getMale());
+    }
+
+    private void writeObjectToXml() {
+        XMLWriterTest.generate(PersonOld1.getChenHe(), "PersonRenameDemo");
+    }
+
+    private void renameClassAndFieldName() {
+        //Do nothing
+        //这里建了新的Person 并且字段nameOld改成了name
+    }
+
+    private Object readNewObjectFromXml() throws Exception{
+        Class objClass = Class.forName(Person.class.getName());
+        Object o;
+        o = BIConstructorUtils.forceConstructObject(objClass);
+        o = XMLWriterTest.get(o, "PersonRenameDemo");
+        return o;
+    }
+
+}
