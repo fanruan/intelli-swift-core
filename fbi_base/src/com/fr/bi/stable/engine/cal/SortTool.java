bf6265e0918d7eab71a29e0a0cc86a6c6383ef38
pony <pony@finereport.com>
2017-03-28 17:51:09 +0800

    bug

--- fbi_base/src/com/fr/bi/stable/engine/cal/SortTool.java
+++ fbi_base/src/com/fr/bi/stable/engine/cal/SortTool.java
@@ -4,5 +4,5 @@ package com.fr.bi.stable.engine.cal;
  * Created by 小灰灰 on 2016/8/1.
  */
 public enum SortTool {
-    ALL_SHOW, TREE_MAP, DIRECT, TREE_MAP_RE_SORT, INT_ARRAY, INT_ARRAY_RE_SORT, ORIGIN
+    ALL_SHOW, TREE_MAP, DIRECT, EMPTY, TREE_MAP_RE_SORT, INT_ARRAY, INT_ARRAY_RE_SORT
 }
