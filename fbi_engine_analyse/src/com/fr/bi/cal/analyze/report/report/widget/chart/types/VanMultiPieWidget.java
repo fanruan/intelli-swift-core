555015dcb1a2827568f3deb6c15b4e33c7ffd152
eason-skx <eason.skx@gmail.com>
2017-04-03 16:27:49 +0800

    多层饼图的配置

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
@@ -1,5 +1,6 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
+import com.fr.bi.stable.constant.BIReportConstant;
 import com.fr.json.JSONArray;
 import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
@@ -30,10 +31,55 @@ public class VanMultiPieWidget extends VanPieWidget{
         return plotOptions;
     }
 
-    public JSONArray createSeries(JSONObject data) throws Exception{
+    public JSONArray createSeries(JSONObject originData) throws Exception{
 
+        JSONArray series = JSONArray.create();
+        String[] targetIDs = this.getUsedTargetID();
 
-        return JSONArray.create();
+        if(!originData.has("c") || targetIDs.length < 1){
+            return series;
+        }
+
+        JSONArray data = this.createChildren(originData);
+
+        series.put(JSONObject.create().put("data", data).put("name", this.getDimensionNameByID(targetIDs[0])).put("dimensionID", targetIDs[0]));
+
+        return series;
+    }
+
+    private JSONArray createChildren(JSONObject originData) throws JSONException {
+        JSONArray children = JSONArray.create();
+
+        if(!originData.has("c")){
+            return children;
+        }
+
+        JSONArray rawChildren = originData.optJSONArray("c");
+
+        for(int i = 0, dataCount = rawChildren.length(); i < dataCount; i++){
+            JSONObject item = rawChildren.getJSONObject(i);
+            double value = item.getJSONArray("s").getDouble(0);
+            String name =  item.optString("n");
+            JSONObject datum = JSONObject.create().put("name", name).put("value", value);
+            if(item.has("c")){
+                datum.put("children", this.createChildren(item));
+            }
+            children.put(datum);
+        }
+
+        return children;
+    }
+
+    protected int numberLevel(String dimensionID){
+        return BIReportConstant.TARGET_STYLE.NUM_LEVEL.NORMAL;
+    }
+
+    protected void formatSeriesTooltipFormat(JSONObject options) throws Exception{
+
+    }
+
+    protected void formatSeriesDataLabelFormat(JSONObject options) throws Exception{
+
     }
 
     public String getSeriesType(String dimensionID){
