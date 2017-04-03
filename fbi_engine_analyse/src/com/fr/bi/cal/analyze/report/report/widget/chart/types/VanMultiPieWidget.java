7eb3d351b93f4320a169ff30750d148bd5aa9a6b
eason-skx <eason.skx@gmail.com>
2017-04-03 15:10:59 +0800

    饼图的配置

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
@@ -1,14 +1,12 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
-import com.fr.bi.cal.analyze.report.report.widget.VanChartWidget;
 import com.fr.json.JSONArray;
-import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
 
 /**
  * Created by eason on 2017/3/22.
  */
-public class VanMultiPieWidget extends VanChartWidget{
+public class VanMultiPieWidget extends VanPieWidget{
 
     public JSONArray createSeries(JSONObject data) throws Exception{
 
