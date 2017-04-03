34ecfb22e74a78afae7aa22b8b25b2be38d8bd0f
eason-skx <eason.skx@gmail.com>
2017-04-03 15:16:59 +0800

    多层饼图的配置

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanMultiPieWidget.java
@@ -1,12 +1,34 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
 import com.fr.json.JSONArray;
+import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
 
 /**
  * Created by eason on 2017/3/22.
  */
 public class VanMultiPieWidget extends VanPieWidget{
+
+    private static final int LIGHTER = 1;
+    private static final int DARKER = 2;
+
+    protected JSONObject populateDefaultSettings() throws JSONException {
+        JSONObject settings = super.populateDefaultSettings();
+
+        settings.put("gradientType", LIGHTER);
+
+        return settings;
+    }
+
+    public JSONObject createPlotOptions(JSONObject globalStyle, JSONObject settings) throws Exception{
+        JSONObject plotOptions = super.createPlotOptions(globalStyle, settings);
+
+        plotOptions.put("gradual", settings.optInt("gradientType") == LIGHTER ? "lighter" : "darker");
+
+        plotOptions.put("drilldown", true);
+
+        return plotOptions;
+    }
 
     public JSONArray createSeries(JSONObject data) throws Exception{
 
