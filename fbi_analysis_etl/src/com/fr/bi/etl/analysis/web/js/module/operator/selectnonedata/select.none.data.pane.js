1ce4ceba02a509aa059728c32f970f69e797db92
windy <1374721899@qq.com>
2017-04-13 09:06:49 +0800

    update

--- fbi_analysis_etl/src/com/fr/bi/etl/analysis/web/js/module/operator/selectnonedata/select.none.data.pane.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by windy on 2017/4/11.
- */
-BI.AnalysisETLOperatorSelectNoneDataPane = BI.inherit(BI.Widget, {
-    populate : function (m, options) {
-    }
-})
-BI.shortcut(ETLCst.ANALYSIS_ETL_PAGES.SELECT_NONE_DATA +  ETLCst.ANALYSIS_TABLE_PANE, BI.AnalysisETLOperatorSelectNoneDataPane);
-BI.shortcut(ETLCst.ANALYSIS_ETL_PAGES.SELECT_DATA +  ETLCst.ANALYSIS_TABLE_PANE, BI.AnalysisETLOperatorSelectNoneDataPane);
