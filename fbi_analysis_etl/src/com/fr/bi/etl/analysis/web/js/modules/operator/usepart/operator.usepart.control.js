cb9141376bc3e1373a3d1181722587d106f42c4b
richie <richie@fanruan.com>
2017-04-18 10:16:00 +0800

    JS移动到web目录下

--- fbi_analysis_etl/src/com/fr/bi/etl/analysis/web/js/modules/operator/usepart/operator.usepart.control.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by 小灰灰 on 2016/4/18.
- */
-BI.AnalysisETLOperatorUsePartController = BI.inherit(BI.AnalysisETLOperatorAbstractController, {
-    _init : function() {
-        BI.AnalysisETLOperatorUsePartController.superclass._init.apply(this, arguments);
-        this._editing = false;
-    }
-})
