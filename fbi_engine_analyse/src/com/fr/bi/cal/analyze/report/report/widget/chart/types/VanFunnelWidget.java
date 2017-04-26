4974700fa145b67a97740218b2668222fefc59d0
eason-skx <eason.skx@gmail.com>
2017-04-26 11:04:12 +0800

    funnel配置

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanFunnelWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanFunnelWidget.java
@@ -1,14 +1,25 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
 import com.fr.bi.cal.analyze.report.report.widget.VanChartWidget;
-import com.fr.json.JSONArray;
-import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
 
 /**
  * Created by eason on 2017/2/27.
  */
 public class VanFunnelWidget extends VanChartWidget{
+
+    private static final int SAME_ANGLE = 1;
+
+    public JSONObject createPlotOptions(JSONObject globalStyle, JSONObject settings) throws Exception {
+
+        JSONObject plotOptions = super.createPlotOptions(globalStyle, settings);
+
+        plotOptions.put("sort", false);
+
+        plotOptions.put("useSameSlantAngle", settings.optInt("slantStyle", SAME_ANGLE) == SAME_ANGLE);
+
+        return plotOptions;
+    }
 
     protected String categoryKey(){
         return "name";
