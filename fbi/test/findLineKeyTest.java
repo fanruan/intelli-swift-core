5b1562bf9253ca5d188deb82f404e5f93ba83de5
kary <kary@finereport.com>
2017-05-03 17:06:38 +0800

    no message

--- fbi/test/findLineKeyTest.java
+++ fbi/test/findLineKeyTest.java
@@ -1,15 +1,11 @@
 import com.fr.bi.stable.utils.file.BIFileUtils;
 import com.fr.bi.stable.utils.program.BINonValueUtils;
-import com.fr.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,27 +13,45 @@ import java.util.regex.Pattern;
  * Created by Kary on 2017/5/3.
  */
 public class findLineKeyTest {
-    private static final String outputPath="C:\\code\\FineBI\\keys.json";
+    private static final String outputPath = "C:\\code\\FineBI\\keys.json";
+
     public static void main(String[] args) {
         List<String> rootDirs = setRootDIrs();
         List<String> allFiles = getSelectedFiles(rootDirs);
-        Map<String, List<String>> allKeys = getAllKeysFromFiles(allFiles);
-        JSONObject keysJson = new JSONObject(allKeys);
-        BIFileUtils.writeFile(outputPath,keysJson.toString());
-
+//        Map<String, Set<String>> allKeys = getAllKeysFromFiles(allFiles);
+//        JSONObject keysJson = new JSONObject(allKeys);
+//        BIFileUtils.writeFile(outputPath,keysJson.toString());
+        replaceAllKeys(allFiles);
+//        String jsonStr = "new JSONObject().optJSONObject(\"table_id_count\").getJSONObject(\"column_size_count\");";
+//        String s = replaceKey(jsonStr);
+//        System.out.println(s);
     }
 
-    private static Map<String,List<String>> getAllKeysFromFiles(List<String> allFiles) {
-        Map<String,List<String>> container=new HashMap<String, List<String>>();
+    private static void replaceAllKeys(List<String> allFiles) {
+        Map<String, Set<String>> container = new HashMap<String, Set<String>>();
         for (String file : allFiles) {
-            List<String> keys=new ArrayList<String>();
+            System.out.println(file);
+            StringBuilder sb=new StringBuilder();
             List<String> lines = readFile(file);
             for (String line : lines) {
-               keys.addAll(findKey(line));
+                sb.append(replaceKey(line)+"\n");
             }
+            BIFileUtils.writeFile(file, sb.toString());
+        }
+    }
+
+    private static Map<String, Set<String>> getAllKeysFromFiles(List<String> allFiles) {
+        Map<String, Set<String>> container = new HashMap<String, Set<String>>();
+        for (String file : allFiles) {
+//            if (file.endsWith("test.java")){
+//                continue;
+//            }
+            List<String> keys = new ArrayList<String>();
+
+            keys.addAll(findKey(BIFileUtils.readFile(file)));
             for (String key : keys) {
-                if (!container.containsKey(key)){
-                    container.put(key,new ArrayList<String>());
+                if (!container.containsKey(key)) {
+                    container.put(key, new HashSet<String>());
                 }
                 container.get(key).add(new File(file).getName());
             }
@@ -75,17 +89,41 @@ public class findLineKeyTest {
         return keys;
     }
 
+    private static String replaceKey(String oriContent) {
+        String finalReg = createRegStr();
+        Pattern linePattern = Pattern.compile(finalReg);
+        Matcher m = linePattern.matcher(oriContent);
+        boolean result = m.find();
+        while (result) {
+            String camels = lineToCamels(m.group());
+            oriContent = oriContent.replace(m.group(), camels);
+            result = m.find();
+        }
+        return oriContent;
+    }
+
     private static String createRegStr() {
-        String normalJsonGetStr="get|getString|getInt|getLong|getDouble|getFloat|getBoolean|getJSONObject|getJsonArray";
-        String optJsonGetStr="opt|optString|optInt|optLong|optDouble|optFloat|optBoolean|optJSONObject|optJSONArray|opt";
-        String otherJsonOpeStr="put|putOpt|remove|has";
-        String optionJsonStr=normalJsonGetStr+"|"+optJsonGetStr+"|"+otherJsonOpeStr;
-        return "("+optionJsonStr+")\\(\"([a-z]{1,}_[\\w]{1,})\"";
+        String normalJsonGetStr = "get|getString|getInt|getLong|getDouble|getFloat|getBoolean|getJSONObject|getJsonArray";
+        String optJsonGetStr = "opt|optString|optInt|optLong|optDouble|optFloat|optBoolean|optJSONObject|optJSONArray|opt";
+        String otherJsonOpeStr = "put|putOpt|remove|has";
+        String optionJsonStr = normalJsonGetStr + "|" + optJsonGetStr + "|" + otherJsonOpeStr;
+        return "(" + optionJsonStr + ")\\(\"([a-z]{1,}_[\\w]{1,})\"";
+    }
+
+    private static String lineToCamels(String str) {
+        Pattern linePattern = Pattern.compile("(?!^)_(\\w)");
+        Matcher matcher = linePattern.matcher(str);
+        StringBuffer sb = new StringBuffer();
+        while (matcher.find()) {
+            matcher.appendReplacement(sb, matcher.group(1).toUpperCase());
+        }
+        matcher.appendTail(sb);
+        return sb.toString();
     }
 
     public static List<String> readFile(String path) {
         File file = new File(path);
-        List<String> lines=new ArrayList<String>();
+        List<String> lines = new ArrayList<String>();
         try {
             FileReader fr = new FileReader(file);
             BufferedReader reader = new BufferedReader(fr);
@@ -102,4 +140,5 @@ public class findLineKeyTest {
         } catch (IOException e) {
             throw BINonValueUtils.beyondControl(e);
         }
-    }}
+    }
+}
