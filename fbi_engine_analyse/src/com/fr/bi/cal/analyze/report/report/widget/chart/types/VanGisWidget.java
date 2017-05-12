f99788efc44adba58705eb457c1d094e9bcc937c
fr_shine <zheng@finereport.com>
2017-05-12 14:58:02 +0800

    gis map label name

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanGisWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanGisWidget.java
@@ -47,7 +47,12 @@ public class VanGisWidget extends VanChartWidget{
                 String lnglat = lObj.getString("n");
                 JSONArray s = lObj.getJSONArray("s");
                 double value = s.isNull(i) ? 0 : s.getDouble(i);
-                data.put(JSONObject.create().put("lnglat", lnglat.split(",")).put("value", value / scale));
+                JSONObject d = JSONObject.create().put("lnglat", lnglat.split(",")).put("value", value / scale);
+                JSONArray c = lObj.optJSONArray("c");
+                if(c != null && c.length() > 0){
+                    d.put("name", c.optJSONObject(0).optString("n"));
+                }
+                data.put(d);
             }
             JSONObject ser = JSONObject.create().put("data", data).put("name", this.getDimensionNameByID(id))
                     .put("targetIDs", JSONArray.create().put(id))
@@ -56,6 +61,10 @@ public class VanGisWidget extends VanChartWidget{
         }
 
         return series;
+    }
+
+    protected String getTooltipIdentifier(){
+        return NAME + SERIES + VALUE;
     }
 
     public String getSeriesType(String dimensionID){
