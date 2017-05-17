6819ea773f36a81b448993fb81803aaed8dc040f
andrew.asa <andrew.asa@finereport>
2017-05-17 09:31:44 +0800

    pmd 函数长度修改

--- fbi_base/src/com/fr/bi/stable/data/key/date/BIDateValueFactory.java
+++ fbi_base/src/com/fr/bi/stable/data/key/date/BIDateValueFactory.java
@@ -16,47 +16,36 @@ public class BIDateValueFactory {
                 return new BISeasonValue(((Number) value).intValue());
             case BIReportConstant.GROUP.W:
                 return new BIWeekValue(((Number) value).intValue());
-            // 年月日
             case BIReportConstant.GROUP.YMD: {
                 return new BIDayValue(((Number) value).longValue());
             }
-            // 周数
             case BIReportConstant.GROUP.WEEK_COUNT: {
                 return new BIWeekCountValue(((Number) value).intValue());
             }
-            // 时
             case BIReportConstant.GROUP.HOUR: {
                 return new BIHourValue(((Number) value).intValue());
             }
-            // 分
             case BIReportConstant.GROUP.MINUTE: {
                 return new BIMinuteValue(((Number) value).intValue());
             }
-            // 秒
             case BIReportConstant.GROUP.SECOND: {
                 return new BISecondValue(((Number) value).intValue());
             }
-            // 年季度
             case BIReportConstant.GROUP.YS: {
                 return new BIYearSessionValue((String) value);
             }
-            // 年月
             case BIReportConstant.GROUP.YM: {
                 return new BIYearMonthValue((String) value);
             }
-            // 年周数
             case BIReportConstant.GROUP.YW: {
                 return new BIYearWeekCountValue((String) value);
             }
-            // 年月日时
             case BIReportConstant.GROUP.YMDH: {
                 return new BIYearMonthDayHourValue(((Number) value).longValue());
             }
-            // 年月日时分
             case BIReportConstant.GROUP.YMDHM: {
                 return new BIYearMonthDayHourMinuteValue(((Number) value).longValue());
             }
-            // 年月日时秒
             case BIReportConstant.GROUP.YMDHMS: {
                 return new BIYearMonthDayHourMinuteSecondValue(((Number) value).longValue());
             }
