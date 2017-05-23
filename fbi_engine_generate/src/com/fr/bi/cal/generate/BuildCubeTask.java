d7c50d85a3e98befc16dc4c67be23d4048ef695f
young <young@finereport.com>
2017-05-23 17:21:55 +0800

    BI-5479

--- fbi_engine_generate/src/com/fr/bi/cal/generate/BuildCubeTask.java
+++ fbi_engine_generate/src/com/fr/bi/cal/generate/BuildCubeTask.java
@@ -472,7 +472,7 @@ public class BuildCubeTask implements CubeTask {
 
     @Override
     public long getUserId() {
-        return UserControl.getInstance().getSuperManagerID();
+        return biUser.getUserId();
     }
 
     @Override
