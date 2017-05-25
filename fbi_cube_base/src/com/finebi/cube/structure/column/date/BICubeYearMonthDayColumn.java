dc6b4ffd0e0a156be20ae019695f15fad057e689
andrew.asa <andrew.asa@finereport>
2017-05-25 17:25:41 +0800

    BI-5644 && BI-2894 && BI-26 时间相关问题

--- fbi_cube_base/src/com/finebi/cube/structure/column/date/BICubeYearMonthDayColumn.java
+++ fbi_cube_base/src/com/finebi/cube/structure/column/date/BICubeYearMonthDayColumn.java
@@ -6,6 +6,7 @@ import com.finebi.cube.structure.column.BICubeLongColumn;
 import com.fr.bi.base.ValueConverterFactory;
 import com.fr.bi.stable.constant.DateConstant;
 import com.fr.bi.stable.io.newio.NIOConstant;
+import com.fr.bi.stable.utils.BICollectionUtils;
 
 import java.util.Calendar;
 
@@ -22,12 +23,22 @@ public class BICubeYearMonthDayColumn extends BICubeDateSubColumn<Long> {
 
     @Override
     protected Long convertDate(Long date) {
-        return date != null ? (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date) : null;
+        //return date != null ? (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date) : null;
+        if(BICollectionUtils.isCubeNullKey(date)){
+            return NIOConstant.LONG.NULL_VALUE;
+        }else{
+            return (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date) ;
+        }
     }
 
     @Override
     protected Long convertDate(Long date, Calendar calendar) {
-        return date != null ? (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date, calendar) : null;
+        //return date != null ? (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date, calendar) : null;
+        if(BICollectionUtils.isCubeNullKey(date)){
+            return NIOConstant.LONG.NULL_VALUE;
+        }else{
+            return (Long) ValueConverterFactory.createDateValueConverter(DateConstant.DATE.YMD).result2Value(date, calendar) ;
+        }
     }
 
     @Override
