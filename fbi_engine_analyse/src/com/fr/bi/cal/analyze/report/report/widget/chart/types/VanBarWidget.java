b7ffde9edd7ef6d27141046344fae1ec35dcc5bd
fr_shine <zheng@finereport.com>
2017-06-12 15:01:46 +0800

    bar yaxis right and xaxis reversed

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanBarWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/types/VanBarWidget.java
@@ -1,5 +1,9 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.types;
 
+import com.fr.json.JSONArray;
+import com.fr.json.JSONException;
+import com.fr.json.JSONObject;
+
 /**
  * Created by eason on 2017/2/27.
  */
@@ -7,6 +11,16 @@ public class VanBarWidget extends VanColumnWidget{
 
     public boolean isInverted(){
         return true;
+    }
+
+    protected JSONArray parseCategoryAxis(JSONObject settings) throws JSONException {
+        JSONArray array = super.parseCategoryAxis(settings);
+        array.optJSONObject(0).put("reversed", true);
+        return array;
+    }
+
+    protected JSONObject parseLeftValueAxis(JSONObject settings) throws JSONException {
+        return super.parseLeftValueAxis(settings).put("position", "right");
     }
 
 }
