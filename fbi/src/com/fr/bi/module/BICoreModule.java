201ba3d6c3dcb015054ed7588b8f31d501171ed6
DESKTOP-QVKS9CG\Young <young@finereport.com>
2017-06-16 09:42:53 +0800

    BI平台

--- fbi/src/com/fr/bi/module/BICoreModule.java
+++ fbi/src/com/fr/bi/module/BICoreModule.java
@@ -59,18 +59,14 @@ import com.fr.bi.fs.HSQLBIReportDAO;
 import com.fr.bi.fs.TableDataBIReportDAO;
 import com.fr.bi.fs.BITableDataDAOProvider;
 import com.fr.bi.fs.BITableDataDAOManager;
-import com.fr.bi.resource.BaseResourceHelper;
-import com.fr.bi.resource.CommonResourceHelper;
-import com.fr.bi.resource.ConfResourceHelper;
-import com.fr.bi.resource.DeziResourceHelper;
-import com.fr.bi.resource.ResourceConstants;
-import com.fr.bi.resource.ShowResourceHelper;
+import com.fr.bi.resource.*;
 import com.fr.bi.stable.utils.BIDBUtils;
 import com.fr.bi.tool.BIReadReportProvider;
 import com.fr.bi.util.BIReadReportUtils;
 import com.fr.bi.web.base.Service4BIBase;
 import com.fr.bi.web.conf.Service4BIConfigure;
 import com.fr.bi.web.dezi.web.Service4BIDezi;
+import com.fr.bi.web.plate.Service4BIPlate;
 import com.fr.bi.web.report.Service4BIReport;
 import com.fr.bi.web.report.services.finecube.Service4FineCube;
 import com.fr.bi.web.report.utils.BIFSReportManager;
@@ -651,6 +647,9 @@ public class BICoreModule extends AbstractModule {
     }
 
     private void registerResources() {
+        StableFactory.registerJavaScriptFiles(ResourceConstants.DEFAULT_BIPlate_JS, BIPlateResourceHelper.getBIPlateJS());
+        StableFactory.registerStyleFiles(ResourceConstants.DEFAULT_BIPlate_CSS, BIPlateResourceHelper.getBIPlateCSS());
+
         StableFactory.registerJavaScriptFiles(ResourceConstants.DEFAULT_THIRD_JS, BaseResourceHelper.getThirdJs());
         StableFactory.registerJavaScriptFiles(ResourceConstants.DEFAULT_MAP_JS, BaseResourceHelper.getMapJS(), BaseResourceHelper.MapTransmitter);
         StableFactory.registerJavaScriptFiles(ResourceConstants.DEFAULT_BASE_JS, BaseResourceHelper.getBaseJs());
@@ -676,6 +675,7 @@ public class BICoreModule extends AbstractModule {
     }
 
     public void loadResources(Locale[] locales) {
+        com.fr.web.ResourceHelper.forceInitJSCache(ResourceConstants.DEFAULT_BIPlate_JS);
         com.fr.web.ResourceHelper.forceInitJSCache(ResourceConstants.DEFAULT_THIRD_JS);
         com.fr.web.ResourceHelper.forceInitJSCache(ResourceConstants.DEFAULT_BASE_JS);
         com.fr.web.ResourceHelper.forceInitJSCache(ResourceConstants.DEFAULT_DESIGN_JS);
@@ -686,6 +686,7 @@ public class BICoreModule extends AbstractModule {
 
         BaseResourceHelper.FormulaTransmitter.transmit(BaseResourceHelper.getFormulaCollectionJS(), locales);
 
+        com.fr.web.ResourceHelper.forceInitStyleCache(ResourceConstants.DEFAULT_BIPlate_CSS);
         com.fr.web.ResourceHelper.forceInitStyleCache(ResourceConstants.DEFAULT_THIRD_CSS);
         com.fr.web.ResourceHelper.forceInitStyleCache(ResourceConstants.DEFAULT_BASE_CSS);
         com.fr.web.ResourceHelper.forceInitStyleCache(ResourceConstants.DEFAULT_DESIGN_CSS);
@@ -721,6 +722,7 @@ public class BICoreModule extends AbstractModule {
                 new Service4BIDezi(),
                 new Service4BIBase(),
                 new Service4FineCube(),
+                new Service4BIPlate()
         };
     }
 
