ada75c6512ab6e79b9dd66fb3d3b26f711291e4d
fr_shine <zheng@finereport.com>
2017-06-26 09:37:56 +0800

    gauge ring label

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
@@ -64,7 +64,7 @@ public abstract class VanChartWidget extends TableWidget {
 
     protected static final String PERCENT_SYMBOL = "%";
     private static final String WHITE = "#ffffff";
-    private static final String DARK = "#1a1a1a";
+    protected static final String DARK = "#1a1a1a";
 
     private static final int WEEK_COUNT = 52;
     private static final int MONTH_COUNT = 12;
