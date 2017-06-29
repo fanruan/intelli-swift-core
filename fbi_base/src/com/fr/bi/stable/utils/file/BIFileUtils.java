54be0e6497a7a61dd10f1b1429dd57ae8342b9ad
kary <kary@finereport.com>
2017-06-29 15:10:22 +0800

    BI-3770 系统参数

--- fbi_base/src/com/fr/bi/stable/utils/file/BIFileUtils.java
+++ fbi_base/src/com/fr/bi/stable/utils/file/BIFileUtils.java
@@ -366,11 +366,4 @@ public class BIFileUtils {
         }
         return src.renameTo(dest);
     }
-
-    public static boolean renameFile(File oldName, File newName) throws IOException {
-        if (!oldName.isFile() || !oldName.exists()) {
-            return false;
-        }
-        return oldName.renameTo(newName);
-    }
 }
