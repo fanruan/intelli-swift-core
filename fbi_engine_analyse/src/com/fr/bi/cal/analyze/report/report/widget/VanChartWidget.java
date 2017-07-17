a4d28debfbac0bb0fe37722fb94028dba6fd2f1f
imp <imp@fanruan.com>
2017-07-17 19:47:56 +0800

    update

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/VanChartWidget.java
@@ -1,6 +1,6 @@
 package com.fr.bi.cal.analyze.report.report.widget;
 
-import com.fr.bi.cal.analyze.report.report.BIWidgetUtils;
+import com.fr.bi.conf.report.widget.BIWidgetUtils;
 import com.fr.bi.conf.report.conf.BIWidgetConfUtils;
 import com.fr.bi.conf.session.BISessionProvider;
 import com.fr.bi.stable.constant.BIReportConstant;
