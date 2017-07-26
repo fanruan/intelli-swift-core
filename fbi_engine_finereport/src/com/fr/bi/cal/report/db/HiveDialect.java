624d6ebfd8b55a6dcc34bcc097043b1893e9b1fa
Roy <roy@RoydeMBP.(none)>
2017-07-26 13:33:46 +0800

    数据连接测试

--- fbi_engine_finereport/src/com/fr/bi/cal/report/db/HiveDialect.java
+++ fbi_engine_finereport/src/com/fr/bi/cal/report/db/HiveDialect.java
@@ -1,12 +1,11 @@
 package com.fr.bi.cal.report.db;
 
-import com.fr.data.core.db.dialect.AbstractDialect;
 import com.fr.data.core.db.dml.Table;
 
 /**
  * Created by roy on 2016/12/21.
  */
-public class HiveDialect extends AbstractDialect {
+public class HiveDialect extends com.fr.data.core.db.dialect.HiveDialect {
     @Override
     public String getTopNRowSql(int row, Table table) {
         return "select * from " + this.table2SQL(table) + " limit 0, " + row;
