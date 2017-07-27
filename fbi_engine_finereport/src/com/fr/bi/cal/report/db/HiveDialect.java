3a95cd75e6fcf18b18e1ae95868f3a97099ecfe4
Roy <roy@RoydeMBP.(none)>
2017-07-27 14:44:51 +0800

    无JIRA任务 HIVE数据库测试连接失败

--- fbi_engine_finereport/src/com/fr/bi/cal/report/db/HiveDialect.java
+++ fbi_engine_finereport/src/com/fr/bi/cal/report/db/HiveDialect.java
@@ -2,6 +2,8 @@ package com.fr.bi.cal.report.db;
 
 import com.fr.data.core.db.dml.Table;
 
+import java.sql.Connection;
+
 /**
  * Created by roy on 2016/12/21.
  */
@@ -9,5 +11,10 @@ public class HiveDialect extends com.fr.data.core.db.dialect.HiveDialect {
     @Override
     public String getTopNRowSql(int row, Table table) {
         return "select * from " + this.table2SQL(table) + " limit 0, " + row;
+    }
+
+    @Override
+    public String defaultValidationQuery(Connection connection) {
+        return super.defaultValidationQuery(connection);
     }
 }
