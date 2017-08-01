500f3228a661502bff43055af97c5b58c5e3391f
kary <kary@finereport.com>
2017-08-01 13:50:37 +0800

    BI-7273 4.1后台数据样式分离

---
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/format/operation/BITableCellDateFormatOperation.java
@@ -0,0 +1,33 @@
+package com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.operation;
+
+import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.setting.ICellFormatSetting;
+import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.utils.BITableCellFormatHelper;
+import com.fr.json.JSONObject;
+
+/**
+ * Created by Kary on 2017/4/10.
+ */
+public class BITableCellDateFormatOperation extends BITableCellFormatOperation {
+    private int typeGroup;
+
+    public BITableCellDateFormatOperation(int groupType, ICellFormatSetting ICellFormatSetting) {
+        this.typeGroup = groupType;
+        iCellFormatSetting = ICellFormatSetting;
+    }
+
+    @Override
+    public String formatItemTextValues(String text) throws Exception {
+         JSONObject format = null != iCellFormatSetting ? iCellFormatSetting.createJSON() : new JSONObject();
+            return BITableCellFormatHelper.dateFormat(format, typeGroup, text);
+    }
+
+    @Override
+    public String formatHeaderText(String headerText) throws Exception {
+        return headerText;
+    }
+
+    @Override
+    protected String getTextAlign() {
+        return "left";
+    }
+}
---
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/format/operation/ITableCellFormatOperation.java
@@ -0,0 +1,12 @@
+package com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.operation;
+
+import com.fr.json.JSONObject;
+
+/**
+ * Created by Kary on 2017/5/12.
+ */
+public interface ITableCellFormatOperation {
+    String formatItemTextValues(String text) throws Exception;
+    JSONObject createItemTextStyle(String text) throws Exception;
+    String formatHeaderText(String headerText) throws Exception;
+}
