b666b43e119b8275ff1bb4bec121cfa394d506c1
pony <pony@finereport.com>
2017-08-10 09:10:17 +0800

    pmd

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/sssecret/GroupUtils.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/cal/sssecret/GroupUtils.java
@@ -310,8 +310,12 @@ public class GroupUtils {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
 
             NodeKey nodeKey = (NodeKey) o;
 
