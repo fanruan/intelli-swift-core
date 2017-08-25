0da36d004537d3d309b57be6e19272950a04472e
kary <kary@finereport.com>
2017-08-25 14:19:49 +0800

    BI-8434	【h5】移动端h5中的交叉表的指标为空的情况下没有显示图标标记，与PC端不一致

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/format/utils/BITableCellFormatHelper.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/format/utils/BITableCellFormatHelper.java
@@ -3,7 +3,6 @@ package com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.utils
 import com.finebi.cube.common.log.BILoggerFactory;
 import com.fr.bi.stable.constant.BIReportConstant;
 import com.fr.bi.stable.utils.program.BIStringUtils;
-import com.fr.general.ComparatorUtils;
 import com.fr.general.Inter;
 import com.fr.json.JSONArray;
 import com.fr.json.JSONException;
@@ -99,7 +98,7 @@ public class BITableCellFormatHelper {
     }
 
     public static String dateFormat(JSONObject format, int groupType, String text) throws JSONException {
-        if (StringUtils.isBlank(text) || ComparatorUtils.equals(text, NONE_VALUE)) {
+        if (StringUtils.isBlank(text) || !StableUtils.isNumber(text)) {
             return text;
         }
         JSONObject dateFormat = format.optJSONObject("dateFormat");
