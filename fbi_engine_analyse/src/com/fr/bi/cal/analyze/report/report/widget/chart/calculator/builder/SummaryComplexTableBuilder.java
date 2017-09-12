f73dab60d25c2f18086cf993c71b53d4278ed486
kary <kary@finereport.com>
2017-09-12 16:26:55 +0800

    BI-9167 hashMap无序导致的不同环境下数据展示效果不一致

--- fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/builder/SummaryComplexTableBuilder.java
+++ fbi_engine_analyse/src/com/fr/bi/cal/analyze/report/report/widget/chart/calculator/builder/SummaryComplexTableBuilder.java
@@ -1,12 +1,12 @@
 package com.fr.bi.cal.analyze.report.report.widget.chart.calculator.builder;
 
+import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.utils.BITableDimensionHelper;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.BIBasicTableItem;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.BITableHeader;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.ITableHeader;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.ITableItem;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.constructor.BISummaryDataConstructor;
 import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.item.constructor.DataConstructor;
-import com.fr.bi.cal.analyze.report.report.widget.chart.calculator.format.utils.BITableDimensionHelper;
 import com.fr.bi.conf.report.style.table.BITableStyleHelper;
 import com.fr.bi.conf.report.widget.BIWidgetStyle;
 import com.fr.bi.stable.constant.BIReportConstant;
@@ -16,9 +16,11 @@ import com.fr.json.JSONException;
 import com.fr.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by Kary on 2017/2/27.
@@ -384,7 +386,8 @@ public class SummaryComplexTableBuilder extends TableAbstractDataBuilder {
     //获取有效的行表头区域
     public List<JSONArray> getRowRegions() throws JSONException {
         List<JSONArray> rowRegions = new ArrayList<JSONArray>();
-        for (Integer regionId : dimAndTar.keySet()) {
+        Map<Integer, List<JSONObject>> dimAndTarMap = getSortedMap();
+        for (Integer regionId : dimAndTarMap.keySet()) {
             JSONArray temp = new JSONArray();
             if (BITableDimensionHelper.isDimensionRegion1ByRegionType(regionId)) {
                 List<JSONObject> list = dimAndTar.get(regionId);
@@ -434,7 +437,8 @@ public class SummaryComplexTableBuilder extends TableAbstractDataBuilder {
     //获取有效的列表头区域
     public List<JSONArray> getColRegions() throws JSONException {
         List<JSONArray> colRegions = new ArrayList<JSONArray>();
-        for (Integer regionId : dimAndTar.keySet()) {
+        Map<Integer, List<JSONObject>> dimAndTarMap = getSortedMap();
+        for (Integer regionId : dimAndTarMap.keySet()) {
             if (BITableDimensionHelper.isDimensionRegion2ByRegionType(regionId)) {
                 JSONArray array = new JSONArray();
                 for (JSONObject dIdJson : dimAndTar.get(regionId)) {
@@ -448,6 +452,21 @@ public class SummaryComplexTableBuilder extends TableAbstractDataBuilder {
             }
         }
         return colRegions;
+    }
+/*
+* BI-9167 hashMap无序导致的不同环境下数据展示效果不一致
+* */
+    private Map<Integer, List<JSONObject>> getSortedMap() {
+        Map<Integer, List<JSONObject>> dimAndTarMap = new TreeMap<Integer, List<JSONObject>>(
+                new Comparator<Integer>() {
+                    public int compare(Integer o1, Integer o2) {
+                        return o1 > o2 ? 1 : -1;
+                    }
+                });
+        for (int key : dimAndTar.keySet()) {
+            dimAndTarMap.put(key, dimAndTar.get(key));
+        }
+        return dimAndTarMap;
     }
 
     protected void refreshDimsInfo() throws Exception {
