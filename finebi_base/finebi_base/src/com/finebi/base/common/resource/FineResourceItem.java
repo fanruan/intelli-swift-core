aa1d46a72017b56fe424f9bf44eb0f4731235f34
kary <kary@finereport.com>
2017-12-27 15:17:16 +0800

    BI-14004 配置结构优化

---
+++ finebi_base/finebi_base/src/com/finebi/base/common/resource/FineResourceItem.java
@@ -0,0 +1,14 @@
+package com.finebi.base.common.resource;
+
+public interface FineResourceItem {
+
+    /**
+     * 资源名字
+     *
+     * @return
+     */
+    String getName();
+    String getId();
+    
+    
+}
