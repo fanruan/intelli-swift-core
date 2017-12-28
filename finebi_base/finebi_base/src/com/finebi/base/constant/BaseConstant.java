a77849fe4abe992fc3cf9d10d7ad53cfae37962e
andrew.asa <andrew.asa@fanruan.com>
2017-12-28 09:29:44 +0800

    调整

--- finebi_base/finebi_base/src/com/finebi/base/constant/BaseConstant.java
+++ finebi_base/finebi_base/src/com/finebi/base/constant/BaseConstant.java
@@ -2,4 +2,35 @@ package com.finebi.base.constant;
 
 public class BaseConstant {
 
+    /**
+     * 表类型
+     */
+    public static final class TABLETYPE {
+
+        /**
+         * 数据库表
+         */
+        public static final int DB = 1;
+
+        /**
+         * etl
+         */
+        public static final int ETL = 2;
+
+        /**
+         * sql
+         */
+        public static final int SQL = 3;
+
+        /**
+         * excel
+         */
+        public static final int EXCEL = 4;
+
+        /**
+         * server
+         */
+        public static final int SERVER = 5;
+
+    }
 }
