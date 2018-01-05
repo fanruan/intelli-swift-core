ebc3bbcb2408bc75d03c3d5df59e05a2e46366d8
andrew.asa <andrew.asa@fanruan.com>
2018-01-05 18:01:08 +0800

    国际化的引入

--- finebi_base/finebi_base/src/com/finebi/base/utils/data/io/FileUtils.java
+++ finebi_base/finebi_base/src/com/finebi/base/utils/data/io/FileUtils.java
@@ -3,7 +3,12 @@ package com.finebi.base.utils.data.io;
 import com.finebi.log.BILoggerFactory;
 import com.fr.stable.StableUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -35,5 +40,38 @@ public class FileUtils {
     public static void createDirs(File f) {
 
         StableUtils.mkdirs(f);
+    }
+
+    /**
+     * 一行一行读文件
+     *
+     * @return list集合
+     */
+    public static String readFile(String path) throws IOException {
+
+        File file = new File(path);
+        StringBuffer sb = new StringBuffer();
+        FileReader fr = new FileReader(file);
+        BufferedReader reader = new BufferedReader(fr);
+        String line;
+        try {
+            while ((line = reader.readLine()) != null)
+                sb.append(line);
+            return sb.toString();
+        } finally {
+            reader.close();
+            fr.close();
+        }
+    }
+
+    public static void writeFile(String path, String content) throws IOException {
+
+        File tmp = new File(path);
+        createFile(tmp);
+        FileWriter fileWriter = new FileWriter(tmp, false);
+        BufferedWriter bufferWriter = new BufferedWriter(fileWriter);
+        bufferWriter.write(content);
+        bufferWriter.close();
+        fileWriter.close();
     }
 }
