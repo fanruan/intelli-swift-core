f06a605670536bfe6acc9f754fc92c916ed0aa81
Each.Zhang <each.zhang@fanruan.com>
2018-02-01 16:27:32 +0800

    BI-14560 添加Swift的关联实现

--- swift_adaptor/src/com/finebi/conf/provider/SwiftRelationPathConfProvider.java
+++ swift_adaptor/src/com/finebi/conf/provider/SwiftRelationPathConfProvider.java
@@ -2,9 +2,17 @@ package com.finebi.conf.provider;
 
 import com.finebi.base.constant.FineEngineType;
 import com.finebi.conf.service.engine.relation.EngineRelationPathManager;
+import com.finebi.conf.structure.bean.table.FineBusinessTable;
 import com.finebi.conf.structure.path.FineBusinessTableRelationPath;
 import com.finebi.conf.structure.relation.FineBusinessTableRelation;
+import com.fr.general.ComparatorUtils;
+import com.fr.swift.conf.business.path.SwiftRelationPathDao;
+import com.fr.swift.conf.business.relation.SwiftRelationDao;
+import com.fr.swift.log.SwiftLogger;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.manager.ProviderManager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,40 +23,70 @@ import java.util.List;
  * @since Advanced FineBI Analysis 1.0
  */
 public class SwiftRelationPathConfProvider implements EngineRelationPathManager {
+    
+    // FIXME SwiftRelationPathDAO没有实现
+    private SwiftRelationDao businessRelationDAO;
+    private SwiftRelationPathDao businessPathDAO;
+    
+    private static final SwiftLogger LOGGER = SwiftLoggers.getLogger(ProviderManager.class);
+    
+    private String xmlFileName = "relation.xml";
 
     @Override
     public List<FineBusinessTableRelation> getAllRelations() {
-        return null;
+        List<FineBusinessTableRelation> allConfig = new ArrayList<FineBusinessTableRelation>();
+        allConfig.addAll(businessRelationDAO.getAllConfig());
+        return allConfig;
     }
 
     @Override
-    public List<FineBusinessTableRelation> getRelationsByTables(String primaryTableId, String foreignTableId) {
-        return null;
+    public List<FineBusinessTableRelation> getRelationsByTables(String primaryTableName, String foreignTableName) {
+        List<FineBusinessTableRelation> resultRelations = new ArrayList<FineBusinessTableRelation>();
+        List<FineBusinessTableRelation> allRelations = getAllRelations();
+        for (FineBusinessTableRelation relation : allRelations) {
+            String primaryTable = relation.getPrimaryBusinessTable().getName();
+            String foreignTable = relation.getForeignBusinessTable().getName();
+            if (ComparatorUtils.equals(primaryTableName, primaryTable) && ComparatorUtils.equals(foreignTableName, foreignTable)) {
+                resultRelations.add(relation);
+            }
+        }
+        return resultRelations;
     }
 
     @Override
-    public List<FineBusinessTableRelation> getRelationsByTableId(String tableId) {
-        return null;
+    public List<FineBusinessTableRelation> getRelationsByTableId(String tableName) {
+        List<FineBusinessTableRelation> resultRelations = new ArrayList<FineBusinessTableRelation>();
+        List<FineBusinessTableRelation> allRelations = getAllRelations();
+        for (FineBusinessTableRelation relation : allRelations) {
+            String primaryName = relation.getPrimaryBusinessTable().getName();
+            String foreignName = relation.getForeignBusinessTable().getName();
+            if (ComparatorUtils.equals(primaryName, tableName) || ComparatorUtils.equals(foreignName, tableName)) {
+                resultRelations.add(relation);
+            }
+        }
+        return resultRelations;
     }
 
     @Override
     public boolean addRelations(List<FineBusinessTableRelation> relations) {
-        return false;
+        return businessRelationDAO.saveConfigs(relations);
     }
 
     @Override
     public boolean updateRelations(List<FineBusinessTableRelation> relations) {
+        
         return false;
     }
 
     @Override
     public boolean removeRelations(List<FineBusinessTableRelation> relations) {
-        return false;
+        return businessRelationDAO.removeConfigs(relations);
     }
 
     @Override
     public List<FineBusinessTableRelationPath> getAllRelationPaths() {
-        return null;
+        
+        return new ArrayList<FineBusinessTableRelationPath>();
     }
 
     @Override
@@ -78,11 +116,20 @@ public class SwiftRelationPathConfProvider implements EngineRelationPathManager 
 
     @Override
     public boolean isRelationExist(List<FineBusinessTableRelation> relation) {
-        return false;
+        List<FineBusinessTableRelation> allRelations = getAllRelations();
+        return allRelations.containsAll(relation);
     }
 
     @Override
-    public boolean isRelationExist(String primaryTableId, String foreignTableId) {
+    public boolean isRelationExist(String primaryTableName, String foreignTableName) {
+        List<FineBusinessTableRelation> allRelations = getAllRelations();
+        for (FineBusinessTableRelation relation : allRelations) {
+            String primaryName = relation.getPrimaryBusinessTable().getName();
+            String foreignName = relation.getForeignBusinessTable().getName();
+            if (ComparatorUtils.equals(primaryName, primaryTableName) && ComparatorUtils.equals(foreignName, foreignTableName)) {
+                return true;
+            }
+        }
         return false;
     }
 
