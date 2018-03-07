550909c5fdec7599cd01dba313121a4fb0d0eead
LiuYingjun <LiuYingjun@fanruan.com>
2018-03-07 09:20:50 +0800

    ETL

--- swift_base/src/com/fr/swift/source/etl/OperatorType.java
+++ swift_base/src/com/fr/swift/source/etl/OperatorType.java
@@ -6,15 +6,9 @@ package com.fr.swift.source.etl;
  * 操作的类型，union，join等等
  */
 public enum OperatorType {
-<<<<<<< HEAD
-    DETAIL(true), UNION(false), JOIN(false), FILTER(false), COLUMNROWTRANS(false), GROUPSUM(false),
-    ONEUNIONRELATION(true), TWOUNIONRELATION(true), COLUMNFORMULA(true), SORT(false), GETDATE(true),
+    DETAIL(true), UNION(false), JOIN(false), FILTER(false), COLUMN_ROW_TRANS(false), GROUP_SUM(false),
+    ONE_UNION_RELATION(true), TWO_UNION_RELATION(true), COLUMN_FORMULA(true), SORT(false), GETDATE(true),
     DATEDIFF(true), ACCUMULATE(true), ALLDATA(true), PERIOD(true), PERCENTAGE(true), RANK(true), CONVERTER(true);
-=======
-    DETAIL(true), UNION(false), JOIN(false), FILTER(false),
-    COLUMN_ROW_TRANS(false), GROUP_SUM(false), ONE_UNION_RELATION(true),
-    TWO_UNION_RELATION(true), COLUMN_FORMULA(true), SORT(false);
->>>>>>> 22aeba7cc2ad9fe87e1f557423de16040be8b1a5
 
     private boolean isAddColumn;
 
