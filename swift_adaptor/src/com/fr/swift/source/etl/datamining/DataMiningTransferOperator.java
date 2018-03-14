c58e352a610f8825de6922114f33eadefbe894d3
jonas <825545356@qq.com>
2018-03-14 17:32:18 +0800

    conf已经提交，取消注释数据挖掘

--- swift_adaptor/src/com/fr/swift/source/etl/datamining/DataMiningTransferOperator.java
+++ swift_adaptor/src/com/fr/swift/source/etl/datamining/DataMiningTransferOperator.java
@@ -1,44 +1,74 @@
-//package com.fr.swift.source.etl.datamining;
-//
-//import com.finebi.conf.internalimp.analysis.bean.operator.datamining.AlgorithmBean;
-//import com.fr.swift.segment.Segment;
-//import com.fr.swift.source.SwiftMetaData;
-//import com.fr.swift.source.SwiftResultSet;
-//import com.fr.swift.source.etl.ETLTransferOperator;
-//import com.fr.swift.source.etl.datamining.timeseries.arima.ArimaResultSet;
-//import com.fr.swift.source.etl.datamining.timeseries.holtwinter.HoltWinterResultSet;
-//import com.fr.swift.source.etl.datamining.timeseries.regression.RegressionResultSet;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-///**
-// * Created by Handsome on 2018/1/17 0017 10:52
-// */
-//public class DataMiningTransferOperator implements ETLTransferOperator {
-//
-//    private AlgorithmBean algorithmBean;
-//
-//    public DataMiningTransferOperator(AlgorithmBean algorithmBean) {
-//        this.algorithmBean = algorithmBean;
-//    }
-//
-//    @Override
-//    public SwiftResultSet createResultSet(SwiftMetaData metaData, List<SwiftMetaData> basedMetas, List<Segment[]> basedSegments) {
-//        List<List<Segment>> tis = new ArrayList<List<Segment>>();
-//        for (int i = 0; i < basedSegments.size(); i++) {
-//            tis.add(Arrays.asList(basedSegments.get(i)));
-//        }
-//        switch (algorithmBean.getAlgorithmName()){
-//            case ARIMA:
-//                return new ArimaResultSet(algorithmBean, metaData,basedMetas.get(0), tis.get(0));
-//            case MULTI_REGRESSION:
-//                return new RegressionResultSet(algorithmBean, tis, metaData);
-//            case HOLT_WINTERS:
-//                return new HoltWinterResultSet(algorithmBean, tis, metaData);
-//            default:
-//                return null;
-//        }
-//    }
-//}
+package com.fr.swift.source.etl.datamining;
+
+import com.finebi.conf.internalimp.analysis.bean.operator.datamining.AlgorithmBean;
+import com.fr.swift.log.SwiftLogger;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.segment.Segment;
+import com.fr.swift.source.Row;
+import com.fr.swift.source.SwiftMetaData;
+import com.fr.swift.source.SwiftResultSet;
+import com.fr.swift.source.etl.ETLTransferOperator;
+import com.fr.swift.source.etl.datamining.timeseries.arima.ArimaResultSet;
+import com.fr.swift.source.etl.datamining.timeseries.holtwinter.HoltWinterResultSet;
+import com.fr.swift.source.etl.datamining.timeseries.regression.RegressionResultSet;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Handsome on 2018/1/17 0017 10:52
+ */
+public class DataMiningTransferOperator implements ETLTransferOperator {
+    private static final SwiftLogger LOGGER = SwiftLoggers.getLogger(DataMiningTransferOperator.class);
+
+    private AlgorithmBean algorithmBean;
+
+    public DataMiningTransferOperator(AlgorithmBean algorithmBean) {
+        this.algorithmBean = algorithmBean;
+    }
+
+    @Override
+    public SwiftResultSet createResultSet(SwiftMetaData metaData, List<SwiftMetaData> basedMetas, List<Segment[]> basedSegments) {
+        List<List<Segment>> tis = new ArrayList<List<Segment>>();
+        for (int i = 0; i < basedSegments.size(); i++) {
+            tis.add(Arrays.asList(basedSegments.get(i)));
+        }
+        try{
+            switch (algorithmBean.getAlgorithmName()){
+                case ARIMA:
+                    return new ArimaResultSet(algorithmBean, metaData,basedMetas.get(0), tis.get(0));
+                case MULTI_REGRESSION:
+                    return new RegressionResultSet(algorithmBean, metaData,basedMetas.get(0), tis.get(0));
+                case HOLT_WINTERS:
+                    return new HoltWinterResultSet(algorithmBean, metaData,basedMetas.get(0), tis.get(0));
+                default:
+                    return null;
+            }
+        }catch (Exception e){
+            LOGGER.error(e.getMessage(),e);
+            return new SwiftResultSet() {
+                @Override
+                public void close() throws SQLException {
+
+                }
+
+                @Override
+                public boolean next() throws SQLException {
+                    return false;
+                }
+
+                @Override
+                public SwiftMetaData getMetaData() throws SQLException {
+                    return null;
+                }
+
+                @Override
+                public Row getRowData() throws SQLException {
+                    return null;
+                }
+            };
+        }
+    }
+}
