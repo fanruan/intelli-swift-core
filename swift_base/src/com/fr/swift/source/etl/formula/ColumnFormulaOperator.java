26a22b215066abd4c8a0c127a941df7fe12519cf
pony <pony@finereport.com>
2018-03-21 16:31:04 +0800

    BI-17016 新增列的bug，id跟类型算的不对

--- swift_base/src/com/fr/swift/source/etl/formula/ColumnFormulaOperator.java
+++ swift_base/src/com/fr/swift/source/etl/formula/ColumnFormulaOperator.java
@@ -6,7 +6,6 @@ import com.fr.swift.source.MetaDataColumn;
 import com.fr.swift.source.SwiftMetaData;
 import com.fr.swift.source.SwiftMetaDataColumn;
 import com.fr.swift.source.core.CoreField;
-import com.fr.swift.source.core.MD5Utils;
 import com.fr.swift.source.etl.AbstractOperator;
 import com.fr.swift.source.etl.OperatorType;
 
@@ -36,7 +35,7 @@ public class ColumnFormulaOperator extends AbstractOperator {
     public List<SwiftMetaDataColumn> getColumns(SwiftMetaData[] metaDatas) {
         List<SwiftMetaDataColumn> columnList = new ArrayList<SwiftMetaDataColumn>();
         columnList.add(new MetaDataColumn(this.columnName,
-                this.columnName, ColumnTypeUtils.columnTypeToSqlType(this.columnType), MD5Utils.getMD5String(new String[]{(this.expression + this.columnType)})));
+                this.columnName, ColumnTypeUtils.columnTypeToSqlType(this.columnType), fetchObjectCore().getValue()));
         return columnList;
     }
 
