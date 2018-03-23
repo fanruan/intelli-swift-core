26e78f3a2bbae5b15f737c729e7082cc26b7494c
LiuYingjun <LiuYingjun@fanruan.com>
2018-03-23 17:54:13 +0800

    ETL

--- swift_adaptor/src/com/fr/swift/source/etl/datamining/kmeans/KmeansOperator.java
+++ swift_adaptor/src/com/fr/swift/source/etl/datamining/kmeans/KmeansOperator.java
@@ -1,14 +1,19 @@
 package com.fr.swift.source.etl.datamining.kmeans;
 
+import com.finebi.conf.internalimp.analysis.bean.operator.datamining.AlgorithmBean;
+import com.finebi.conf.internalimp.analysis.bean.operator.datamining.kmeans.KmeansBean;
+import com.fr.swift.source.ColumnTypeUtils;
 import com.fr.swift.source.MetaDataColumn;
 import com.fr.swift.source.SwiftMetaData;
 import com.fr.swift.source.SwiftMetaDataColumn;
 import com.fr.swift.source.core.MD5Utils;
 import com.fr.swift.source.etl.AbstractOperator;
 import com.fr.swift.source.etl.OperatorType;
+import com.fr.swift.source.ColumnTypeConstants.*;
 
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -21,6 +26,8 @@ public class KmeansOperator extends AbstractOperator {
     private boolean replaceMissing;
     private boolean distanceFunction;
     private String[] dimensions;
+    private AlgorithmBean algorithmBean;
+    private final String NAME = "聚类序号2";
 
     public int getCluster() {
         return cluster;
@@ -42,18 +49,25 @@ public class KmeansOperator extends AbstractOperator {
         return dimensions;
     }
 
-    public KmeansOperator(int cluster, int iterations, boolean replaceMissing,
-                          boolean distanceFunction, String[] dimensions) {
-        this.cluster = cluster;
-        this.iterations = iterations;
-        this.replaceMissing = replaceMissing;
-        this.distanceFunction = distanceFunction;
-        this.dimensions = dimensions;
+    public KmeansOperator(AlgorithmBean algorithmBean) {
+        this.algorithmBean = algorithmBean;
+        init();
+    }
+
+    private void init() {
+        KmeansBean bean = (KmeansBean) algorithmBean;
+        this.cluster = bean.getCluster();
+        this.iterations = bean.getIterations();
+        this.replaceMissing = bean.isReplaceMissing();
+        this.distanceFunction = bean.isDistanceFunction();
+        this.dimensions = bean.getDimensions();
     }
 
     @Override
     public List<SwiftMetaDataColumn> getColumns(SwiftMetaData[] metaDatas) {
         List<SwiftMetaDataColumn> columnList = new ArrayList<SwiftMetaDataColumn>();
+        columnList.add(new MetaDataColumn(NAME, NAME, ColumnTypeUtils.columnTypeToSqlType(ColumnType.STRING),
+                MD5Utils.getMD5String(new String[]{(NAME)})));;
         for(int i = 0; i < dimensions.length; i ++) {
             columnList.add(new MetaDataColumn(dimensions[i], dimensions[i],
                     Types.DOUBLE, MD5Utils.getMD5String(new String[]{(dimensions[i])})));
@@ -66,9 +80,10 @@ public class KmeansOperator extends AbstractOperator {
         return OperatorType.EXTRA_TRUE;
     }
 
-    // TODO 这个后面还要改进，暂时返回空
     @Override
     public List<String> getNewAddedName() {
-        return null;
+        List<String> columnList = Arrays.asList(dimensions);
+        columnList.add(NAME);
+        return columnList;
     }
 }
