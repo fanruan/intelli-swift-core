ed755770ff13a839c5eb1a8816116461032ed283
pony <pony@finereport.com>
2018-03-24 16:43:12 +0800

    楼价

---
+++ swift_base/src/com/fr/swift/query/filter/detail/impl/AllShowDetailFilter.java
@@ -0,0 +1,26 @@
+package com.fr.swift.query.filter.detail.impl;
+
+import com.fr.swift.bitmap.ImmutableBitMap;
+import com.fr.swift.query.filter.detail.DetailFilter;
+import com.fr.swift.result.SwiftNode;
+import com.fr.swift.segment.Segment;
+
+/**
+ * Created by pony on 2018/3/23.
+ */
+public class AllShowDetailFilter implements DetailFilter {
+    private Segment segment;
+    public AllShowDetailFilter(Segment segment) {
+        this.segment = segment;
+    }
+
+    @Override
+    public ImmutableBitMap createFilterIndex() {
+        return segment.getAllShowIndex();
+    }
+
+    @Override
+    public boolean matches(SwiftNode node) {
+        return false;
+    }
+}
