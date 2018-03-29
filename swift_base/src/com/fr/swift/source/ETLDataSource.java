b4f5d09fa908d5f6a489976f0ad7dcb6670c7366
pony <pony@finereport.com>
2018-03-30 07:52:01 +0800

    接口

--- swift_base/src/com/fr/swift/source/ETLDataSource.java
+++ swift_base/src/com/fr/swift/source/ETLDataSource.java
@@ -3,7 +3,6 @@ package com.fr.swift.source;
 import com.fr.swift.source.etl.ETLOperator;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by pony on 2017/11/15.
@@ -13,10 +12,4 @@ public interface ETLDataSource extends DataSource {
     ETLOperator getOperator();
 
     List<DataSource> getBasedSources();
-
-    /**
-     * 获取使用的字段信息
-     * @return
-     */
-    Map<Integer, String> getFieldsInfo();
 }
