a17027736adb083e0da04544c210031dc84c516a
jonas <825545356@qq.com>
2018-04-03 15:24:02 +0800

    适配直连引擎，把算法部分提取到foundation部分

--- swift_adaptor/src/com/fr/swift/source/etl/datamining/DataMiningOperator.java
+++ swift_adaptor/src/com/fr/swift/source/etl/datamining/DataMiningOperator.java
@@ -1,41 +1,31 @@
 package com.fr.swift.source.etl.datamining;
 
+import com.finebi.conf.algorithm.*;
 import com.finebi.conf.internalimp.analysis.bean.operator.datamining.AlgorithmBean;
-import com.finebi.conf.internalimp.analysis.bean.operator.datamining.kmeans.KmeansBean;
+import com.fr.swift.log.SwiftLogger;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.source.MetaDataColumn;
 import com.fr.swift.source.SwiftMetaData;
 import com.fr.swift.source.SwiftMetaDataColumn;
 import com.fr.swift.source.etl.AbstractOperator;
 import com.fr.swift.source.etl.OperatorType;
-import com.fr.swift.source.etl.datamining.kmeans.KmeansOperator;
-import com.fr.swift.source.etl.datamining.timeseries.holtwinter.HoltWinterOperator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Jonas on 2018/3/12 9:00
  */
 public class DataMiningOperator extends AbstractOperator {
+    private static final SwiftLogger LOGGER = SwiftLoggers.getLogger(DataMiningOperator.class);
 
     private AlgorithmBean algorithmBean = null;
 
-    private AbstractOperator algorithmOperator = null;
+    private DMAbstractAlgorithm algorithm;
 
     public DataMiningOperator(AlgorithmBean algorithmBean) {
         this.algorithmBean = algorithmBean;
-        init();
-    }
-
-    private void init() {
-        switch (algorithmBean.getAlgorithmName()) {
-            case HOLT_WINTERS:
-                algorithmOperator = new HoltWinterOperator(algorithmBean);
-                break;
-            case KMEANS:
-                algorithmOperator = new KmeansOperator(algorithmBean);
-
-            default:
-                break;
-        }
+        this.algorithm = DMAlgorithmFactory.create(algorithmBean.getAlgorithmName());
     }
 
     public AlgorithmBean getAlgorithmBean() {
@@ -44,16 +34,47 @@ public class DataMiningOperator extends AbstractOperator {
 
     @Override
     public List<SwiftMetaDataColumn> getColumns(SwiftMetaData[] tables) {
-        return algorithmOperator.getColumns(tables);
+        SwiftMetaData table = tables[0];
+        List<SwiftMetaDataColumn> columnList = new ArrayList<SwiftMetaDataColumn>();
+        try {
+            DMRowMetaData inputData = new DMRowMetaData();
+            for (int i = 0; i < table.getColumnCount(); i++) {
+                SwiftMetaDataColumn column = table.getColumn(i + 1);
+                inputData.addColMeta(new DMColMetaData(column.getName(), DMType.fromSwiftInt(column.getType())));
+            }
+
+            algorithm.init(algorithmBean, new DMDataModel(null, inputData));
+            DMRowMetaData outputMetaData = algorithm.getOutputMetaData();
+            for (DMColMetaData colMetaData : outputMetaData.getColMetas()) {
+                columnList.add(new MetaDataColumn(colMetaData.getColName(), colMetaData.getColType().toSwiftInt()));
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+        return columnList;
     }
 
     @Override
     public OperatorType getOperatorType() {
-        return algorithmOperator.getOperatorType();
+        if (algorithm.isAddColumns()) {
+            return OperatorType.EXTRA_TRUE;
+        } else {
+            return OperatorType.EXTRA_FALSE;
+        }
     }
 
     @Override
     public List<String> getNewAddedName() {
-        return algorithmOperator.getNewAddedName();
+        DMRowMetaData outputMetaData;
+        List<String> list = new ArrayList<String>();
+        try {
+            outputMetaData = algorithm.getOutputMetaData();
+            for (DMColMetaData colMetaData : outputMetaData.getColMetas()) {
+                list.add(colMetaData.getColName());
+            }
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+        return list;
     }
 }
