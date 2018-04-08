8d759b236f606b72db3fbfffd4fbcf8902fedf37
anchore <anchore@fanruan.com>
2018-04-08 10:39:19 +0800

    重构:提取代码

--- swift_test/test/com/fr/swift/generate/history/TablePathIndexerTest.java
+++ swift_test/test/com/fr/swift/generate/history/TablePathIndexerTest.java
@@ -2,18 +2,9 @@ package com.fr.swift.generate.history;
 
 //import com.fr.dav.LocalEnv;
 
-import com.fr.config.DBEnv;
-import com.fr.config.dao.DaoContext;
-import com.fr.config.dao.impl.HibernateClassHelperDao;
-import com.fr.config.dao.impl.HibernateEntityDao;
-import com.fr.config.dao.impl.HibernateXmlEnityDao;
-import com.fr.config.entity.ClassHelper;
-import com.fr.config.entity.Entity;
-import com.fr.config.entity.XmlEntity;
 import com.fr.general.ComparatorUtils;
-import com.fr.stable.db.DBContext;
-import com.fr.stable.db.option.DBOption;
 import com.fr.swift.bitmap.ImmutableBitMap;
+import com.fr.swift.config.TestConfDb;
 import com.fr.swift.cube.queue.CubeTasks;
 import com.fr.swift.cube.task.SchedulerTask;
 import com.fr.swift.cube.task.Task;
@@ -89,23 +80,7 @@ public class TablePathIndexerTest extends TestCase {
     }
 
     private void initConfigDB() throws Exception {
-        DBOption dbOption = new DBOption();
-        dbOption.setPassword("");
-        dbOption.setDialectClass("com.fr.third.org.hibernate.dialect.H2Dialect");
-        dbOption.setDriverClass("org.h2.Driver");
-        dbOption.setUsername("sa");
-        dbOption.setUrl("jdbc:h2:~/config");
-        dbOption.addRawProperty("hibernate.show_sql", false)
-                .addRawProperty("hibernate.format_sql", true).addRawProperty("hibernate.connection.autocommit", true);
-        DBContext dbProvider = DBContext.create();
-        dbProvider.addEntityClass(Entity.class);
-        dbProvider.addEntityClass(XmlEntity.class);
-        dbProvider.addEntityClass(ClassHelper.class);
-        dbProvider.init(dbOption);
-        DBEnv.setDBContext(dbProvider);
-        DaoContext.setClassHelperDao(new HibernateClassHelperDao());
-        DaoContext.setXmlEntityDao(new HibernateXmlEnityDao());
-        DaoContext.setEntityDao(new HibernateEntityDao());
+        TestConfDb.setConfDb();
     }
 
     public void buildMultiRelationIndex() throws Exception {
