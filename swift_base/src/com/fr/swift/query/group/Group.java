a9128d11cb8bd2826f0ef646c3a0534547181775
anchore <anchore@fanruan.com>
2018-04-10 13:37:40 +0800

    分组：分组类型加入source计算

--- swift_base/src/com/fr/swift/query/group/Group.java
+++ swift_base/src/com/fr/swift/query/group/Group.java
@@ -6,7 +6,7 @@ import com.fr.swift.source.core.CoreService;
  * @author pony
  * @date 2017/12/11
  */
-public interface Group<Base, Derive> extends CoreService{
+public interface Group<Base, Derive> extends CoreService {
     GroupOperator<Base, Derive> getGroupOperator();
 
     GroupType getGroupType();
