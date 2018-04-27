1204f5c3ba002370834f627d2c1e3cadaf781140
anchore <anchore@fanruan.com>
2018-04-27 14:21:56 +0800

    平台日志：activator改包名

--- swift_log_adaptor/src/com/fr/log/impl/LogImplActivator.java
+++ swift_log_adaptor/src/com/fr/log/impl/LogImplActivator.java
@@ -1,13 +1,14 @@
-package com.fr.swift.adaptor.log;
+package com.fr.log.impl;
 
 import com.fr.general.LogOperatorFactory;
 import com.fr.module.Activator;
+import com.fr.swift.adaptor.log.LogOperatorImpl;
 
 /**
  * @author anchore
  * @date 2018/4/26
  */
-public class LogActivator extends Activator {
+public class LogImplActivator extends Activator {
     @Override
     public void start() {
         LogOperatorFactory.registerLogOperatorProvider(LogOperatorImpl.getInstance());
