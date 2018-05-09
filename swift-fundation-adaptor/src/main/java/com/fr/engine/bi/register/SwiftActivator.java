7ed3e31aae4502bb6405d4e0dbfc345a06744dbf
pony <pony@finereport.com>
2018-05-09 13:38:55 +0800

    av

--- swift-fundation-adaptor/src/main/java/com/fr/engine/bi/register/SwiftActivator.java
+++ swift-fundation-adaptor/src/main/java/com/fr/engine/bi/register/SwiftActivator.java
@@ -1,7 +1,23 @@
 package com.fr.engine.bi.register;
 
 import com.finebi.base.stable.StableManager;
+import com.finebi.conf.algorithm.DMDataModel;
 import com.fr.module.Activator;
+import com.fr.swift.manager.ConnectionProvider;
+import com.fr.swift.service.LocalSwiftServerService;
+import com.fr.swift.service.SwiftAnalyseService;
+import com.fr.swift.service.SwiftHistoryService;
+import com.fr.swift.service.SwiftIndexingService;
+import com.fr.swift.service.SwiftRealTimeService;
+import com.fr.swift.source.db.ConnectionManager;
+import com.fr.swift.source.etl.ETLOperator;
+import com.fr.swift.source.etl.ETLTransferOperator;
+import com.fr.swift.source.etl.EtlTransferOperatorFactory;
+import com.fr.swift.source.etl.datamining.DataMiningOperator;
+import com.fr.swift.source.etl.datamining.DataMiningTransferOperator;
+import com.fr.swift.source.etl.rcompile.RCompileOperator;
+import com.fr.swift.source.etl.rcompile.RCompileTransferOperator;
+import com.fr.swift.util.Crasher;
 
 /**
  * Created by pony on 2018/5/9.
@@ -9,7 +25,7 @@ import com.fr.module.Activator;
 public class SwiftActivator extends Activator {
     @Override
     public void start() {
-        StableManager.addClass("localSwiftServerService", com.fr.swift.service.LocalSwiftServerService.class);
+        startSwift();
         StableManager.addClass("swiftTableEngineExecutor", com.finebi.conf.imp.SwiftTableEngineExecutor.class);
         StableManager.addClass("swiftAnalysisTableManager", com.finebi.conf.imp.SwiftAnalysisTableManager.class);
         StableManager.addClass("swiftEngineWidgetExecutorManager", com.fr.swift.adaptor.widget.SwiftWidgetExecutorManager.class);
@@ -20,6 +36,36 @@ public class SwiftActivator extends Activator {
         StableManager.addClass("swiftTableManager", com.finebi.conf.provider.SwiftTableManager.class);
         StableManager.addClass("swiftRelationPathConfProvider", com.finebi.conf.provider.SwiftRelationPathConfProvider.class);
         StableManager.addClass("swiftSpaceManager", com.fr.swift.adaptor.space.SwiftSpaceManager.class);
+    }
+
+    private void startSwift() {
+        ConnectionManager.getInstance().registerProvider(new ConnectionProvider());
+        EtlTransferOperatorFactory.register(DataMiningOperator.class, new EtlTransferOperatorFactory.ETLTransferCreator() {
+
+            @Override
+            public ETLTransferOperator createTransferOperator(ETLOperator operator) {
+                return new DataMiningTransferOperator(((DataMiningOperator) operator).getAlgorithmBean());
+            }
+        });
+        EtlTransferOperatorFactory.register(RCompileOperator.class, new EtlTransferOperatorFactory.ETLTransferCreator() {
+
+            @Override
+            public ETLTransferOperator createTransferOperator(ETLOperator operator) {
+                RCompileOperator op = (RCompileOperator) operator;
+                DMDataModel dataList = op.getDataModel();
+                return new RCompileTransferOperator(dataList);
+            }
+        });
+
+        try {
+            new LocalSwiftServerService().start();
+            new SwiftAnalyseService().start();
+            new SwiftHistoryService().start();
+            new SwiftIndexingService().start();
+            new SwiftRealTimeService().start();
+        } catch (Exception e) {
+            Crasher.crash("swift service start failed", e);
+        }
     }
 
     @Override
