6b7765e0c0dce79d905c3aaf511cad76fe79b4d4
liu <Xiaolei.Liu@fanruan.com>
2018-05-11 18:03:49 +0800

    多块排序结果集越界问题

--- swift-analyse/src/main/java/com/fr/swift/result/SortMultiSegmentDetailResultSet.java
+++ swift-analyse/src/main/java/com/fr/swift/result/SortMultiSegmentDetailResultSet.java
@@ -53,14 +53,26 @@ public class SortMultiSegmentDetailResultSet extends DetailResultSet {
 
     }
 
+    /**
+     * 类似纸牌游戏，n块数据时unsortedRows数组大小为n;每次分别从n块排好序的数据中取一行数据放在unsortedRows对应的位置上,
+     * 比较出这n行数据的最大或最小值frontRow，并且从该frontrow所在的块取下一行数据放在unsortedRows对应的位置上（替换掉
+     * 该位置上的值）;数组元素为空表示该数组位置对应的块数据已经被取完。
+     */
     private Row getLatestRowData(Row frontRow) throws SQLException {
         int pos = 0;
         for (int i = 1; i < unsortedRows.length; i++) {
             while (unsortedRows[i] == null) {
+                if (i >= unsortedRows.length - 1) {
+                    break;
+                }
                 i = i + 1;
+            }
+            if (unsortedRows[i] == null) {
+                continue;
             }
             if (frontRow == null) {
                 frontRow = unsortedRows[i];
+                pos = i;
             }
             if (comparator.compare(frontRow, unsortedRows[i]) > 0) {
                 frontRow = unsortedRows[i];
