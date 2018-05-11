442315a5a302dd56e0f2484cf1f53b7277c9544a
Yee <yee@fanruan.com>
2018-05-11 17:40:53 +0800

    FieldInfo

--- swift-fundation-adaptor/src/main/java/com/fr/swift/conf/business/field/FieldInfoHelper.java
+++ swift-fundation-adaptor/src/main/java/com/fr/swift/conf/business/field/FieldInfoHelper.java
@@ -3,44 +3,43 @@ package com.fr.swift.conf.business.field;
 import com.finebi.common.internalimp.config.fieldinfo.FieldInfoImpl;
 import com.finebi.common.structure.config.entryinfo.EntryInfo;
 import com.finebi.common.structure.config.fieldinfo.FieldInfo;
+import com.finebi.conf.constant.BICommonConstants;
+import com.finebi.conf.structure.bean.field.FineBusinessField;
+import com.finebi.conf.structure.bean.table.FineBusinessTable;
 import com.fr.engine.bi.config.Field;
 import com.fr.engine.bi.config.SimpleField;
 import com.fr.engine.constant.Type;
 import com.fr.swift.adaptor.encrypt.SwiftEncryption;
-import com.fr.swift.exception.meta.SwiftMetaDataException;
-import com.fr.swift.source.ColumnTypeConstants;
-import com.fr.swift.source.ColumnTypeUtils;
-import com.fr.swift.source.SwiftMetaData;
-import com.fr.swift.source.SwiftMetaDataColumn;
 
+import java.util.List;
 
 /**
  * @author yee
  * @date 2018/4/8
  */
 public class FieldInfoHelper {
-    public static FieldInfo createFieldInfo(EntryInfo entryInfo, SwiftMetaData table) throws SwiftMetaDataException {
+    public static FieldInfo createFieldInfo(EntryInfo entryInfo, FineBusinessTable table) {
         return createDefaultFieldInfo(entryInfo, table);
     }
 
-    private static FieldInfo createDefaultFieldInfo(EntryInfo entryInfo, SwiftMetaData table) throws SwiftMetaDataException {
-        int columnCount = table.getColumnCount();
-        Field[] infoFields = new Field[columnCount];
-        for (int i = 1; i <= columnCount; i++) {
-            SwiftMetaDataColumn businessField = table.getColumn(i);
-            infoFields[i - 1] = new SimpleField(SwiftEncryption.encryptFieldId(table.getTableName(), businessField.getName()), businessField.getName(), convert2Type(ColumnTypeUtils.getColumnType(businessField)), true);
+    private static FieldInfo createDefaultFieldInfo(EntryInfo entryInfo, FineBusinessTable table) {
+        List<FineBusinessField> fields = table.getFields();
+        Field[] infoFields = new Field[fields.size()];
+        for (int i = 0; i < fields.size(); i++) {
+            FineBusinessField businessField = fields.get(i);
+            infoFields[i] = new SimpleField(SwiftEncryption.encryptFieldId(table.getName(), businessField.getName()), businessField.getName(), convert2Type(businessField.getType()), true, businessField.getFieldGroupType());
         }
         return FieldInfoImpl.create(entryInfo.getID(), infoFields);
     }
 
 
-    private static Type convert2Type(ColumnTypeConstants.ColumnType type) {
-        switch (type) {
-            case NUMBER:
+    private static Type convert2Type(int fieldType) {
+        switch (fieldType) {
+            case BICommonConstants.COLUMN.NUMBER:
                 return Type.Decimal;
-            case STRING:
+            case BICommonConstants.COLUMN.STRING:
                 return Type.String;
-            case DATE:
+            case BICommonConstants.COLUMN.DATE:
                 return Type.Date;
             default:
                 return Type.String;
