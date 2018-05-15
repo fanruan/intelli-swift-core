a6043c3710b3ec9f31d1d8b7aca56daefb7d7116
lucifer <lucifer@fanruan.com>
2018-05-15 15:00:48 +0800

    no message

--- swift-fundation-adaptor/src/main/java/com/fr/engine/bi/register/SwiftActivator.java
+++ swift-fundation-adaptor/src/main/java/com/fr/engine/bi/register/SwiftActivator.java
@@ -38,7 +38,6 @@ public class SwiftActivator extends Activator {
         StableManager.addClass("swiftTableManager", com.finebi.conf.provider.SwiftTableManager.class);
         StableManager.addClass("swiftRelationPathConfProvider", com.finebi.conf.provider.SwiftRelationPathConfProvider.class);
         StableManager.addClass("swiftSpaceManager", com.fr.swift.adaptor.space.SwiftSpaceManager.class);
-        AttachmentCacheManagerImpl.register();
         startSwift();
     }
 
