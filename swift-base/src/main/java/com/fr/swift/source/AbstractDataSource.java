896f1f18a74bf7f13caca3a141c88b3b3fbf8c04
pony <pony@finereport.com>
2018-05-18 08:34:02 +0800

    BI-20568 metadata改名

--- swift-base/src/main/java/com/fr/swift/source/AbstractDataSource.java
+++ swift-base/src/main/java/com/fr/swift/source/AbstractDataSource.java
@@ -4,6 +4,11 @@ import com.fr.swift.source.core.Core;
 import com.fr.swift.source.core.CoreGenerator;
 import com.fr.swift.util.Util;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by pony on 2017/11/15.
  */
@@ -30,8 +35,35 @@ public abstract class AbstractDataSource implements DataSource {
     public SwiftMetaData getMetadata() {
         if (metaData == null) {
             initMetaData();
+            checkColumnNames();
         }
         return metaData;
+    }
+
+    protected void checkColumnNames(){
+        try {
+            List<SwiftMetaDataColumn> columnList = new ArrayList<SwiftMetaDataColumn>();
+            Set<String> names = new HashSet<String>();
+            for (int i = 0;i < metaData.getColumnCount(); i++){
+                SwiftMetaDataColumn column = metaData.getColumn(i+1);
+                if (names.contains(column.getName())){
+                    String newName = createNewName(names, column.getName());
+                    column = new MetaDataColumn(newName, column.getRemark(), column.getType(),column.getPrecision(),column.getScale(), column.getColumnId());
+                }
+                columnList.add(column);
+                names.add(column.getName());
+            }
+            metaData = new SwiftMetaDataImpl(metaData.getTableName(), metaData.getRemark(), metaData.getSchemaName(), columnList);
+        } catch (Exception ignore){
+
+        }
+    }
+
+    protected String createNewName(Set<String> names, String name){
+        while (names.contains(name)){
+            name = name + 1;
+        }
+        return name;
     }
 
     protected abstract void initMetaData();
