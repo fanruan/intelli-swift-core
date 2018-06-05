65d9dc1869ea0c541f84dd6651a55510ae1e4bab
Yee <yee@fanruan.com>
2018-06-05 10:09:29 +0800

    history 接口

--- swift-history/src/main/java/com/fr/swift/service/SwiftHistoryService.java
+++ swift-history/src/main/java/com/fr/swift/service/SwiftHistoryService.java
@@ -1,11 +1,23 @@
 package com.fr.swift.service;
 
+import com.fr.swift.config.SwiftCubePathConfig;
+import com.fr.swift.context.SwiftContext;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.query.QueryInfo;
+import com.fr.swift.repository.SwiftRepository;
+import com.fr.swift.repository.SwiftRepositoryManager;
+import com.fr.swift.source.SwiftResultSet;
+
+import java.io.IOException;
 import java.io.Serializable;
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.Set;
 
 /**
  * Created by pony on 2017/10/10.
  */
-public class SwiftHistoryService extends AbstractSwiftService implements Serializable {
+public class SwiftHistoryService extends AbstractSwiftService implements HistoryService, Serializable {
 
     private static final long serialVersionUID = -6013675740141588108L;
 
@@ -19,5 +31,23 @@ public class SwiftHistoryService extends AbstractSwiftService implements Seriali
     @Override
     public ServiceType getServiceType() {
         return ServiceType.HISTORY;
+    }
+
+    @Override
+    public <T extends SwiftResultSet> T query(QueryInfo<T> queryInfo) throws SQLException {
+        SwiftLoggers.getLogger().info("History query");
+        return null;
+    }
+
+    @Override
+    public void load(Set<URI> remoteUris) throws IOException {
+
+        SwiftLoggers.getLogger().info("History load uri");
+
+        String path = SwiftCubePathConfig.getInstance().getPath();
+        SwiftRepository repository = SwiftRepositoryManager.getManager().getDefaultRepository();
+        for (URI remote : remoteUris) {
+            repository.copyFromRemote(remote, URI.create(path + remote.getPath()));
+        }
     }
 }
