249a54aa75d9138aa13db5f9d6478bdcb175ea55
Yee <yee@fanruan.com>
2018-06-21 18:14:13 +0800

    DEC-2518 swift各模块通讯跑通 queryinfo序列化问题

--- swift-analyse/src/main/java/com/fr/swift/query/info/bean/query/QueryBeanFactory.java
+++ swift-analyse/src/main/java/com/fr/swift/query/info/bean/query/QueryBeanFactory.java
@@ -1,9 +1,18 @@
 package com.fr.swift.query.info.bean.query;
 
+import com.fr.swift.query.info.bean.element.DimensionBean;
+import com.fr.swift.query.info.bean.element.FilterInfoBean;
+import com.fr.swift.query.info.bean.element.SortBean;
+import com.fr.swift.query.info.detail.DetailQueryInfo;
+import com.fr.swift.query.info.element.metric.Metric;
+import com.fr.swift.query.info.group.GroupQueryInfoImpl;
+import com.fr.swift.query.query.QueryInfo;
 import com.fr.third.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Lyon on 2018/6/7.
@@ -14,5 +23,40 @@ public class QueryBeanFactory {
 
     public static QueryBean create(URL url) throws IOException {
         return MAPPER.readValue(url, QueryBean.class);
+    }
+
+    public static QueryBean create(String jsonString) throws IOException {
+        return MAPPER.readValue(jsonString, QueryBean.class);
+    }
+
+    public static QueryBean create(QueryInfo queryInfo) {
+        // TODO 处理其他属性
+        switch (queryInfo.getType()) {
+            case GROUP:
+            case LOCAL_GROUP_ALL:
+            case LOCAL_GROUP_PART:
+            case CROSS_GROUP:
+                GroupQueryInfoImpl groupQueryInfo = (GroupQueryInfoImpl) queryInfo;
+                List<Metric> groupMetricList = groupQueryInfo.getMetrics();
+                GroupQueryBean groupQueryBean = new GroupQueryBean();
+                groupQueryBean.setTableName(groupQueryInfo.getTable().getId());
+                groupQueryBean.setQueryId(queryInfo.getQueryId());
+                return groupQueryBean;
+            case RESULT_JOIN:
+                ResultJoinQueryBean resultJoinQueryBean = new ResultJoinQueryBean();
+                resultJoinQueryBean.setQueryId(queryInfo.getQueryId());
+                return resultJoinQueryBean;
+            case DETAIL:
+            case LOCAL_DETAIL:
+                DetailQueryBean detailQueryBean = new DetailQueryBean();
+                detailQueryBean.setQueryId(queryInfo.getQueryId());
+                detailQueryBean.setTableName(((DetailQueryInfo) queryInfo).getTable().getId());
+                detailQueryBean.setDimensionBeans(new ArrayList<DimensionBean>());
+                detailQueryBean.setFilterInfoBean(new FilterInfoBean());
+                detailQueryBean.setSortBeans(new ArrayList<SortBean>());
+                return detailQueryBean;
+            case NEST:
+        }
+        return null;
     }
 }
--- swift-analyse/src/main/java/com/fr/swift/query/info/remote/RemoteQueryInfoImpl.java
+++ swift-analyse/src/main/java/com/fr/swift/query/info/remote/RemoteQueryInfoImpl.java
@@ -1,32 +1,64 @@
 package com.fr.swift.query.info.remote;
 
+import com.fr.swift.query.info.bean.parser.QueryInfoParser;
+import com.fr.swift.query.info.bean.query.QueryBean;
+import com.fr.swift.query.info.bean.query.QueryBeanFactory;
 import com.fr.swift.query.query.QueryInfo;
 import com.fr.swift.query.query.QueryType;
 import com.fr.swift.source.SwiftResultSet;
+import com.fr.third.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.Serializable;
 import java.net.URI;
 
 /**
  * Created by Lyon on 2018/6/5.
  */
-public class RemoteQueryInfoImpl<T extends SwiftResultSet> implements RemoteQueryInfo<T> {
+public class RemoteQueryInfoImpl<T extends SwiftResultSet> implements RemoteQueryInfo<T>, Serializable {
 
+    private static final long serialVersionUID = -5148058795330646387L;
     private QueryType remoteQueryType;
-    private QueryInfo<T> queryInfo;
+    private String queryBeanJson;
+    private transient QueryInfo<T> queryInfo;
+    private transient ObjectMapper mapper = new ObjectMapper();
 
     public RemoteQueryInfoImpl(QueryType remoteQueryType, QueryInfo<T> queryInfo) {
         this.remoteQueryType = remoteQueryType;
-        this.queryInfo = queryInfo;
+        makeQueryJson(queryInfo);
     }
 
-    @Override
-    public QueryInfo<T> getQueryInfo() {
+    private void makeQueryJson(QueryInfo<T> queryInfo) {
+        QueryBean queryBean = QueryBeanFactory.create(queryInfo);
+        try {
+            queryBeanJson = mapper.writeValueAsString(queryBean);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private QueryInfo<T> createQueryInfo() {
+        try {
+            queryInfo = QueryInfoParser.parse(QueryBeanFactory.create(queryBeanJson));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return queryInfo;
     }
 
     @Override
+    public QueryInfo<T> getQueryInfo() {
+        if (null != queryInfo) {
+            return queryInfo;
+        }
+        return createQueryInfo();
+    }
+
+    @Override
     public String getQueryId() {
-        return queryInfo.getQueryId();
+        if (null != queryInfo) {
+            return queryInfo.getQueryId();
+        }
+        return createQueryInfo().getQueryId();
     }
 
     @Override
@@ -36,12 +68,19 @@ public class RemoteQueryInfoImpl<T extends SwiftResultSet> implements RemoteQuer
 
     @Override
     public URI getQuerySegment() {
-        return queryInfo.getQuerySegment();
+        if (null != queryInfo) {
+            return queryInfo.getQuerySegment();
+        }
+        return createQueryInfo().getQuerySegment();
     }
 
     @Override
     public void setQuerySegment(URI target) {
+        if (null == queryInfo) {
+            queryInfo = createQueryInfo();
+        }
         queryInfo.setQuerySegment(target);
+        makeQueryJson(queryInfo);
     }
 
 }
