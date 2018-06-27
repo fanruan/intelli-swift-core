7f8a9dca8aed1723813533b66671f876a8a25dbf
Yee <yee@fanruan.com>
2018-06-27 09:26:48 +0800

    DEC-2681 统一下风格

--- swift-analyse/src/main/java/com/fr/swift/query/info/bean/factory/RelationSourceBeanFactory.java
+++ swift-analyse/src/main/java/com/fr/swift/query/info/bean/factory/RelationSourceBeanFactory.java
@@ -18,7 +18,6 @@ import java.util.List;
  */
 public class RelationSourceBeanFactory implements BeanFactory<List<RelationSource>, List<IRelationSourceBean>> {
 
-    private static RelationSourceBeanFactory instance;
     public static final BeanFactory<RelationSource, IRelationSourceBean> SINGLE_RELATION_SOURCE_BEAN_FACTORY = new BeanFactory<RelationSource, IRelationSourceBean>() {
         @Override
         public IRelationSourceBean create(RelationSource source) {
@@ -51,14 +50,11 @@ public class RelationSourceBeanFactory implements BeanFactory<List<RelationSourc
     }
 
     private static RelationSourceBeanFactory getInstance() {
-        if (null == instance) {
-            synchronized (RelationSourceBeanFactory.class) {
-                if (null == instance) {
-                    instance = new RelationSourceBeanFactory();
-                }
-            }
-        }
-        return instance;
+        return SingletonHolder.factory;
+    }
+
+    private static class SingletonHolder {
+        private static RelationSourceBeanFactory factory = new RelationSourceBeanFactory();
     }
 
     @Override
