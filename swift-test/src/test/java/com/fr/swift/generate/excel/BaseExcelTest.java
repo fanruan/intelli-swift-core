2dea33d55ea73d8cfbf4d7196aa9f71c5b4e6901
pony <pony@finereport.com>
2018-06-29 09:41:21 +0800

    test修改，fr的resource默认实现有问题

--- swift-test/src/test/java/com/fr/swift/generate/excel/BaseExcelTest.java
+++ swift-test/src/test/java/com/fr/swift/generate/excel/BaseExcelTest.java
@@ -7,6 +7,9 @@ import com.fr.swift.resource.ResourceUtils;
 import com.fr.swift.source.ColumnTypeConstants;
 import com.fr.swift.source.excel.ExcelDataSource;
 import com.fr.swift.test.Preparer;
+import com.fr.workspace.simple.SimpleWork;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 /**
@@ -18,11 +21,23 @@ import org.junit.BeforeClass;
  */
 public abstract class BaseExcelTest extends BaseConfigTest {
 
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        SimpleWork.checkIn(ResourceUtils.class.getClassLoader().getResource("").getPath());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        SimpleWork.checkOut();
+    }
+
     protected static final SwiftLogger LOGGER = SwiftLoggers.getLogger(BaseExcelTest.class);
 
-    protected String path1 = ResourceUtils.getFileAbsolutePath("com/fr/swift/resource/excel/test.xlsx");
-    protected String path2 = ResourceUtils.getFileAbsolutePath("com/fr/swift/resource/excel/test1.xlsx");
-    protected String path3 = ResourceUtils.getFileAbsolutePath("com/fr/swift/resource/excel/test2.xlsx");
+    protected String path1 = "com/fr/swift/resource/excel/test.xlsx";
+    protected String path2 = "com/fr/swift/resource/excel/test1.xlsx";
+    protected String path3 = "com/fr/swift/resource/excel/test2.xlsx";
 
     protected ExcelDataSource dataSource;
     protected String[] names = {"A", "B"};
