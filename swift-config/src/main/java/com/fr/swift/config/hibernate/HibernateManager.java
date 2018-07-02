1517f6588c356e025e81962c84535cfd73aa0e4a
Yee <yee@fanruan.com>
2018-07-02 09:14:57 +0800

    DEC-2518 先用fr的配置

--- swift-config/src/main/java/com/fr/swift/config/hibernate/HibernateManager.java
+++ swift-config/src/main/java/com/fr/swift/config/hibernate/HibernateManager.java
@@ -1,7 +1,6 @@
 package com.fr.swift.config.hibernate;
 
-import com.fr.swift.config.SwiftConfDBConfig;
-import com.fr.swift.config.bean.SwiftConfDbBean;
+import com.fr.finedb.FineDBProperties;
 import com.fr.swift.config.entity.SwiftMetaDataEntity;
 import com.fr.swift.config.entity.SwiftSegmentEntity;
 import com.fr.swift.config.entity.SwiftServiceInfoEntity;
@@ -9,8 +8,9 @@ import com.fr.third.org.hibernate.SessionFactory;
 import com.fr.third.org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import com.fr.third.org.hibernate.cfg.Configuration;
 import com.fr.third.org.hibernate.service.ServiceRegistry;
-import com.fr.third.springframework.beans.factory.annotation.Autowired;
 import com.fr.third.springframework.context.annotation.Bean;
+
+import java.util.Properties;
 
 /**
  * @author yee
@@ -18,8 +18,8 @@ import com.fr.third.springframework.context.annotation.Bean;
  */
 @com.fr.third.springframework.context.annotation.Configuration
 public class HibernateManager {
-    @Autowired
-    private SwiftConfigProperties properties;
+//    @Autowired
+//    private SwiftConfigProperties properties;
 
     public HibernateManager() {
 
@@ -27,19 +27,24 @@ public class HibernateManager {
 
     @Bean
     public Configuration getConfiguration() {
-        SwiftConfDbBean config = SwiftConfDBConfig.getInstance().getConfig();
-        if (null != config) {
-            properties.setDialectClass(config.getDialectClass());
-            properties.setDriverClass(config.getDriverClass());
-            properties.setPassword(config.getPassword());
-            properties.setUrl(config.getUrl());
-            properties.setUsername(config.getUsername());
-        }
+//        SwiftConfDbBean config = SwiftConfDBConfig.getInstance().getConfig();
+        Properties dbProperties = FineDBProperties.getInstance().get().getProperties();
+//        if (null != config) {
+//            properties.setDialectClass(config.getDialectClass());
+//            properties.setDriverClass(config.getDriverClass());
+//            properties.setPassword(config.getPassword());
+//            properties.setUrl(config.getUrl());
+//            properties.setUsername(config.getUsername());
+//            dbProperties = properties.getProperties();
+//        } else {
+//            dbProperties = properties.getDefaultOption().getProperties();
+//        }
+
         Configuration configuration = new Configuration();
         configuration.addAnnotatedClass(SwiftMetaDataEntity.class);
         configuration.addAnnotatedClass(SwiftSegmentEntity.class);
         configuration.addAnnotatedClass(SwiftServiceInfoEntity.class);
-        configuration.setProperties(properties.getProperties());
+        configuration.setProperties(dbProperties);
         return configuration;
     }
 
