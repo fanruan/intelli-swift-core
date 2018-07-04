8a07573452f91c570006c9d9d26a4f3e6a02a3cb
Yee <yee@fanruan.com>
2018-07-04 15:49:30 +0800

    调整 上传修改，segment下的目录单独压一下 整个segment压缩没办法修改内容

--- swift-repository/src/main/java/com/fr/swift/repository/utils/ZipUtils.java
+++ swift-repository/src/main/java/com/fr/swift/repository/utils/ZipUtils.java
@@ -24,13 +24,11 @@ public class ZipUtils {
     /**
      * 压缩成ZIP 方法1
      *
-     * @param srcDir           压缩文件夹路径
-     * @param out              压缩文件输出流
-     * @param KeepDirStructure 是否保留原来的目录结构,true:保留目录结构;
-     *                         false:所有文件跑到压缩包根目录下(注意：不保留目录结构可能会出现同名文件,会压缩失败)
+     * @param srcDir 压缩文件夹路径
+     * @param out    压缩文件输出流
      * @throws RuntimeException 压缩失败会抛出运行时异常
      */
-    public static void toZip(String srcDir, OutputStream out, boolean KeepDirStructure)
+    public static void toZip(String srcDir, OutputStream out)
             throws RuntimeException {
 
         long start = System.currentTimeMillis();
@@ -38,17 +36,17 @@ public class ZipUtils {
         try {
             zos = new ZipOutputStream(out);
             File sourceFile = new File(srcDir);
-            compress(sourceFile, zos, sourceFile.getName(), KeepDirStructure);
+            compress(sourceFile, zos, sourceFile.getName());
             long end = System.currentTimeMillis();
             SwiftLoggers.getLogger().info("压缩完成，耗时：" + (end - start) + " ms");
         } catch (Exception e) {
-            throw new RuntimeException("zip error from ZipUtils", e);
+            throw new RuntimeException("zip error", e);
         } finally {
             if (zos != null) {
                 try {
                     zos.close();
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    throw new RuntimeException("zip error", e);
                 }
             }
         }
@@ -56,14 +54,11 @@ public class ZipUtils {
     }
 
     public static void unZip(String parent, InputStream inputStream) throws Exception {
-
-
+        long start = System.currentTimeMillis();
         ZipInputStream zis = new ZipInputStream(new BufferedInputStream(inputStream));
-
-        File fout = null;
         ZipEntry entry;
         while ((entry = zis.getNextEntry()) != null && !entry.isDirectory()) {
-            fout = new File(parent, entry.getName());
+            File fout = new File(parent, entry.getName());
             if (!fout.exists()) {
                 (new File(fout.getParent())).mkdirs();
             }
@@ -79,10 +74,11 @@ public class ZipUtils {
         }
 
         zis.close();
+        long end = System.currentTimeMillis();
+        SwiftLoggers.getLogger().info("解压完成，耗时：" + (end - start) + " ms");
     }
 
-    private static void compress(File sourceFile, ZipOutputStream zos, String name,
-                                 boolean KeepDirStructure) throws Exception {
+    private static void compress(File sourceFile, ZipOutputStream zos, String name) throws Exception {
         byte[] buf = new byte[BUFFER_SIZE];
         if (sourceFile.isFile()) {
             // 向zip输出流中添加一个zip实体，构造器中name为zip实体的文件的名字
@@ -99,25 +95,16 @@ public class ZipUtils {
         } else {
             File[] listFiles = sourceFile.listFiles();
             if (listFiles == null || listFiles.length == 0) {
-                // 需要保留原来的文件结构时,需要对空文件夹进行处理
-                if (KeepDirStructure) {
-                    // 空文件夹的处理
-                    zos.putNextEntry(new ZipEntry(name + "/"));
-                    // 没有文件，不需要文件的copy
-                    zos.closeEntry();
-                }
-
+                // 空文件夹的处理
+                zos.putNextEntry(new ZipEntry(name + "/"));
+                // 没有文件，不需要文件的copy
+                zos.closeEntry();
             } else {
                 for (File file : listFiles) {
                     // 判断是否需要保留原来的文件结构
-                    if (KeepDirStructure) {
-                        // 注意：file.getName()前面需要带上父文件夹的名字加一斜杠,
-                        // 不然最后压缩包中就不能保留原来的文件结构,即：所有文件都跑到压缩包根目录下了
-                        compress(file, zos, name + "/" + file.getName(), KeepDirStructure);
-                    } else {
-                        compress(file, zos, file.getName(), KeepDirStructure);
-                    }
-
+                    // 注意：file.getName()前面需要带上父文件夹的名字加一斜杠,
+                    // 不然最后压缩包中就不能保留原来的文件结构,即：所有文件都跑到压缩包根目录下了
+                    compress(file, zos, name + "/" + file.getName());
                 }
             }
         }
