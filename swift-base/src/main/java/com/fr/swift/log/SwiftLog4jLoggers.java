a5f6015d1509813ec84a43f7c2016e8bede710ca
anchore <anchore@fanruan.com>
2018-07-06 14:25:04 +0800

    logger改投slf4j，兼容fr和现有logger

--- swift-base/src/main/java/com/fr/swift/log/SwiftLog4jLoggers.java
+++ swift-base/src/main/java/com/fr/swift/log/SwiftLog4jLoggers.java
@@ -1,7 +1,12 @@
 package com.fr.swift.log;
 
 import com.fr.swift.util.function.Function;
+import com.fr.third.apache.log4j.ConsoleAppender;
+import com.fr.third.apache.log4j.DailyRollingFileAppender;
 import com.fr.third.apache.log4j.Logger;
+import com.fr.third.apache.log4j.PatternLayout;
+
+import java.io.IOException;
 
 /**
  * @author anchore
@@ -12,6 +17,14 @@ public class SwiftLog4jLoggers implements Function<Void, SwiftLogger> {
 
     private static SwiftLogger newLogger() {
         Logger logger = Logger.getLogger(SwiftLog4jLogger.class);
+        PatternLayout layout = new PatternLayout("%d{yy-M-d H:m:s.S} %t %p [%C.%M] %m%n");
+        logger.addAppender(new ConsoleAppender(layout, "swift_console"));
+        try {
+            DailyRollingFileAppender appender = new DailyRollingFileAppender(layout, "logs/swift.log", "'.'yy-M-d");
+            appender.setName("swift_daily_file");
+            logger.addAppender(appender);
+        } catch (IOException ignore) {
+        }
         return new SwiftLog4jLogger(logger);
     }
 
