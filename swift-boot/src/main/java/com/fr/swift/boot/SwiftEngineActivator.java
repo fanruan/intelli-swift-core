999380e9e6ef5c057e16210d242b8811877d25fb
Yee <yee@fanruan.com>
2018-07-06 16:28:21 +0800

    DEC-2888 启动的同步一下配置

--- swift-boot/src/main/java/com/fr/swift/boot/SwiftEngineActivator.java
+++ swift-boot/src/main/java/com/fr/swift/boot/SwiftEngineActivator.java
@@ -3,28 +3,16 @@ package com.fr.swift.boot;
 import com.fr.module.Activator;
 import com.fr.module.extension.Prepare;
 import com.fr.stable.db.constant.BaseDBConstant;
-import com.fr.swift.config.bean.unique.RepositoryConfigUnique;
-import com.fr.swift.config.bean.unique.RpcServiceAddressUnique;
 import com.fr.swift.config.context.SwiftConfigContext;
 import com.fr.swift.config.entity.SwiftMetaDataEntity;
 import com.fr.swift.config.entity.SwiftSegmentEntity;
 import com.fr.swift.config.entity.SwiftServiceInfoEntity;
-import com.fr.swift.config.service.SwiftPathService;
-import com.fr.swift.config.service.SwiftRepositoryConfService;
-import com.fr.swift.config.service.SwiftServiceAddressService;
-import com.fr.swift.config.service.SwiftZipService;
 import com.fr.swift.context.SwiftContext;
 import com.fr.swift.cube.queue.ProviderTaskManager;
-import com.fr.swift.decision.config.SwiftCubePathConfig;
-import com.fr.swift.decision.config.SwiftRepositoryConfig;
-import com.fr.swift.decision.config.SwiftServiceAddressConfig;
-import com.fr.swift.decision.config.SwiftZipConfig;
 import com.fr.swift.event.ClusterListenerHandler;
 import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.server.SwiftEngineStart;
 import com.fr.swift.service.register.LocalSwiftRegister;
-
-import java.util.Iterator;
-import java.util.Map;
 
 /**
  * @author anchore
@@ -61,22 +49,6 @@ public class SwiftEngineActivator extends Activator implements Prepare {
     }
 
     private void syncFRConfig() {
-        String path = SwiftCubePathConfig.getInstance().get();
-        SwiftContext.getInstance().getBean(SwiftPathService.class).setSwiftPath(path);
-        boolean zip = SwiftZipConfig.getInstance().get();
-        SwiftContext.getInstance().getBean(SwiftZipService.class).setZip(zip);
-        RepositoryConfigUnique unique = SwiftRepositoryConfig.getInstance().getCurrentRepository();
-        if (null != unique) {
-            SwiftContext.getInstance().getBean(SwiftRepositoryConfService.class).setCurrentRepository(unique.convert());
-        }
-        Map<String, RpcServiceAddressUnique> all = SwiftServiceAddressConfig.getInstance().get();
-        if (!all.isEmpty()) {
-            Iterator<Map.Entry<String, RpcServiceAddressUnique>> iterator = all.entrySet().iterator();
-            SwiftServiceAddressService service = SwiftContext.getInstance().getBean(SwiftServiceAddressService.class);
-            while (iterator.hasNext()) {
-                Map.Entry<String, RpcServiceAddressUnique> entry = iterator.next();
-                service.addOrUpdateAddress(entry.getKey(), entry.getValue().convert());
-            }
-        }
+        SwiftEngineStart.syncConfiguration();
     }
 }
