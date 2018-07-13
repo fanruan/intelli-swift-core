29f71c6fb0501134917e75a9d5e89d00e1286ff1
anchore <anchore@fanruan.com>
2018-07-13 14:49:40 +0800

    swift启动没加表

--- swift-boot/src/main/java/com/fr/swift/boot/SwiftEngineActivator.java
+++ swift-boot/src/main/java/com/fr/swift/boot/SwiftEngineActivator.java
@@ -4,8 +4,10 @@ import com.fr.module.Activator;
 import com.fr.module.extension.Prepare;
 import com.fr.stable.db.constant.BaseDBConstant;
 import com.fr.swift.config.context.SwiftConfigContext;
+import com.fr.swift.config.entity.SwiftConfigEntity;
 import com.fr.swift.config.entity.SwiftMetaDataEntity;
 import com.fr.swift.config.entity.SwiftSegmentEntity;
+import com.fr.swift.config.entity.SwiftSegmentLocationEntity;
 import com.fr.swift.config.entity.SwiftServiceInfoEntity;
 import com.fr.swift.context.SwiftContext;
 import com.fr.swift.cube.queue.ProviderTaskManager;
@@ -45,7 +47,12 @@ public class SwiftEngineActivator extends Activator implements Prepare {
 
     @Override
     public void prepare() {
-        this.addMutable(BaseDBConstant.BASE_ENTITY_KEY, SwiftMetaDataEntity.class, SwiftSegmentEntity.class, SwiftServiceInfoEntity.class);
+        this.addMutable(BaseDBConstant.BASE_ENTITY_KEY,
+                SwiftMetaDataEntity.class,
+                SwiftSegmentEntity.class,
+                SwiftServiceInfoEntity.class,
+                SwiftSegmentLocationEntity.class,
+                SwiftConfigEntity.class);
     }
 
     private void syncFRConfig() {
