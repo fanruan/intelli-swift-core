e983332dbcca8517450c2f154e89fbc5963e5e6b
Yee <yee@fanruan.com>
2018-07-17 13:21:01 +0800

    access

--- swift-base/src/main/java/com/fr/swift/util/ReflectUtils.java
+++ swift-base/src/main/java/com/fr/swift/util/ReflectUtils.java
@@ -78,6 +78,7 @@ public final class ReflectUtils {
                     }
                 }
                 if (marked) {
+                    constructor.setAccessible(true);
                     return constructor;
                 }
             }
