0bbad2c695b378c0ad73bf3a092647c097e21fb7
Yee <yee@fanruan.com>
2018-07-24 09:13:16 +0800

    修改

--- swift-indexing/src/main/java/com/fr/swift/service/SwiftIndexingService.java
+++ swift-indexing/src/main/java/com/fr/swift/service/SwiftIndexingService.java
@@ -201,7 +201,7 @@ public class SwiftIndexingService extends AbstractSwiftService implements Indexi
             Integer tmpPath = entity.getTmpDir();
             entity.setTablePath(tmpPath);
             entity.setLastPath(path);
-            if (tablePathService.saveOrUpdate(entity) && locationService.delete(sourceKey.getId(), getID()) && path.compareTo(tmpPath) != 0) {
+            if (path.compareTo(tmpPath) != 0 && tablePathService.saveOrUpdate(entity) && locationService.delete(sourceKey.getId(), getID())) {
                 String deletePath = String.format("%s/%s/%d/%s",
                         pathService.getSwiftPath(),
                         dataSource.getMetadata().getSwiftSchema().getDir(),
