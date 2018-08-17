8d6aa632b990d25a7b37f1412d30f1719262b8a6
Yee <yee@fanruan.com>
2018-08-17 09:25:11 +0800

    DEC-3794 fineio默认的压缩太慢了 默认使用LZ4压缩 10.0.1依赖feature

--- swift-base/src/main/java/com/fr/swift/cube/io/impl/fineio/connector/Lz4Connector.java
+++ swift-base/src/main/java/com/fr/swift/cube/io/impl/fineio/connector/Lz4Connector.java
@@ -3,23 +3,28 @@ package com.fr.swift.cube.io.impl.fineio.connector;
 import com.fineio.io.file.FileBlock;
 import com.fineio.storage.AbstractConnector;
 import com.fr.swift.util.Strings;
+import com.fr.third.net.jpountz.lz4.LZ4BlockInputStream;
+import com.fr.third.net.jpountz.lz4.LZ4BlockOutputStream;
+import com.fr.third.net.jpountz.lz4.LZ4Compressor;
+import com.fr.third.net.jpountz.lz4.LZ4Factory;
+import com.fr.third.net.jpountz.lz4.LZ4FastDecompressor;
 import com.fr.third.org.apache.commons.io.FileUtils;
+import com.fr.third.org.apache.commons.io.IOUtils;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
-
-//import net.jpountz.lz4.LZ4FrameInputStream;
-//import net.jpountz.lz4.LZ4FrameOutputStream;
 
 /**
  * @author yee
  * @date 2018/8/9
  */
 public class Lz4Connector extends AbstractConnector {
-    //    private static final int BLOCK_SIZE = 32 * 1024 * 1024;
-//    private int compressedLevel = 7;
+        private static final int BLOCK_SIZE = 32 * 1024 * 1024;
     private URI parentURI;
 
     private Lz4Connector(String path) {
@@ -40,37 +45,30 @@ public class Lz4Connector extends AbstractConnector {
 
     @Override
     public InputStream read(FileBlock block) throws IOException {
-//        LZ4FastDecompressor decompressor = LZ4Factory.fastestInstance().fastDecompressor();
-//        LZ4BlockInputStream zis = new LZ4BlockInputStream(is, decompressor);
-//        FileInputStream is = new FileInputStream(getPath(block, false));
-//        return new LZ4FrameInputStream(is);
-        return null;
+        FileInputStream is = new FileInputStream(getPath(block, false));
+        LZ4FastDecompressor decompressor = LZ4Factory.fastestInstance().fastDecompressor();
+        return new LZ4BlockInputStream(is, decompressor);
     }
 
     @Override
     public void write(FileBlock block, InputStream is)
             throws IOException {
-//        LZ4Compressor compressor = LZ4Factory.nativeInstance().fastCompressor();
-//        if (compressedLevel > 17 || compressedLevel <=0) {
-//            compressor = LZ4Factory.fastestInstance().highCompressor(7);
-//        } else {
-//            compressor = LZ4Factory.fastestInstance().highCompressor(compressedLevel);
-//        }
-//        FileOutputStream fos = new FileOutputStream(getPath(block, true));
-//        BufferedOutputStream bos = new BufferedOutputStream(fos);
-//        LZ4BlockOutputStream zos = new LZ4BlockOutputStream(bos, BLOCK_SIZE, compressor);
+        LZ4Compressor compressor = LZ4Factory.nativeInstance().fastCompressor();
+        FileOutputStream fos = new FileOutputStream(getPath(block, true));
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        LZ4BlockOutputStream zos = new LZ4BlockOutputStream(bos, BLOCK_SIZE, compressor);
 //        LZ4FrameOutputStream zos = new LZ4FrameOutputStream(bos);
-//        IOUtils.copy(is, zos);
-//        zos.flush();
-//        if (is != null) {
-//            is.close();
-//        }
-//        if (null != bos) {
-//            bos.close();
-//        }
-//        if (fos != null) {
-//            fos.close();
-//        }
+        IOUtils.copy(is, zos);
+        zos.finish();
+        if (is != null) {
+            is.close();
+        }
+        if (null != bos) {
+            bos.close();
+        }
+        if (fos != null) {
+            fos.close();
+        }
     }
 
     @Override
