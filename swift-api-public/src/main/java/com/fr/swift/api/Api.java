e001d7dff17d4a5f5efb91f5c09db9121245c53c
Yee <yee@fanruan.com>
2018-08-27 17:58:51 +0800

    DEC-4005 swift给云中心提供数据服务

--- swift-api-public/src/main/java/com/fr/swift/api/Api.java
+++ swift-api-public/src/main/java/com/fr/swift/api/Api.java
@@ -3,7 +3,6 @@ package com.fr.swift.api;
 import com.fr.swift.api.rpc.DataMaintenanceService;
 import com.fr.swift.api.rpc.SelectService;
 import com.fr.swift.api.rpc.bean.Column;
-import com.fr.swift.api.rpc.holder.InternalServiceAddressHolder;
 import com.fr.swift.api.rpc.invoke.ApiProxyFactory;
 import com.fr.swift.db.Schema;
 import com.fr.swift.db.Where;
@@ -18,62 +17,74 @@ import java.util.List;
  * @author yee
  * @date 2018/8/24
  */
-public class API implements DataMaintenanceService, SelectService {
-    protected InternalServiceAddressHolder holder;
+public class Api {
+    protected String address;
 
-    private API(String address) {
-        holder = InternalServiceAddressHolder.getHolder(address);
+    private Api(String address) {
+        this.address = address;
     }
 
-    public static API connect(String address) {
-        return new API(address);
+    public static SelectApi connectSelectApi(String address) {
+        return new SelectApi(address);
     }
 
-    @Override
-    public int insert(Schema schema, String tableName, List<String> fields, List<Row> rows) throws SQLException {
-        String address = holder.nextRealTimeAddress();
-        DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
-        return service.insert(schema, tableName, fields, rows);
+    public static DataMaintenanceApi connectDataMaintenanceApi(String address) {
+        return new DataMaintenanceApi(address);
     }
 
-    @Override
-    public int insert(Schema schema, String tableName, List<Row> rows) throws SQLException {
-        return insert(schema, tableName, Collections.<String>emptyList(), rows);
+    public static class SelectApi extends Api implements SelectService {
+
+        private SelectApi(String address) {
+            super(address);
+        }
+
+        @Override
+        public SwiftResultSet query(String queryJson) {
+            SelectService service = ApiProxyFactory.getProxy(SelectService.class, address);
+            return service.query(queryJson);
+        }
     }
 
-    @Override
-    public int insert(Schema schema, String tableName, String queryJson) throws SQLException {
-        String address = holder.nextRealTimeAddress();
-        DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
-        return service.insert(schema, tableName, queryJson);
-    }
+    public static class DataMaintenanceApi extends Api implements DataMaintenanceService {
 
-    @Override
-    public int delete(Schema schema, String tableName, Where where) {
-        String address = holder.nextRealTimeAddress();
-        DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
-        return service.delete(schema, tableName, where);
-    }
+        private DataMaintenanceApi(String address) {
+            super(address);
+        }
 
-    @Override
-    public int update(Schema schema, String tableName, SwiftResultSet resultSet, Where where) {
-        String address = holder.nextRealTimeAddress();
-        DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
-        return service.update(schema, tableName, resultSet, where);
-    }
+        @Override
+        public int insert(Schema schema, String tableName, List<String> fields, List<Row> rows) throws SQLException {
+            DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
+            return service.insert(schema, tableName, fields, rows);
+        }
 
-    @Override
-    public int createTable(Schema schema, String tableName, List<Column> columns) {
-        String address = holder.nextRealTimeAddress();
-        DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
-        return service.createTable(schema, tableName, columns);
-    }
+        @Override
+        public int insert(Schema schema, String tableName, List<Row> rows) throws SQLException {
+            return insert(schema, tableName, Collections.<String>emptyList(), rows);
+        }
 
-    @Override
-    public SwiftResultSet query(String queryJson) {
-        String address = holder.nextAnalyseAddress();
-        SelectService service = ApiProxyFactory.getProxy(SelectService.class, address);
-        return service.query(queryJson);
+        @Override
+        public int insert(Schema schema, String tableName, String queryJson) throws SQLException {
+            DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
+            return service.insert(schema, tableName, queryJson);
+        }
+
+        @Override
+        public int delete(Schema schema, String tableName, Where where) {
+            DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
+            return service.delete(schema, tableName, where);
+        }
+
+        @Override
+        public int update(Schema schema, String tableName, SwiftResultSet resultSet, Where where) {
+            DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
+            return service.update(schema, tableName, resultSet, where);
+        }
+
+        @Override
+        public int createTable(Schema schema, String tableName, List<Column> columns) {
+            DataMaintenanceService service = ApiProxyFactory.getProxy(DataMaintenanceService.class, address);
+            return service.createTable(schema, tableName, columns);
+        }
     }
 
 
