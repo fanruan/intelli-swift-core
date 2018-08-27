318de363fca7fd1029c2e37a05d4ac291919d839
Yee <yee@fanruan.com>
2018-08-27 17:38:57 +0800

    DEC-4005 swift给云中心提供数据服务

--- swift-api-public/src/test/java/com/fr/swift/api/ApiTest.java
+++ swift-api-public/src/test/java/com/fr/swift/api/ApiTest.java
@@ -1,21 +1,27 @@
 package com.fr.swift.api;
 
 import com.fr.swift.api.rpc.SimpleDetailQueryBean;
+import com.fr.swift.api.rpc.invoke.ApiProxyFactory;
 import com.fr.swift.api.rpc.session.impl.SwiftApiSessionFactoryImpl;
 import com.fr.swift.db.Schema;
+import com.fr.swift.netty.rpc.exception.ServiceInvalidException;
+import com.fr.swift.service.cluster.ClusterAnalyseService;
 import com.fr.swift.source.ListBasedRow;
 import com.fr.swift.source.Row;
 import com.fr.swift.source.SwiftResultSet;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.Serializable;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author yee
@@ -23,7 +29,6 @@ import static org.junit.Assert.assertEquals;
  */
 public class ApiTest implements Serializable {
 
-    public static Api api;
     private static List<Row> datas;
     private static SwiftApiSessionFactoryImpl factory;
 
@@ -37,6 +42,7 @@ public class ApiTest implements Serializable {
     }
 
     @Test
+    @Ignore
     public void insert() throws SQLException {
         assertEquals(factory.openSession().insert(Schema.CUBE, "test_table", Arrays.asList("id", "name"), datas), datas.size());
     }
@@ -47,6 +53,7 @@ public class ApiTest implements Serializable {
     }
 
     @Test
+    @Ignore
     public void query() throws SQLException {
 //        aa4f69b2
         SimpleDetailQueryBean bean = new SimpleDetailQueryBean();
@@ -57,6 +64,22 @@ public class ApiTest implements Serializable {
             System.out.println(resultSet.getNextRow().getValue(0));
         }
         resultSet.close();
+    }
+
+    @Test
+    @Ignore
+    public void testException() {
+        ClusterAnalyseService service = ApiProxyFactory.getProxy(ClusterAnalyseService.class, "192.168.0.7:7000");
+        try {
+            SimpleDetailQueryBean bean = new SimpleDetailQueryBean();
+            bean.setTable("e88238c2");
+            bean.setColumns(Arrays.asList("id", "name"));
+            service.getRemoteQueryResult(bean.getQueryString(), null);
+        } catch (UndeclaredThrowableException e) {
+            assertTrue(e.getCause() instanceof ServiceInvalidException);
+        } catch (Exception e) {
+            assertTrue(false);
+        }
     }
 
 }
