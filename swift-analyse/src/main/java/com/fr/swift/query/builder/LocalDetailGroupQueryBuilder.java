edb308990559968ced88ffcd5f67490abc68249d
lyon <Lyon@fanruan.com>
2018-09-05 16:18:49 +0800

    排序结果合并比较器报空

--- swift-analyse/src/main/java/com/fr/swift/query/builder/LocalDetailGroupQueryBuilder.java
+++ swift-analyse/src/main/java/com/fr/swift/query/builder/LocalDetailGroupQueryBuilder.java
@@ -1,6 +1,9 @@
 package com.fr.swift.query.builder;
 
+import com.fr.swift.compare.Comparators;
 import com.fr.swift.context.SwiftContext;
+import com.fr.swift.db.impl.SwiftDatabase;
+import com.fr.swift.log.SwiftLoggers;
 import com.fr.swift.query.filter.FilterBuilder;
 import com.fr.swift.query.filter.SwiftDetailFilterType;
 import com.fr.swift.query.filter.info.FilterInfo;
@@ -13,12 +16,18 @@ import com.fr.swift.query.query.Query;
 import com.fr.swift.query.result.detail.SortDetailResultQuery;
 import com.fr.swift.query.segment.detail.SortDetailSegmentQuery;
 import com.fr.swift.query.sort.Sort;
+import com.fr.swift.query.sort.SortType;
 import com.fr.swift.result.DetailResultSet;
 import com.fr.swift.segment.Segment;
 import com.fr.swift.segment.SwiftSegmentManager;
 import com.fr.swift.segment.column.Column;
+import com.fr.swift.source.ColumnTypeConstants;
+import com.fr.swift.source.ColumnTypeUtils;
+import com.fr.swift.source.SourceKey;
+import com.fr.swift.source.SwiftMetaDataColumn;
 import com.fr.swift.structure.Pair;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -38,7 +47,6 @@ public class LocalDetailGroupQueryBuilder implements LocalDetailQueryBuilder {
         List<Query<DetailResultSet>> queries = new ArrayList<Query<DetailResultSet>>();
         List<Segment> segments = localSegmentProvider.getSegmentsByIds(info.getTable(), info.getQuerySegment());
         List<Dimension> dimensions = info.getDimensions();
-        List<Pair<Sort, Comparator>> comparators = null;
         for (Segment segment : segments) {
             List<FilterInfo> filterInfos = new ArrayList<FilterInfo>();
             filterInfos.add(new SwiftDetailFilterInfo<Object>(null, null, SwiftDetailFilterType.ALL_SHOW));
@@ -49,26 +57,44 @@ public class LocalDetailGroupQueryBuilder implements LocalDetailQueryBuilder {
             List<Sort> sorts = info.getSorts();
             queries.add(new SortDetailSegmentQuery(info.getFetchSize(), columns,
                     FilterBuilder.buildDetailFilter(segment, new GeneralFilterInfo(filterInfos, GeneralFilterInfo.AND)), sorts));
-            if (comparators == null) {
-                comparators = getComparators(columns, sorts);
-                info.setComparators(comparators);
-            }
         }
-        return new SortDetailResultQuery(info.getFetchSize(), queries, comparators);
-    }
-
-    private static List<Pair<Sort, Comparator>> getComparators(List<Pair<Column, IndexInfo>> columnList, List<Sort> sorts) {
-        List<Pair<Sort, Comparator>> pairs = new ArrayList<Pair<Sort, Comparator>>();
-        for (Sort sort : sorts) {
-            // TODO: 2018/7/24 不一定是索引列
-            Comparator comparator = columnList.get(sort.getTargetIndex()).getKey().getDictionaryEncodedColumn().getComparator();
-            pairs.add(Pair.of(sort, comparator));
-        }
-        return pairs;
+        return new SortDetailResultQuery(info.getFetchSize(), queries, getComparators(info.getTable(), info.getSorts()));
     }
 
     @Override
     public Query<DetailResultSet> buildResultQuery(List<Query<DetailResultSet>> queries, DetailQueryInfo info) {
-        return new SortDetailResultQuery(info.getFetchSize(), queries, info.getTargets(), info.getComparators());
+        return new SortDetailResultQuery(info.getFetchSize(), queries, info.getTargets(), getComparators(info.getTable(), info.getSorts()));
+    }
+
+    private static List<Pair<Sort, Comparator>> getComparators(SourceKey table, List<Sort> sorts) {
+        List<Pair<Sort, Comparator>> comparators = new ArrayList<Pair<Sort, Comparator>>();
+        for (Sort sort : sorts) {
+            comparators.add(Pair.of(sort, getComparator(sort.getSortType(), table, sort.getColumnKey().getName())));
+        }
+        return comparators;
+    }
+
+    private static Comparator getComparator(SortType sortType, SourceKey table, String columnName) {
+        ColumnTypeConstants.ClassType type = getClassType(table, columnName);
+        switch (type) {
+            case LONG:
+            case INTEGER:
+            case DATE:
+                return sortType == SortType.ASC ? Comparators.<Long>asc() : Comparators.<Long>desc();
+            case DOUBLE:
+                return sortType == SortType.ASC ? Comparators.<Double>asc() : Comparators.<Double>desc();
+            default:
+                return sortType == SortType.ASC ? Comparators.<String>asc() : Comparators.<String>desc();
+        }
+    }
+
+    private static ColumnTypeConstants.ClassType getClassType(SourceKey table, String columnName) {
+        SwiftMetaDataColumn column = null;
+        try {
+            column = SwiftDatabase.getInstance().getTable(table).getMetadata().getColumn(columnName);
+        } catch (SQLException e) {
+            SwiftLoggers.getLogger().error("failed to read metadata of table: " + table.toString());
+        }
+        return ColumnTypeUtils.getClassType(column);
     }
 }
