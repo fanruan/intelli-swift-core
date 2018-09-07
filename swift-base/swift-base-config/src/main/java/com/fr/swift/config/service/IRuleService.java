54c6479e0076e4ce700956c8e648d4a28186e14a
Yee <yee@fanruan.com>
2018-09-07 13:50:50 +0800

    base 调整

---
+++ swift-base/swift-base-config/src/main/java/com/fr/swift/config/service/IRuleService.java
@@ -0,0 +1,15 @@
+package com.fr.swift.config.service;
+
+/**
+ * @author yee
+ * @date 2018/7/16
+ */
+public interface IRuleService<T> {
+    T getCurrentRule();
+
+    void setCurrentRule(Class<? extends T> clazz);
+
+    void setCurrentRule(T rule);
+
+    void setCurrentRule(String className);
+}
