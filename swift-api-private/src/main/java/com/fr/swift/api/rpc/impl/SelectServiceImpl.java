b80c35e97e49bab9135cae0c6705e92b2df64b6e
lyon <Lyon@fanruan.com>
2018-09-17 10:41:34 +0800

    jdbc调用api返回resultSet序列化问题

--- swift-api-private/src/main/java/com/fr/swift/api/rpc/impl/SelectServiceImpl.java
+++ swift-api-private/src/main/java/com/fr/swift/api/rpc/impl/SelectServiceImpl.java
@@ -10,12 +10,17 @@ import com.fr.swift.db.SwiftDatabase;
 import com.fr.swift.query.info.bean.query.AbstractSingleTableQueryInfoBean;
 import com.fr.swift.query.query.QueryBean;
 import com.fr.swift.query.query.QueryBeanFactory;
+import com.fr.swift.result.DetailResultSet;
+import com.fr.swift.result.serialize.SerializableDetailResultSet;
 import com.fr.swift.service.AnalyseService;
+import com.fr.swift.source.Row;
 import com.fr.swift.source.SwiftMetaData;
 import com.fr.swift.source.SwiftResultSet;
 import com.fr.swift.util.ServiceBeanFactory;
 import com.fr.third.springframework.beans.factory.annotation.Autowired;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -35,21 +40,39 @@ class SelectServiceImpl implements SelectService {
     @SwiftApi
     public SwiftResultSet query(SwiftDatabase database, String queryJson) {
         try {
-            // TODO: 2018/9/16 这边应该取出一页返回
             QueryBean queryBean = SwiftContext.get().getBean(QueryBeanFactory.class).create(queryJson);
             if (queryBean instanceof AbstractSingleTableQueryInfoBean) {
                 String tableName = ((AbstractSingleTableQueryInfoBean) queryBean).getTableName();
                 SwiftMetaData metaData = metaDataService.getMetaDataByKey(tableName);
+                SwiftResultSet resultSet = null;
                 if (null != metaData && metaData.getSwiftDatabase() == database) {
-                    return getAnalyseService().getQueryResult(queryBean);
+                    resultSet = getAnalyseService().getQueryResult(queryBean);
+                } else {
+                    tableService.detectiveMetaData(database, tableName);
+                    resultSet = getAnalyseService().getQueryResult(queryBean);
                 }
-                tableService.detectiveMetaData(database, tableName);
-                return getAnalyseService().getQueryResult(queryBean);
+                return getPageResultSet(queryJson, resultSet);
             }
             throw new UnsupportedOperationException();
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    // 不管分组还是明细，api返回的都是行结果
+    private SerializableDetailResultSet getPageResultSet(String jsonString, SwiftResultSet resultSet) throws SQLException {
+        List<Row> rows = new ArrayList<Row>();
+        int fetchSize = resultSet.getFetchSize();
+        int count = 0;
+        while (resultSet.hasNext() && count < fetchSize) {
+            rows.add(resultSet.getNextRow());
+            count++;
+        }
+        int rowCount = count;
+        if (resultSet instanceof DetailResultSet) {
+            rowCount = ((DetailResultSet) resultSet).getRowCount();
+        }
+        return new SerializableDetailResultSet(jsonString, resultSet.getMetaData(), rows, resultSet.hasNext(), rowCount);
     }
 
     private AnalyseService getAnalyseService() {
