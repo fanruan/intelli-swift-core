b92f6a1801e9557b195222fa3386cd93412be01d
Yee <yee@fanruan.com>
2018-09-26 19:34:44 +0800

    DEC-4655 swift jdbc支持下传json

--- swift-jdbc/src/main/java/com/fr/swift/jdbc/parser/SqlParserFactory.java
+++ swift-jdbc/src/main/java/com/fr/swift/jdbc/parser/SqlParserFactory.java
@@ -5,16 +5,23 @@ import com.fr.general.jsqlparser.parser.CCJSqlParserUtil;
 import com.fr.general.jsqlparser.statement.Statement;
 import com.fr.general.jsqlparser.statement.select.Select;
 import com.fr.swift.db.SwiftDatabase;
+import com.fr.swift.jdbc.SwiftJdbcConstants;
 import com.fr.swift.jdbc.invoke.SqlInvoker;
+import com.fr.swift.jdbc.invoke.impl.SelectInvokerImpl;
 import com.fr.swift.jdbc.proxy.invoke.JdbcCaller;
 
 import java.io.StringReader;
 
 /**
- * Created by pony on 2018/8/17.
+ *
+ * @author pony
+ * @date 2018/8/17
  */
 public class SqlParserFactory {
     public static SqlInvoker parseSql(String sql, SwiftDatabase schema, JdbcCaller.SelectJdbcCaller caller, JdbcCaller.MaintenanceJdbcCaller maintain) throws JSQLParserException {
+        if (isJsonQuery(sql)) {
+            return new SelectInvokerImpl(sql.substring(SwiftJdbcConstants.JSON_QUERY_HEAD_LENGTH), schema, caller);
+        }
         Statement stmt = CCJSqlParserUtil.parse(new StringReader(sql));
         SwiftSqlVisitor visitor = null;
         if (stmt instanceof Select) {
@@ -24,5 +31,9 @@ public class SqlParserFactory {
         }
         stmt.accept(visitor);
         return visitor.get();
+    }
+
+    private static boolean isJsonQuery(String sql) {
+        return sql.trim().toLowerCase().startsWith(SwiftJdbcConstants.KeyWords.JSON_QUERY);
     }
 }
