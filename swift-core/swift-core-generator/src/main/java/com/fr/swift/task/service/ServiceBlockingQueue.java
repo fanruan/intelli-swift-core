9251033e14c46dd2aa91ccee5d9eb712ab6247f1
LUCIFER\lucifer <lucifer@fanruan.com>
2018-11-17 18:36:47 +0800

    REPORT-12732 DEC-5387 1、service task消费队列重构，避免消费者生产，死锁。

---
+++ swift-core/swift-core-generator/src/main/java/com/fr/swift/task/service/ServiceBlockingQueue.java
@@ -0,0 +1,18 @@
+package com.fr.swift.task.service;
+
+import com.fr.swift.source.SourceKey;
+
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * This class created on 2018/11/16
+ *
+ * @author Lucifer
+ * @description
+ * @since Advanced FineBI 5.0
+ */
+public interface ServiceBlockingQueue extends BlockingQueue<ServiceCallable> {
+
+    Integer getNumBySourceKey(SourceKey sourceKey);
+
+}
