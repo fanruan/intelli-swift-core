29f0660a6274e545abdd97a213a8f700b146c692
anchore <anchore@fanruan.com>
2018-11-28 10:18:58 +0800

    DEC-5543 去除pinyin排序的依赖，string排序方式由用户自定义

--- swift/swift-common/src/test/java/com/fr/swift/compare/ComparatorsTest.java
+++ swift/swift-common/src/test/java/com/fr/swift/compare/ComparatorsTest.java
@@ -1,6 +1,5 @@
 package com.fr.swift.compare;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Comparator;
@@ -12,31 +11,6 @@ import static org.junit.Assert.assertEquals;
  * @date 2018/4/13
  */
 public class ComparatorsTest {
-
-    @Test
-    public void testPinyinCompare() {
-        Comparator<String> cmp = Comparators.STRING_ASC;
-        Assert.assertEquals(0, cmp.compare(null, null));
-        Assert.assertEquals(1, cmp.compare("开机动画风格dlfg", null));
-        Assert.assertEquals(-1, cmp.compare(null, "抠脚大汉"));
-
-        Assert.assertEquals(0, cmp.compare("$", "$"));
-        Assert.assertEquals(-1, cmp.compare("a", "b"));
-        Assert.assertEquals(1, cmp.compare("z", "c"));
-        Assert.assertEquals(-1, cmp.compare("x", "X"));
-        Assert.assertEquals(1, cmp.compare("b", "a啊士大夫"));
-
-        Assert.assertEquals(0, cmp.compare("抠脚大汉m", "抠脚大汉m"));
-        Assert.assertEquals(-1, cmp.compare("1抠脚大汉", "抠脚大汉"));
-        Assert.assertEquals(1, cmp.compare("抠脚大汉1", "抠脚大汉"));
-        Assert.assertEquals(-1, cmp.compare("抠脚1大汉", "抠脚大汉1"));
-
-        Assert.assertEquals(0, cmp.compare("摳腳大漢max", "摳腳大漢max"));
-        Assert.assertEquals(-1, cmp.compare("抠脚大汉1", "摳腳大漢1"));
-        Assert.assertEquals(1, cmp.compare("摳腳大漢", "1抠脚大汉"));
-        Assert.assertEquals(-1, cmp.compare("摳腳大漢", "體數據"));
-        Assert.assertEquals(1, cmp.compare("鍋脚大汉", "摳腳大漢"));
-    }
 
     @Test
     public void numberAsc() {
