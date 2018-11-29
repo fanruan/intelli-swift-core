38cb427e7625189c4c4ba5538f256cddbe6346eb
Yee <yee@zyee.me>
2018-11-29 10:30:06 +0800

    REPORT-13086 swift在定义数据连接中成功，数据库查询中swift链接失败 emb不走pool

--- swift-jdbc/src/main/java/com/fr/swift/jdbc/session/impl/SwiftJdbcSessionFactoryImpl.java
+++ swift-jdbc/src/main/java/com/fr/swift/jdbc/session/impl/SwiftJdbcSessionFactoryImpl.java
@@ -2,7 +2,6 @@ package com.fr.swift.jdbc.session.impl;
 
 import com.fr.swift.db.SwiftDatabase;
 import com.fr.swift.jdbc.mode.Mode;
-import com.fr.swift.jdbc.proxy.invoke.ClientProxyPool;
 import com.fr.swift.jdbc.proxy.invoke.JdbcCaller;
 import com.fr.swift.jdbc.rpc.holder.JdbcAddressHolder;
 import com.fr.swift.jdbc.session.SwiftJdbcSession;
@@ -17,27 +16,27 @@ public class SwiftJdbcSessionFactoryImpl implements SwiftJdbcSessionFactory {
     protected JdbcAddressHolder holder;
     private SwiftDatabase schema;
     private static final String DEFAULT_ADDRESS = "localhost:7000";
-    private ClientProxyPool proxyPool;
+    private Mode mode;
 
     public SwiftJdbcSessionFactoryImpl(SwiftDatabase schema, String host, int port) {
         holder = JdbcAddressHolder.getHolder(host, port, Mode.SERVER);
         this.schema = schema;
-        proxyPool = ClientProxyPool.getInstance(Mode.SERVER);
+        mode = Mode.SERVER;
     }
 
     public SwiftJdbcSessionFactoryImpl(SwiftDatabase schema) {
         this.schema = schema;
         holder = JdbcAddressHolder.getHolder(DEFAULT_ADDRESS, Mode.EMB);
-        proxyPool = ClientProxyPool.getInstance(Mode.EMB);
+        mode = Mode.EMB;
     }
 
     @Override
     public SwiftJdbcSession openSession() {
-        return new SwiftJdbcSessionImpl(schema, JdbcCaller.connectSelectService(holder.nextAnalyseAddress(), proxyPool), JdbcCaller.connectMaintenanceService(holder.nextRealTimeAddress(), proxyPool));
+        return new SwiftJdbcSessionImpl(schema, JdbcCaller.connectSelectService(holder.nextAnalyseAddress(), mode), JdbcCaller.connectMaintenanceService(holder.nextRealTimeAddress(), mode));
     }
 
     @Override
     public void close() {
-        proxyPool.close();
+//        proxyPool.close();
     }
 }
