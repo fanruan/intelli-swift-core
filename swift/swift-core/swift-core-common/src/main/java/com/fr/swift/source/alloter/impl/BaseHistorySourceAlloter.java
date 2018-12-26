4d0706511e819b526fca34a628d99a20b14caab4
lyon <Lyon@fanruan.com>
2018-12-26 10:41:29 +0800

    增量按行分块适配

--- swift/swift-core/swift-core-common/src/main/java/com/fr/swift/source/alloter/impl/BaseHistorySourceAlloter.java
+++ swift/swift-core/swift-core-common/src/main/java/com/fr/swift/source/alloter/impl/BaseHistorySourceAlloter.java
@@ -5,10 +5,6 @@ import com.fr.swift.segment.SegmentKey;
 import com.fr.swift.source.SourceKey;
 import com.fr.swift.source.alloter.AllotRule;
 import com.fr.swift.source.alloter.RowInfo;
-import com.fr.swift.source.alloter.SegmentInfo;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author anchore
@@ -16,37 +12,11 @@ import java.util.Map;
  */
 public abstract class BaseHistorySourceAlloter<A extends AllotRule, R extends RowInfo> extends BaseSourceAlloter<A, R> {
 
-    /**
-     * 逻辑order到实际order的映射
-     * 处理order竞争的
-     */
-    private Map<Integer, SegmentState> logicToReal = new HashMap<Integer, SegmentState>();
-
     public BaseHistorySourceAlloter(SourceKey tableKey, A rule) {
         super(tableKey, rule);
     }
 
-    @Override
-    public SegmentInfo allot(R rowInfo) {
-        int logicOrder = getLogicOrder(rowInfo);
-
-        SegmentState segState;
-        if (logicToReal.containsKey(logicOrder)) {
-            // 已分配
-            segState = logicToReal.get(logicOrder);
-        } else {
-            // 新分配
-            segState = append(logicOrder);
-        }
-        if (segState.incrementAndGet() < rule.getCapacity()) {
-            // 未满
-            return segState.getSegInfo();
-        }
-        // 已满，再分配
-        return append(logicOrder).getSegInfo();
-    }
-
-    private SegmentState append(int logicOrder) {
+    protected SegmentState append(int logicOrder) {
         // todo hash出的seg key可能还要写入此seg key的hash值
         // todo 另外还要处理脏配置
         SegmentKey segKey = SEG_SVC.tryAppendSegment(tableKey, StoreType.FINE_IO);
@@ -55,12 +25,4 @@ public abstract class BaseHistorySourceAlloter<A extends AllotRule, R extends Ro
         logicToReal.put(logicOrder, segState);
         return segState;
     }
-
-    /**
-     * 计算逻辑seg order
-     *
-     * @param rowInfo 行
-     * @return logic order
-     */
-    protected abstract int getLogicOrder(R rowInfo);
 }
--- swift/swift-core/swift-core-common/src/main/java/com/fr/swift/source/alloter/impl/line/RealtimeLineSourceAlloter.java
+++ swift/swift-core/swift-core-common/src/main/java/com/fr/swift/source/alloter/impl/line/RealtimeLineSourceAlloter.java
@@ -1,8 +1,20 @@
 package com.fr.swift.source.alloter.impl.line;
 
+import com.fr.swift.SwiftContext;
+import com.fr.swift.config.service.SwiftMetaDataService;
+import com.fr.swift.cube.CubePathBuilder;
+import com.fr.swift.cube.io.Types;
+import com.fr.swift.cube.io.location.ResourceLocation;
+import com.fr.swift.segment.Segment;
+import com.fr.swift.segment.SegmentKey;
 import com.fr.swift.source.SourceKey;
-import com.fr.swift.source.alloter.SegmentInfo;
+import com.fr.swift.source.SwiftMetaData;
 import com.fr.swift.source.alloter.impl.BaseSourceAlloter;
+import com.fr.swift.source.alloter.impl.SwiftSegmentInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author anchore
@@ -10,13 +22,51 @@ import com.fr.swift.source.alloter.impl.BaseSourceAlloter;
  */
 public class RealtimeLineSourceAlloter extends BaseSourceAlloter<LineAllotRule, LineRowInfo> {
 
+    private static final SwiftMetaDataService metaDataService = SwiftContext.get().getBean(SwiftMetaDataService.class);
+
     public RealtimeLineSourceAlloter(SourceKey tableKey, LineAllotRule rule) {
         super(tableKey, rule);
     }
 
     @Override
-    public SegmentInfo allot(LineRowInfo rowInfo) {
-        // 目前想的分到一个最接近满的seg，然后分新块
-        return null;
+    protected SegmentState append(int logicOrder) {
+        SegmentState segState = createSegmentState();
+        logicToReal.put(logicOrder, segState);
+        return segState;
+    }
+
+    private SegmentState createSegmentState() {
+        Map<SourceKey, List<SegmentKey>> keyListMap = SEG_SVC.getOwnSegments();
+        List<SegmentKey> keys = keyListMap.get(tableKey);
+        keys = keys == null ? new ArrayList<SegmentKey>() : keys;
+        SegmentKey segmentKey = null;
+        int cursor = -1;
+        for (SegmentKey key : keys) {
+            if (key.getStoreType() == Types.StoreType.MEMORY) {
+                Segment segment = newRealTimeSeg(key);
+                if (segment.getRowCount() < rule.getCapacity()) {
+                    // 这边假设配置中可能存在多个realTimeSegment的情况下，取出没有满的segment进行插入
+                    segmentKey = key;
+                    cursor = segment.getRowCount() - 1;
+                    break;
+                }
+            }
+        }
+        if (segmentKey == null) {
+            segmentKey = SEG_SVC.tryAppendSegment(tableKey, Types.StoreType.MEMORY);
+        }
+        SwiftSegmentInfo segInfo = new SwiftSegmentInfo(segmentKey.getOrder(), Types.StoreType.MEMORY);
+        return new SegmentState(segInfo, cursor);
+    }
+
+    private Segment newRealTimeSeg(SegmentKey key) {
+        ResourceLocation location = new ResourceLocation(new CubePathBuilder(key).asBackup().build(), key.getStoreType());
+        SwiftMetaData metaData = metaDataService.getMetaDataByKey(tableKey.getId());
+        return SwiftContext.get().getBean("realtimeSegment", Segment.class, location, metaData);
+    }
+
+    @Override
+    protected int getLogicOrder(LineRowInfo rowInfo) {
+        return 0;
     }
 }
