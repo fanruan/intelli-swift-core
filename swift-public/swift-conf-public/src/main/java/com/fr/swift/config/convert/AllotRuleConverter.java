c222024b677ccc9fe9e824e2949177533fef93df
lucifer <lucifer@fanruan.com>
2018-12-27 09:12:14 +0800

    DEC-6114  配合平台下feature单机和集群联调 1、jar class解析 2、部分启动异常

--- swift-public/swift-conf-public/src/main/java/com/fr/swift/config/convert/AllotRuleConverter.java
+++ swift-public/swift-conf-public/src/main/java/com/fr/swift/config/convert/AllotRuleConverter.java
@@ -1,8 +1,8 @@
 package com.fr.swift.config.convert;
 
-import com.fr.swift.base.json.mapper.SwiftBeanMapper;
-import com.fr.swift.config.convert.hibernate.BaseAllotRuleConverter;
+import com.fr.swift.base.json.JsonBuilder;
 import com.fr.swift.source.alloter.AllotRule;
+import com.fr.swift.util.Strings;
 
 import javax.persistence.AttributeConverter;
 
@@ -10,8 +10,22 @@ import javax.persistence.AttributeConverter;
  * @author yee
  * @date 2018-11-27
  */
-public class AllotRuleConverter extends BaseAllotRuleConverter implements AttributeConverter<AllotRule, String> {
-    public AllotRuleConverter() {
-        super(new SwiftBeanMapper());
+public class AllotRuleConverter implements AttributeConverter<AllotRule, String> {
+    @Override
+    public String convertToDatabaseColumn(AllotRule allotRule) {
+        try {
+            return JsonBuilder.writeJsonString(allotRule);
+        } catch (Exception e) {
+            return Strings.EMPTY;
+        }
+    }
+
+    @Override
+    public AllotRule convertToEntityAttribute(String s) {
+        try {
+            return Strings.isNotEmpty(s) ? JsonBuilder.readValue(s, AllotRule.class) : null;
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
