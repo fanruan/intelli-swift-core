fd72129af580261d721053027eb2cd3e38ac6df1
lucifer <lucifer@fanruan.com>
2018-12-27 09:14:10 +0800

    DEC-6114  配合平台下feature单机和集群联调 1、jar class解析 2、部分启动异常

--- swift-public/swift-spring-boot/src/main/java/com/fr/swift/boot/controller/test/TestController.java
+++ swift-public/swift-spring-boot/src/main/java/com/fr/swift/boot/controller/test/TestController.java
@@ -1,19 +1,32 @@
 package com.fr.swift.boot.controller.test;
 
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import com.fr.swift.SwiftContext;
+import com.fr.swift.config.bean.MetaDataColumnBean;
+import com.fr.swift.config.bean.SwiftMetaDataBean;
+import com.fr.swift.config.service.SwiftMetaDataService;
+import com.fr.swift.data.importing.SwiftImportResultSet;
+import com.fr.swift.data.importing.file.SingleStreamImportResultSet;
+import com.fr.swift.data.importing.file.impl.TabLineParser;
+import com.fr.swift.db.SwiftDatabase;
+import com.fr.swift.db.Table;
+import com.fr.swift.query.info.bean.query.QueryBeanFactory;
+import com.fr.swift.query.query.QueryBean;
+import com.fr.swift.segment.insert.HistoryBlockImporter;
+import com.fr.swift.source.SourceKey;
+import com.fr.swift.source.SwiftMetaData;
+import com.fr.swift.source.SwiftMetaDataColumn;
+import com.fr.swift.source.alloter.impl.line.HistoryLineSourceAlloter;
+import com.fr.swift.source.alloter.impl.line.LineAllotRule;
+import com.fr.swift.source.resultset.LimitedResultSet;
+import com.fr.swift.source.resultset.progress.ProgressResultSet;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.sql.Types;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * This class created on 2018/6/13
@@ -25,42 +38,82 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    //    @ResponseBody
+//    @RequestMapping(value = "swift/test/string", method = RequestMethod.GET)
+//    public String testPrint(HttpServletResponse response, HttpServletRequest request) {
+//        System.out.println("swift/test");
+//        return String.valueOf(System.currentTimeMillis());
+//    }
+//
+//    @ResponseBody
+//    @RequestMapping(value = "swift/test/map", method = RequestMethod.GET)
+//    public Object testPrint1(HttpServletResponse response, HttpServletRequest request) {
+//        System.out.println("swift/test");
+//        Map map = new HashMap();
+//        map.put("1", System.currentTimeMillis());
+//        return map;
+//    }
+//
+//    @ResponseBody
+//    @RequestMapping(value = "swift/test/{id}", method = RequestMethod.GET)
+//    public Object testPrint2(HttpServletResponse response, HttpServletRequest request, @PathVariable("id") String id) {
+//        System.out.println("swift/test");
+//        List<String> list = new ArrayList<String>();
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        return list;
+//    }
+//
+//    @ResponseBody
+//    @RequestMapping(value = "swift/test/postdata", method = RequestMethod.POST)
+//    public Object testPrint2(HttpServletResponse response, HttpServletRequest request,
+//                             @RequestBody(required = false) Map<String, Object> requestMap) {
+//        System.out.println("swift/test");
+//        List<String> list = new ArrayList<String>();
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        list.add(String.valueOf(System.currentTimeMillis()));
+//        return list;
+//    }
+    @RequestMapping("swift/import")
     @ResponseBody
-    @RequestMapping(value = "swift/test/string", method = RequestMethod.GET)
-    public String testPrint(HttpServletResponse response, HttpServletRequest request) {
-        System.out.println("swift/test");
-        return String.valueOf(System.currentTimeMillis());
+    public Boolean testImport(String path) throws Exception {
+        Table table = com.fr.swift.db.impl.SwiftDatabase.getInstance().getTable(new SourceKey("test_yiguan"));
+        SwiftImportResultSet resultSet = new SingleStreamImportResultSet(table.getMetadata(), path, new TabLineParser(false));
+        HistoryLineSourceAlloter alloter = new HistoryLineSourceAlloter(new SourceKey("test_yiguan"), new LineAllotRule(10000));
+
+        HistoryBlockImporter importer = new HistoryBlockImporter(table,
+                alloter);
+        importer.importData(new ProgressResultSet(new LimitedResultSet(resultSet, 1000000), "test_yiguan"));
+        return true;
     }
 
+    @RequestMapping("swift/createTable")
     @ResponseBody
-    @RequestMapping(value = "swift/test/map", method = RequestMethod.GET)
-    public Object testPrint1(HttpServletResponse response, HttpServletRequest request) {
-        System.out.println("swift/test");
-        Map map = new HashMap();
-        map.put("1", System.currentTimeMillis());
-        return map;
+    public Boolean createTable() {
+        SwiftMetaData metaData = new SwiftMetaDataBean();
+        ((SwiftMetaDataBean) metaData).setId("test_yiguan");
+        ((SwiftMetaDataBean) metaData).setSwiftDatabase(SwiftDatabase.CUBE);
+        ((SwiftMetaDataBean) metaData).setTableName("test_yiguan");
+        List<SwiftMetaDataColumn> metaDataColumns = new ArrayList<SwiftMetaDataColumn>();
+        metaDataColumns.add(new MetaDataColumnBean("id", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("currentTime", Types.TIMESTAMP));
+        metaDataColumns.add(new MetaDataColumnBean("eventType", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("eventName", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("json", Types.VARCHAR));
+        metaDataColumns.add(new MetaDataColumnBean("longValue", Types.BIGINT));
+
+        ((SwiftMetaDataBean) metaData).setFields(metaDataColumns);
+        SwiftContext.get().getBean(SwiftMetaDataService.class).addMetaData("test_yiguan", metaData);
+        return true;
     }
 
+    @RequestMapping("swift/query")
     @ResponseBody
-    @RequestMapping(value = "swift/test/{id}", method = RequestMethod.GET)
-    public Object testPrint2(HttpServletResponse response, HttpServletRequest request, @PathVariable("id") String id) {
-        System.out.println("swift/test");
-        List<String> list = new ArrayList<String>();
-        list.add(String.valueOf(System.currentTimeMillis()));
-        list.add(String.valueOf(System.currentTimeMillis()));
-        list.add(String.valueOf(System.currentTimeMillis()));
-        return list;
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "swift/test/postdata", method = RequestMethod.POST)
-    public Object testPrint2(HttpServletResponse response, HttpServletRequest request,
-                             @RequestBody(required = false) Map<String, Object> requestMap) {
-        System.out.println("swift/test");
-        List<String> list = new ArrayList<String>();
-        list.add(String.valueOf(System.currentTimeMillis()));
-        list.add(String.valueOf(System.currentTimeMillis()));
-        list.add(String.valueOf(System.currentTimeMillis()));
-        return list;
+    public QueryBean test() throws Exception {
+        String json = "{\"filter\":{\"filterValue\":[{\"filterValue\":[{\"filterValue\":[{\"filterValue\":{\"endIncluded\":false,\"start\":\"1545667200000\",\"startIncluded\":true},\"column\":\"time\",\"type\":\"NUMBER_IN_RANGE\"},{\"filterValue\":{\"endIncluded\":true,\"end\":\"1545839999000\",\"startIncluded\":false},\"column\":\"time\",\"type\":\"NUMBER_IN_RANGE\"}],\"type\":\"AND\"}],\"type\":\"AND\"}],\"type\":\"AND\"},\"fetchSize\":200,\"sorts\":[{\"name\":\"time\",\"type\":\"DESC\"}],\"tableName\":\"fine_record_operate\",\"dimensions\":[{\"column\":\"time\",\"type\":\"DETAIL\"},{\"column\":\"type\",\"type\":\"DETAIL\"},{\"column\":\"item\",\"type\":\"DETAIL\"},{\"column\":\"resource\",\"type\":\"DETAIL\"},{\"column\":\"operate\",\"type\":\"DETAIL\"},{\"column\":\"username\",\"type\":\"DETAIL\"},{\"column\":\"detail\",\"type\":\"DETAIL\"},{\"column\":\"ip\",\"type\":\"DETAIL\"},{\"column\":\"requestParam\",\"type\":\"DETAIL\"}],\"queryType\":\"DETAIL\",\"queryId\":\"e630c0c3-6266-4a4a-a739-e04e741396ed\"}";
+        QueryBean queryBean = QueryBeanFactory.create(json);
+        return queryBean;
     }
 }
