6aa5e9dbc249ea0a66b47ccc78d832d52ac2c41c
lucifer <lucifer@fanruan.com>
2018-12-27 09:14:40 +0800

    DEC-6114  配合平台下feature单机和集群联调 1、jar class解析 2、部分启动异常

--- swift/swift-base/swift-base-config/src/main/java/com/fr/swift/config/dao/impl/SwiftTablePathDaoImpl.java
+++ swift/swift-base/swift-base-config/src/main/java/com/fr/swift/config/dao/impl/SwiftTablePathDaoImpl.java
@@ -1,5 +1,6 @@
 package com.fr.swift.config.dao.impl;
 
+import com.fr.swift.beans.annotation.SwiftBean;
 import com.fr.swift.config.bean.SwiftTablePathBean;
 import com.fr.swift.config.dao.BasicDao;
 import com.fr.swift.config.dao.SwiftTablePathDao;
@@ -9,6 +10,7 @@ import com.fr.swift.config.oper.impl.RestrictionFactoryImpl;
  * @author yee
  * @date 2018-11-28
  */
+@SwiftBean
 public class SwiftTablePathDaoImpl extends BasicDao<SwiftTablePathBean> implements SwiftTablePathDao {
     public SwiftTablePathDaoImpl() {
         super(SwiftTablePathBean.TYPE, RestrictionFactoryImpl.INSTANCE);
