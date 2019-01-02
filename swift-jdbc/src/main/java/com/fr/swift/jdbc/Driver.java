ff79ca0a8046bfd1cb63ec1554326d8dda265e10
Yee <yee@fanruan.com>
2019-01-02 17:59:49 +0800

    移动下metadata相关实现，单独jdbc跑不起来

--- swift-jdbc/src/main/java/com/fr/swift/jdbc/Driver.java
+++ swift-jdbc/src/main/java/com/fr/swift/jdbc/Driver.java
@@ -4,6 +4,7 @@ import com.fr.swift.api.info.AuthRequestInfo;
 import com.fr.swift.api.server.response.ApiResponse;
 import com.fr.swift.api.server.response.AuthResponse;
 import com.fr.swift.jdbc.exception.Exceptions;
+import com.fr.swift.jdbc.rpc.JdbcExecutor;
 import com.fr.swift.jdbc.sql.BaseSwiftConnection;
 import com.fr.swift.jdbc.sql.ConnectionConfig;
 import com.fr.swift.jdbc.sql.UnregisteredDriver;
@@ -52,10 +53,12 @@ public class Driver extends UnregisteredDriver {
         }
         AuthRequestInfo info = new AuthRequestInfo(config.swiftUser(), config.swiftPassword());
         info.setFrom(holder.getConnectUri().getHost() + ":" + holder.getConnectUri().getPort());
-        ApiResponse response = holder.getRequestService().applyWithRetry(config.requestExecutor(), info, 3);
+        JdbcExecutor executor = config.requestExecutor();
+        ApiResponse response = holder.getRequestService().applyWithRetry(executor, info, 3);
         if (response.isError()) {
             throw Exceptions.sql(response.statusCode(), response.description());
         }
+        executor.stop();
         // 结果应该包括用户校验码以及 realtime和analyse服务的地址
         AuthResponse result = (AuthResponse) response.result();
         holder.setAuthCode(result.getAuthCode());
