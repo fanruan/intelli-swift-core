e2dcfb2c0b1c7b5ab3c0aa74c525181ab72fce47
anchore <anchore@fanruan.com>
2019-01-12 14:48:46 +0800

    Feature：default logger 改用System

--- swift-finereport/swift-common-fr/src/main/java/com/fr/swift/log/SwiftFrLogger.java
+++ swift-finereport/swift-common-fr/src/main/java/com/fr/swift/log/SwiftFrLogger.java
@@ -2,150 +2,55 @@ package com.fr.swift.log;
 
 import com.fr.log.FineLoggerFactory;
 import com.fr.swift.log.impl.BaseSwiftLogger;
-import org.slf4j.helpers.FormattingTuple;
+import com.fr.swift.structure.Pair;
+import com.fr.swift.util.Optional;
 
 /**
  * @author anchore
  * @date 2018/7/4
  */
 class SwiftFrLogger extends BaseSwiftLogger implements SwiftLogger {
+
     SwiftFrLogger() {
     }
 
     @Override
-    public String getName() {
-        return FineLoggerFactory.getLogger().toString();
-    }
-
-    @Override
-    public void trace(String s) {
-    }
-
-    @Override
-    public void trace(String s, Object o) {
-    }
-
-    @Override
-    public void trace(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void trace(String s, Object... objects) {
-    }
-
-    @Override
-    public void trace(String s, Throwable throwable) {
-    }
-
-    @Override
-    public void debug(String s) {
-        FineLoggerFactory.getLogger().debug(s);
-    }
-
-    @Override
-    public void debug(String s, Object o) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o);
-        FineLoggerFactory.getLogger().debug(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void debug(String s, Object o, Object o1) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o, o1);
-        FineLoggerFactory.getLogger().debug(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
     public void debug(String s, Object... objects) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, objects);
-        FineLoggerFactory.getLogger().debug(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void debug(String s, Throwable throwable) {
-        FineLoggerFactory.getLogger().debug(s, throwable);
-    }
-
-    @Override
-    public void info(String s) {
-        FineLoggerFactory.getLogger().info(s);
-    }
-
-    @Override
-    public void info(String s, Object o) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o);
-        FineLoggerFactory.getLogger().info(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void info(String s, Object o, Object o1) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o, o1);
-        FineLoggerFactory.getLogger().info(tuple.getMessage(), tuple.getThrowable());
+        Pair<String, Optional<Throwable>> pair = format(s, objects);
+        if (pair.getValue().isPresent()) {
+            FineLoggerFactory.getLogger().debug(pair.getKey(), pair.getValue().get());
+        } else {
+            FineLoggerFactory.getLogger().debug(pair.getKey());
+        }
     }
 
     @Override
     public void info(String s, Object... objects) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, objects);
-        FineLoggerFactory.getLogger().info(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void info(String s, Throwable throwable) {
-        FineLoggerFactory.getLogger().info(s, throwable);
-    }
-
-    @Override
-    public void warn(String s) {
-        FineLoggerFactory.getLogger().warn(s);
-    }
-
-    @Override
-    public void warn(String s, Object o) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o);
-        FineLoggerFactory.getLogger().warn(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void warn(String s, Object o, Object o1) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o, o1);
-        FineLoggerFactory.getLogger().warn(tuple.getMessage(), tuple.getThrowable());
+        Pair<String, Optional<Throwable>> pair = format(s, objects);
+        if (pair.getValue().isPresent()) {
+            FineLoggerFactory.getLogger().info(pair.getKey(), pair.getValue().get());
+        } else {
+            FineLoggerFactory.getLogger().info(pair.getKey());
+        }
     }
 
     @Override
     public void warn(String s, Object... objects) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, objects);
-        FineLoggerFactory.getLogger().warn(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void warn(String s, Throwable throwable) {
-        FineLoggerFactory.getLogger().warn(s, throwable);
-    }
-
-    @Override
-    public void error(String s) {
-        FineLoggerFactory.getLogger().error(s);
-    }
-
-    @Override
-    public void error(String s, Object o) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o);
-        FineLoggerFactory.getLogger().error(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void error(String s, Object o, Object o1) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, o, o1);
-        FineLoggerFactory.getLogger().error(tuple.getMessage(), tuple.getThrowable());
+        Pair<String, Optional<Throwable>> pair = format(s, objects);
+        if (pair.getValue().isPresent()) {
+            FineLoggerFactory.getLogger().warn(pair.getKey(), pair.getValue().get());
+        } else {
+            FineLoggerFactory.getLogger().warn(pair.getKey());
+        }
     }
 
     @Override
     public void error(String s, Object... objects) {
-        FormattingTuple tuple = BaseSwiftLogger.format(s, objects);
-        FineLoggerFactory.getLogger().error(tuple.getMessage(), tuple.getThrowable());
-    }
-
-    @Override
-    public void error(String s, Throwable throwable) {
-        FineLoggerFactory.getLogger().error(s, throwable);
+        Pair<String, Optional<Throwable>> pair = format(s, objects);
+        if (pair.getValue().isPresent()) {
+            FineLoggerFactory.getLogger().error(pair.getKey(), pair.getValue().get());
+        } else {
+            FineLoggerFactory.getLogger().error(pair.getKey());
+        }
     }
 }
--- swift/swift-base/swift-base-common/src/main/java/com/fr/swift/log/impl/NoopLogger.java
+++ swift/swift-base/swift-base-common/src/main/java/com/fr/swift/log/impl/NoopLogger.java
@@ -5,108 +5,20 @@ package com.fr.swift.log.impl;
  * @date 2019/1/3
  */
 public class NoopLogger extends BaseSwiftLogger {
+
     @Override
-    public String getName() {
-        return getClass().getSimpleName();
+    public void debug(String msg, Object... args) {
     }
 
     @Override
-    public void trace(String s) {
+    public void info(String msg, Object... args) {
     }
 
     @Override
-    public void trace(String s, Object o) {
+    public void warn(String msg, Object... args) {
     }
 
     @Override
-    public void trace(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void trace(String s, Object... objects) {
-    }
-
-    @Override
-    public void trace(String s, Throwable throwable) {
-    }
-
-    @Override
-    public void debug(String s) {
-    }
-
-    @Override
-    public void debug(String s, Object o) {
-    }
-
-    @Override
-    public void debug(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void debug(String s, Object... objects) {
-    }
-
-    @Override
-    public void debug(String s, Throwable throwable) {
-    }
-
-    @Override
-    public void info(String s) {
-    }
-
-    @Override
-    public void info(String s, Object o) {
-    }
-
-    @Override
-    public void info(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void info(String s, Object... objects) {
-    }
-
-    @Override
-    public void info(String s, Throwable throwable) {
-    }
-
-    @Override
-    public void warn(String s) {
-    }
-
-    @Override
-    public void warn(String s, Object o) {
-    }
-
-    @Override
-    public void warn(String s, Object... objects) {
-    }
-
-    @Override
-    public void warn(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void warn(String s, Throwable throwable) {
-    }
-
-    @Override
-    public void error(String s) {
-    }
-
-    @Override
-    public void error(String s, Object o) {
-    }
-
-    @Override
-    public void error(String s, Object o, Object o1) {
-    }
-
-    @Override
-    public void error(String s, Object... objects) {
-    }
-
-    @Override
-    public void error(String s, Throwable throwable) {
+    public void error(String msg, Object... args) {
     }
 }
