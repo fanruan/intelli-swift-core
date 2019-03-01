3d2c84b20f3536107eadc2da6d33e6afddf2783e
Yee <yee@fanruan.com>
2019-03-01 11:08:23 +0800

    REPORT-15145 Swift云中心部署 swift单独部署jar包扫描出错起不来 多次下载报错 hibernate配置及rooturl支持从外部配置 将UploadInfo加回来 方便支持json传输

--- src/main/java/com/fr/swift/cloud/SwiftCloudConstants.java
+++ src/main/java/com/fr/swift/cloud/SwiftCloudConstants.java
@@ -1,13 +1,25 @@
 package com.fr.swift.cloud;
 
 
+import com.fr.swift.util.IoUtil;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author yee
  * @date 2019-02-25
  */
 public class SwiftCloudConstants {
-    // TODO 2019/02/27 临时地址
-    private static final String ROOT_URL = "http://192.168.5.83:3008";
+    /**
+     * 下载的zip存储的位置
+     */
+    public static final String ZIP_FILE_PATH = String.format("%s/analyseSourceData", System.getProperty("user.dir"));
+    private static String ROOT_URL;
+
+
     private static final String ANALYZE_URL = "/api/v1/analyze";
     static final String ANALYZE_AUTH_URL = ROOT_URL + ANALYZE_URL;
 
@@ -23,4 +35,31 @@ public class SwiftCloudConstants {
      * Upload
      */
     static final String UPLOAD_URL = ANALYZE_AUTH_URL + "/upload/report";
+
+    /**
+     * root url可以从外部读取
+     * PS 主要为了方便debug
+     */
+    static {
+        File config = new File("cloud.properties");
+        InputStream inputStream = null;
+        try {
+            if (config.exists()) {
+                inputStream = new FileInputStream(config);
+            } else {
+                inputStream = SwiftCloudConstants.class.getResourceAsStream("/cloud.properties");
+            }
+            if (null != inputStream) {
+                Properties properties = new Properties();
+                properties.load(inputStream);
+                ROOT_URL = properties.getProperty("url.root", "https://market.fanruan.com");
+            } else {
+                ROOT_URL = "https://market.fanruan.com";
+            }
+        } catch (Exception ignore) {
+            ROOT_URL = "https://market.fanruan.com";
+        } finally {
+            IoUtil.close(inputStream);
+        }
+    }
 }
