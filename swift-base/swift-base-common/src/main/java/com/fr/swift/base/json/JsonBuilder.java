5ce9a40ce0c8da18ff74aa7692c8da2c79e30ab9
Yee <yee@fanruan.com>
2019-05-09 14:44:22 +0800

    KERNEL-517 动态修改字节码，将private和public的一些类统一 test

--- swift-base/swift-base-common/src/main/java/com/fr/swift/base/json/JsonBuilder.java
+++ swift-base/swift-base-common/src/main/java/com/fr/swift/base/json/JsonBuilder.java
@@ -11,8 +11,6 @@ import java.util.Map;
  * @date 2018-12-04
  */
 public class JsonBuilder {
-    //    private static final Map<Class<? extends BeanMapper>, BeanMapper> MAPPERS = new ConcurrentHashMap<Class<? extends BeanMapper>, BeanMapper>();
-//    private static final Map<Class, BeanMapper> BEAN_MAPPER_MAP = new ConcurrentHashMap<Class, BeanMapper>();
     private static BeanMapper DEFAULT;
 
     static {
@@ -24,8 +22,6 @@ public class JsonBuilder {
     }
 
     public static <T> T readValue(Map<String, Object> jsonMap, Class<T> reference) throws Exception {
-//        BeanMapper mapper = DEFAULT;
-//        return mapper.map2Object(jsonMap, reference);
         return DEFAULT.map2Object(jsonMap, reference);
     }
 
@@ -34,64 +30,15 @@ public class JsonBuilder {
     }
 
     public static <T> T readValue(String jsonString, Class<T> reference) throws Exception {
-//        BeanMapper mapper;
-//        if (!ReflectUtils.isAssignable(reference, Map.class) && reference.isAnnotationPresent(JsonMapper.class)) {
-//            JsonMapper jsonMapper = reference.getAnnotation(JsonMapper.class);
-//            mapper = getMapper(jsonMapper);
-//        } else {
-//            mapper = calculateMapper(reference);
-//        }
-//        if (null == mapper) {
-//            mapper = DEFAULT;
-//        }
-//        return mapper.string2Object(jsonString, reference);
         return DEFAULT.string2Object(jsonString, reference);
     }
 
     public static String writeJsonString(Object o) throws Exception {
-//        if (null == o) {
-//            return Strings.EMPTY;
-//        }
-//        Class reference = o.getClass();
-//        BeanMapper mapper = calculateMapper(reference);
-//        if (null == mapper) {
-//            mapper = DEFAULT;
-//        }
-//        BEAN_MAPPER_MAP.put(reference, mapper);
-//        return mapper.writeValueAsString(o);
-        return DEFAULT.writeValueAsString(o);
+        return writeJsonString(o, null);
     }
 
-//    private static BeanMapper calculateMapper(Class ref) throws Exception {
-//        if (BEAN_MAPPER_MAP.containsKey(ref)) {
-//            return BEAN_MAPPER_MAP.get(ref);
-//        }
-//        for (Class anInterface : ref.getInterfaces()) {
-//            if (anInterface.isAnnotationPresent(JsonMapper.class)) {
-//                JsonMapper jsonMapper = (JsonMapper) anInterface.getAnnotation(JsonMapper.class);
-//                BeanMapper mapper = getMapper(jsonMapper);
-//                BEAN_MAPPER_MAP.put(ref, mapper);
-//                return mapper;
-//            }
-//        }
-//        Class superClass = ref.getSuperclass();
-//        if (null == superClass || superClass.equals(Object.class)) {
-//            return null;
-//        }
-//        return calculateMapper(superClass);
-//    }
-//
-//    private static BeanMapper getMapper(JsonMapper jsonMapper) throws Exception {
-//        BeanMapper mapper;
-//        Class<? extends BeanMapper> mapperClass = jsonMapper.value();
-//        if (MAPPERS.containsKey(mapperClass)) {
-//            mapper = MAPPERS.get(mapperClass);
-//        } else {
-//            Constructor<? extends BeanMapper> constructor = mapperClass.getDeclaredConstructor();
-//            constructor.setAccessible(true);
-//            mapper = constructor.newInstance();
-//            MAPPERS.put(mapperClass, mapper);
-//        }
-//        return mapper;
-//    }
+    public static <T> String writeJsonString(T o, BeanTypeReference<T> reference) throws Exception {
+        return DEFAULT.writeValueAsString(o, reference);
+    }
+
 }
