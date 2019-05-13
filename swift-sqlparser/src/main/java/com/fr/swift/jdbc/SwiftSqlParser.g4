c316b7b447cfd0868ecf9e7c32afb24926053b30
anchore <anchore@fanruan.com>
2019-05-13 16:59:24 +0800

    KERNEL-472 自定义swift sql语法，并生成parser

---
+++ swift-sqlparser/src/main/java/com/fr/swift/jdbc/SwiftSqlParser.g4
@@ -0,0 +1,128 @@
+parser grammar SwiftSqlParser;
+
+/*
+如何生成Parser？
+
+cd到g4文件目录，cp指定antlr4-tool jar的path
+
+java -cp antlr4-4.5.3.jar org.antlr.v4.Tool -package com.fr.swift.jdbc.antlr4 -o antlr4 SwiftSqlLexer.g4 SwiftSqlParser.
+g4
+
+Parser即生成到了com.fr.swift.jdbc.antlr4包下
+*/
+
+options {
+    language = Java;
+    tokenVocab = SwiftSqlLexer;
+}
+
+root: sqls EOF;
+
+sqls: sql SEMI? | sql (SEMI sql)* SEMI?;
+
+sql: ddl | dml;
+
+ddl: createTable | dropTable | alterTable;
+
+dml: insert | delete | select;
+
+// ddl
+createTable:
+    CREATE TABLE tableName = name L_PAR columnDefinitions R_PAR /*partitionBy?*/;
+
+columnDefinitions:
+    name columnDefinition (COMMA name columnDefinition)*?;
+
+// todo 分块
+//partitionBy:
+//    PARTITION BY (
+//        LINE L_PAR capacity = NUMERIC_LITERAL COMMA memCapacity = NUMERIC_LITERAL R_PAR
+//        | HASH functionCall
+//        | RANGE
+//    );
+
+dataType:
+    BIT | TINYINT | SMALLINT | INTEGER | BIGINT
+    | FLOAT | REAL | DOUBLE | NUMERIC | DECIMAL
+    | CHAR | VARCHAR | LONGVARCHAR
+    | DATE | TIME | TIMESTAMP
+    | BOOLEAN;
+
+function: MAX | MIN | SUM | AVG | COUNT | MID
+        | TODATE;
+
+columnDefinition:
+    dataType (L_PAR length = NUMERIC_LITERAL R_PAR)?/* (
+        (NOT)? NULL
+    )?*/;
+
+dropTable: DROP TABLE tableName = name;
+
+alterTable: alterTableAddColumn | alterTableDropColumn;
+
+alterTableAddColumn:
+    ALTER TABLE tableName = name ADD columnDefinitions;
+
+alterTableDropColumn:
+    ALTER TABLE tableName = name DROP columnNames = names;
+
+// dml
+insert:
+    INSERT INTO tableName = name (
+        L_PAR columnNames = names R_PAR
+    )? VALUES L_PAR values R_PAR;
+
+values: literal (COMMA literal)*;
+
+delete: DELETE FROM tableName = name WHERE where = expr;
+
+select:
+    SELECT DISTINCT? (
+        MUL
+        | (name | funcCall) (COMMA name | funcCall)*
+    ) FROM tableName = name (WHERE where = expr)? (
+        GROUP BY columnNames = names
+    )? (HAVING having = expr)? (ORDER BY columnOrders)? (
+        LIMIT limit = NUMERIC_LITERAL
+    )?;
+
+columnOrders: name (ASC | DESC)? ( COMMA name (ASC | DESC)?)*;
+
+names: name (COMMA name)*;
+
+expr:
+    simpleExpr
+    | simpleExpr binaryOp simpleExpr
+    | expr (unaryBoolOp)? binaryBoolOp expr
+    | unaryBoolOp expr
+    | funcCall
+    | keywordBoolExpr;
+
+keywordBoolExpr:
+    IDENTIFIER NOT? inExpr
+    | IDENTIFIER NOT? betweenExpr
+    | IDENTIFIER IS NOT? NULL;
+
+inExpr: IN L_PAR literal (COMMA literal)* R_PAR;
+
+betweenExpr: BETWEEN literal AND literal;
+
+simpleExpr: literal | IDENTIFIER;
+
+funcCall:
+    function L_PAR (DISTINCT? simpleExpr (COMMA simpleExpr)*)? R_PAR;
+
+literal: NULL | NUMERIC_LITERAL | STRING_LITERAL;
+
+unaryBoolOp: NOT;
+
+binaryOp: PLUS | MINUS | MUL | DIV;
+
+binaryBoolOp:
+    GREATER | GEQ
+    | EQ | NEQ
+    | LEQ | LESS
+    | AND | OR
+    | LIKE;
+
+name: IDENTIFIER | L_PAR name R_PAR;
---
+++ swift-sqlparser/src/main/java/com/fr/swift/jdbc/antlr4/SwiftSqlParserBaseListener.java
@@ -0,0 +1,607 @@
+// Generated from SwiftSqlParser.g4 by ANTLR 4.5.3
+package com.fr.swift.jdbc.antlr4;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link SwiftSqlParserListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+public class SwiftSqlParserBaseListener implements SwiftSqlParserListener {
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterRoot(SwiftSqlParser.RootContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitRoot(SwiftSqlParser.RootContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterSqls(SwiftSqlParser.SqlsContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitSqls(SwiftSqlParser.SqlsContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterSql(SwiftSqlParser.SqlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitSql(SwiftSqlParser.SqlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterDdl(SwiftSqlParser.DdlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitDdl(SwiftSqlParser.DdlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterDml(SwiftSqlParser.DmlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitDml(SwiftSqlParser.DmlContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterCreateTable(SwiftSqlParser.CreateTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitCreateTable(SwiftSqlParser.CreateTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterColumnDefinitions(SwiftSqlParser.ColumnDefinitionsContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitColumnDefinitions(SwiftSqlParser.ColumnDefinitionsContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterDataType(SwiftSqlParser.DataTypeContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitDataType(SwiftSqlParser.DataTypeContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterFunction(SwiftSqlParser.FunctionContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitFunction(SwiftSqlParser.FunctionContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterColumnDefinition(SwiftSqlParser.ColumnDefinitionContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitColumnDefinition(SwiftSqlParser.ColumnDefinitionContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterDropTable(SwiftSqlParser.DropTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitDropTable(SwiftSqlParser.DropTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterAlterTable(SwiftSqlParser.AlterTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitAlterTable(SwiftSqlParser.AlterTableContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterAlterTableAddColumn(SwiftSqlParser.AlterTableAddColumnContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitAlterTableAddColumn(SwiftSqlParser.AlterTableAddColumnContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterAlterTableDropColumn(SwiftSqlParser.AlterTableDropColumnContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitAlterTableDropColumn(SwiftSqlParser.AlterTableDropColumnContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterInsert(SwiftSqlParser.InsertContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitInsert(SwiftSqlParser.InsertContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterValues(SwiftSqlParser.ValuesContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitValues(SwiftSqlParser.ValuesContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterDelete(SwiftSqlParser.DeleteContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitDelete(SwiftSqlParser.DeleteContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterSelect(SwiftSqlParser.SelectContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitSelect(SwiftSqlParser.SelectContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterColumnOrders(SwiftSqlParser.ColumnOrdersContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitColumnOrders(SwiftSqlParser.ColumnOrdersContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterNames(SwiftSqlParser.NamesContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitNames(SwiftSqlParser.NamesContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterExpr(SwiftSqlParser.ExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitExpr(SwiftSqlParser.ExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterKeywordBoolExpr(SwiftSqlParser.KeywordBoolExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitKeywordBoolExpr(SwiftSqlParser.KeywordBoolExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterInExpr(SwiftSqlParser.InExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitInExpr(SwiftSqlParser.InExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterBetweenExpr(SwiftSqlParser.BetweenExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitBetweenExpr(SwiftSqlParser.BetweenExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterSimpleExpr(SwiftSqlParser.SimpleExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitSimpleExpr(SwiftSqlParser.SimpleExprContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterFuncCall(SwiftSqlParser.FuncCallContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitFuncCall(SwiftSqlParser.FuncCallContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterLiteral(SwiftSqlParser.LiteralContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitLiteral(SwiftSqlParser.LiteralContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterUnaryBoolOp(SwiftSqlParser.UnaryBoolOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitUnaryBoolOp(SwiftSqlParser.UnaryBoolOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterBinaryOp(SwiftSqlParser.BinaryOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitBinaryOp(SwiftSqlParser.BinaryOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterBinaryBoolOp(SwiftSqlParser.BinaryBoolOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitBinaryBoolOp(SwiftSqlParser.BinaryBoolOpContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterName(SwiftSqlParser.NameContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitName(SwiftSqlParser.NameContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void enterEveryRule(ParserRuleContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void exitEveryRule(ParserRuleContext ctx) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void visitTerminal(TerminalNode node) {
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override
+    public void visitErrorNode(ErrorNode node) {
+    }
+}
