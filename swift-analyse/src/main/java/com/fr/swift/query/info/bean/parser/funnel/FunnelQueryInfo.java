eaae85fe492e31466139488bf134bc980282bcfd
anchore <anchore@fanruan.com>
2019-06-21 11:31:02 +0800

    Refactor：QueryInfo泛型去除

--- swift-analyse/src/main/java/com/fr/swift/query/info/bean/parser/funnel/FunnelQueryInfo.java
+++ swift-analyse/src/main/java/com/fr/swift/query/info/bean/parser/funnel/FunnelQueryInfo.java
@@ -2,7 +2,6 @@ package com.fr.swift.query.info.bean.parser.funnel;
 
 import com.fr.swift.query.info.bean.query.FunnelQueryBean;
 import com.fr.swift.query.query.QueryInfo;
-import com.fr.swift.result.SwiftResultSet;
 
 /**
  * This class created on 2018/12/13
@@ -10,6 +9,6 @@ import com.fr.swift.result.SwiftResultSet;
  * @author Lucifer
  * @description
  */
-public interface FunnelQueryInfo<T extends SwiftResultSet> extends QueryInfo<T> {
+public interface FunnelQueryInfo extends QueryInfo {
     FunnelQueryBean getQueryBean();
 }
