b278da2ce593e65ae356dfa7ee41bf65b346baf1
anchore <anchore@fanruan.com>
2019-06-21 11:29:25 +0800

    Refactor：QueryResultSet泛型纠正

--- swift-analyse/src/test/java/com/fr/swift/query/result/serialize/NodeSerializableQRSTest.java
+++ swift-analyse/src/test/java/com/fr/swift/query/result/serialize/NodeSerializableQRSTest.java
@@ -2,19 +2,18 @@ package com.fr.swift.query.result.serialize;
 
 import com.fr.swift.base.meta.SwiftMetaDataBean;
 import com.fr.swift.query.aggregator.Aggregator;
+import com.fr.swift.query.group.by2.node.GroupPage;
 import com.fr.swift.query.sort.SortType;
 import com.fr.swift.result.GroupNode;
-import com.fr.swift.result.SwiftNode;
-import com.fr.swift.result.node.resultset.INodeQueryResultSetMerger;
 import com.fr.swift.result.node.resultset.NodeQueryResultSetMerger;
+import com.fr.swift.result.qrs.QueryResultSet;
+import com.fr.swift.result.qrs.QueryResultSetMerger;
 import com.fr.swift.source.ColumnTypeConstants;
 import com.fr.swift.structure.Pair;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -31,16 +30,16 @@ public class NodeSerializableQRSTest {
     private NodeSerializableQRS qrs;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         qrs = new NodeSerializableQRS(fetchSize, new NodeQueryResultSetMerger(fetchSize, new boolean[0],
                 new ArrayList<Aggregator>(), new ArrayList<Pair<SortType, ColumnTypeConstants.ClassType>>()),
-                Pair.<SwiftNode, List<Map<Integer, Object>>>of(null, null), false);
+                new GroupPage(null, null), false);
     }
 
     @Test
     public void setInvoker() {
-        Pair<SwiftNode, List<Map<Integer, Object>>> page = Pair.<SwiftNode, List<Map<Integer, Object>>>of(new GroupNode(), null);
-        INodeQueryResultSetMerger merger = new NodeQueryResultSetMerger(fetchSize, new boolean[0],
+        GroupPage page = new GroupPage(new GroupNode(), null);
+        QueryResultSetMerger<QueryResultSet<GroupPage>> merger = new NodeQueryResultSetMerger(fetchSize, new boolean[0],
                 new ArrayList<Aggregator>(), new ArrayList<Pair<SortType, ColumnTypeConstants.ClassType>>());
         final NodeSerializableQRS next = new NodeSerializableQRS(fetchSize, merger, page, false);
         qrs = new NodeSerializableQRS(fetchSize, merger, page, true);
