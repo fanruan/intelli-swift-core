a4eba51a2534fabf27d2f7da8df908b063309694
anchore <anchore@fanruan.com>
2019-06-25 16:41:21 +0800

    Refactor：DetailQuery结构调整简化

---
+++ swift-analyse/src/main/java/com/fr/swift/result/MergeDetailQueryResultSet.java
@@ -0,0 +1,159 @@
+package com.fr.swift.result;
+
+import com.fr.swift.result.qrs.QueryResultSetMerger;
+import com.fr.swift.source.Row;
+import com.fr.swift.source.SwiftMetaData;
+import com.fr.swift.util.IoUtil;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by Xiaolei.Liu on 2018/1/23
+ *
+ * @author yee
+ */
+
+public class MergeDetailQueryResultSet extends BaseDetailQueryResultSet {
+
+    private int rowCount;
+    private List<DetailQueryResultSet> resultSets;
+    /**
+     * mergeIterator和rowIterator看似相同，其实不然，前者可以理解为内部实现(处理翻页缓存等)，后者为外部实现(对应SwiftResult)
+     */
+    private Iterator<Row> mergeIterator;
+
+    public MergeDetailQueryResultSet(int fetchSize, List<DetailQueryResultSet> queryResultSets) {
+        super(fetchSize);
+        int rowCount = 0;
+        for (DetailQueryResultSet queryResultSet : queryResultSets) {
+            rowCount += queryResultSet.getRowCount();
+        }
+        resultSets = queryResultSets;
+        this.rowCount = rowCount;
+        this.mergeIterator = new DetailRowIterator();
+    }
+
+    @Override
+    public List<Row> getPage() {
+        List<Row> rows = new ArrayList<Row>();
+        int count = fetchSize;
+        while (mergeIterator.hasNext() && count-- > 0) {
+            rows.add(mergeIterator.next());
+        }
+        return rows;
+    }
+
+    @Override
+    public boolean hasNextPage() {
+        return mergeIterator.hasNext();
+    }
+
+    @Override
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    @Override
+    public QueryResultSetMerger<DetailQueryResultSet> getMerger() {
+        return new DetailQueryResultSetMerger(fetchSize);
+    }
+
+    @Override
+    public SwiftResultSet convert(SwiftMetaData metaData) {
+        return new DetailResultSetImpl(this, metaData);
+    }
+
+    @Override
+    public void close() {
+        IoUtil.close(resultSets);
+    }
+
+    class DetailRowIterator implements Iterator<Row> {
+
+        private int index = 0;
+        private Iterator<Row> iterator = new ArrayList<Row>().iterator();
+
+        @Override
+        public boolean hasNext() {
+            if (iterator.hasNext()) {
+                return true;
+            }
+            while (index < resultSets.size()) {
+                DetailQueryResultSet resultSet = resultSets.get(index);
+                if (resultSet.hasNextPage()) {
+                    List<Row> page = resultSet.getPage();
+                    if (page != null && !page.isEmpty()) {
+                        iterator = page.iterator();
+                        break;
+                    }
+                }
+                index++;
+            }
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Row next() {
+            return iterator.next();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    static class DetailResultSetImpl implements DetailResultSet {
+        DetailQueryResultSet detailQueryResultSet;
+        SwiftMetaData metaData;
+        Iterator<Row> iterator;
+
+        DetailResultSetImpl(DetailQueryResultSet detailQueryResultSet, SwiftMetaData metaData) {
+            this.detailQueryResultSet = detailQueryResultSet;
+            this.metaData = metaData;
+            iterator = new SwiftRowIteratorImpl<DetailQueryResultSet>(detailQueryResultSet);
+        }
+
+        @Override
+        public List<Row> getPage() {
+            return null;
+        }
+
+        @Override
+        public boolean hasNextPage() {
+            return false;
+        }
+
+        @Override
+        public int getRowCount() {
+            return detailQueryResultSet.getRowCount();
+        }
+
+        @Override
+        public int getFetchSize() {
+            return detailQueryResultSet.getFetchSize();
+        }
+
+        @Override
+        public SwiftMetaData getMetaData() {
+            return metaData;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        @Override
+        public Row getNextRow() {
+            return iterator.next();
+        }
+
+        @Override
+        public void close() {
+            IoUtil.close(detailQueryResultSet);
+        }
+    }
+}
