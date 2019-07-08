4875c98ead2080691288b979d210b8f825922d07
Lifan.li <Lifan.li@fanruan.com>
2019-07-08 10:59:51 +0800

    KERNEL-792 学习单元测试，写detail query的单元测试 1、完善DetailQueryBuilder和QueryBuilder的test

--- swift-analyse/src/test/java/com/fr/swift/query/builder/DetailQueryBuilderTest.java
+++ swift-analyse/src/test/java/com/fr/swift/query/builder/DetailQueryBuilderTest.java
@@ -1,36 +1,26 @@
 package com.fr.swift.query.builder;
 
 import com.fr.swift.SwiftContext;
+import com.fr.swift.beans.factory.BeanFactory;
 import com.fr.swift.db.impl.SwiftDatabase;
 import com.fr.swift.query.filter.FilterBuilder;
-import com.fr.swift.query.filter.SwiftDetailFilterType;
 import com.fr.swift.query.filter.detail.DetailFilter;
 import com.fr.swift.query.filter.info.FilterInfo;
 import com.fr.swift.query.filter.info.GeneralFilterInfo;
-import com.fr.swift.query.filter.info.SwiftDetailFilterInfo;
 import com.fr.swift.query.info.bean.parser.QueryInfoParser;
 import com.fr.swift.query.info.bean.query.DetailQueryInfoBean;
 import com.fr.swift.query.info.detail.DetailQueryInfo;
 import com.fr.swift.query.query.Query;
 import com.fr.swift.query.result.detail.DetailResultQuery;
-import com.fr.swift.query.result.detail.SortedDetailResultQuery;
 import com.fr.swift.query.segment.detail.DetailSegmentQuery;
-import com.fr.swift.query.segment.detail.SortedDetailSegmentQuery;
-import com.fr.swift.query.sort.Sort;
 import com.fr.swift.result.DetailQueryResultSet;
 import com.fr.swift.segment.Segment;
 import com.fr.swift.segment.SwiftSegmentManager;
-import com.fr.swift.segment.column.ColumnKey;
-import com.fr.swift.source.ColumnTypeConstants;
 import com.fr.swift.source.ColumnTypeUtils;
 import com.fr.swift.source.SourceKey;
-import com.fr.swift.source.SwiftMetaDataColumn;
-import com.fr.swift.structure.Pair;
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -39,155 +29,149 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.powermock.api.mockito.PowerMockito.doReturn;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.spy;
 import static org.powermock.api.mockito.PowerMockito.when;
 import static org.powermock.api.mockito.PowerMockito.whenNew;
+
 
 /**
  * Create by lifan on 2019-07-03 11:51
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({SwiftContext.class, QueryInfoParser.class, FilterBuilder.class, SwiftDatabase.class, DetailQueryBuilder.class, ColumnTypeUtils.class})
+@PrepareForTest({SwiftContext.class, QueryInfoParser.class, FilterBuilder.class, SwiftDatabase.class, DetailQueryBuilder.class, ColumnTypeUtils.class, BaseQueryBuilder.class})
 public class DetailQueryBuilderTest {
 
-    @Test
-    public void buildQuery() throws Exception {
+    private static final SwiftSegmentManager swiftSegmentManager = mock(SwiftSegmentManager.class);
+
+    @Before
+    public void setUp() {
         //mock SwiftContext
         mockStatic(SwiftContext.class);
-        SwiftContext swiftContext = mock(SwiftContext.class);
-        when(SwiftContext.get()).thenReturn(swiftContext);
+        BeanFactory beanFactory = mock(BeanFactory.class);
+        when(SwiftContext.get()).thenReturn(beanFactory);
 
         //mock SwiftSegmentManager
-        SwiftSegmentManager localSegmentProvider = mock(SwiftSegmentManager.class);
-        when(swiftContext.getBean("localSegmentProvider", SwiftSegmentManager.class)).thenReturn(localSegmentProvider);
+        when(beanFactory.getBean("localSegmentProvider", SwiftSegmentManager.class)).thenReturn(swiftSegmentManager);
+    }
 
-        //
-        DetailQueryInfoBean bean = mock(DetailQueryInfoBean.class);
-        DetailQueryInfo info = mock(DetailQueryInfo.class);
-        //mock info.getTable  info.getQuerySegment
-        SourceKey table = mock(SourceKey.class);
-        when(info.getTable()).thenReturn(table);
-        Set queryTarget = PowerMockito.mock(Set.class);
-        when(info.getQuerySegment()).thenReturn(queryTarget);
-
-        //mock DetailQueryInfo
+    @Test
+    public void of() throws Exception {
+        DetailQueryInfoBean detailQueryInfoBean = mock(DetailQueryInfoBean.class);
         mockStatic(QueryInfoParser.class);
-        when(QueryInfoParser.parse(bean)).thenReturn(info);
+        DetailQueryInfo detailQueryInfo = mock(DetailQueryInfo.class);
+        when(QueryInfoParser.parse(detailQueryInfoBean)).thenReturn(detailQueryInfo);
 
-        //init queries
-        List<Query<DetailQueryResultSet>> queries = new ArrayList();
+        //info.hashSort()==true   new DetailQueryBuilder(queryInfo)
+        when(detailQueryInfo.hasSort()).thenReturn(true);
+        DetailQueryBuilder detailQueryBuilder = mock(DetailQueryBuilder.class);
+        whenNew(DetailQueryBuilder.class).withArguments(detailQueryInfo).thenReturn(detailQueryBuilder);
+        assertThat(DetailQueryBuilder.of(detailQueryInfoBean)).isEqualTo(detailQueryBuilder);
 
-        //mock Segment List<Segments>
-        Segment segment = PowerMockito.mock(Segment.class);
+        //info.hasSort()==false   new SortedDetailQueryBuilder(queryInfo)
+        when(detailQueryInfo.hasSort()).thenReturn(false);
+        SortedDetailQueryBuilder sortedDetailQueryBuilder = mock(SortedDetailQueryBuilder.class);
+        whenNew(SortedDetailQueryBuilder.class).withArguments(detailQueryInfo).thenReturn(sortedDetailQueryBuilder);
+        assertThat(DetailQueryBuilder.of(detailQueryInfoBean)).isEqualTo(sortedDetailQueryBuilder);
+    }
+
+    @Test
+    public void buildQuery() {
+        DetailQueryInfo detailQueryInfo = mock(DetailQueryInfo.class);
+        List<Query<DetailQueryResultSet>> queries = new ArrayList<Query<DetailQueryResultSet>>();
+        Segment segment = mock(Segment.class);
         List<Segment> segments = Arrays.asList(segment);
-        when(localSegmentProvider.getSegmentsByIds(info.getTable(), info.getQuerySegment())).thenReturn(segments);
 
-        //mock info.getDimensions()
-        List dimensions = PowerMockito.mock(List.class);
-        when(info.getDimensions()).thenReturn(dimensions);
+        //mock SourceKey
+        SourceKey table = mock(SourceKey.class);
+        when(detailQueryInfo.getTable()).thenReturn(table);
 
-        // for
+        //mock QuerySegment
+        Set queryTarget = mock(Set.class);
+        when(detailQueryInfo.getQuerySegment()).thenReturn(queryTarget);
+
+        SwiftSegmentManager localSegmentProvider = SwiftContext.get().getBean("localSegmentProvider", SwiftSegmentManager.class);
+        when(localSegmentProvider.getSegmentsByIds(table, queryTarget)).thenReturn(segments);
+
+        //mock List<Dimentsions>
+        List dimensions = new ArrayList();
+        when(detailQueryInfo.getDimensions()).thenReturn(dimensions);
+
         //init filterInfos
-        List<FilterInfo> filterInfos = new ArrayList<FilterInfo>();
+        List filterInfos = new ArrayList();
 
         //mock columns
-        List columns = PowerMockito.mock(List.class);
+        List columns = mock(List.class);
         mockStatic(BaseQueryBuilder.class);
         when(BaseQueryBuilder.getDimensionSegments(segment, dimensions)).thenReturn(columns);
 
-        //info.getFilterInfo!=null  info.hasSort
-        FilterInfo filterInfo = PowerMockito.mock(FilterInfo.class);
-        when(info.getFilterInfo()).thenReturn(filterInfo);
+        //mock FilterInfo  detailQueryInfo.getFilterInfo() != null
+        FilterInfo filterInfo = mock(FilterInfo.class);
+        when(detailQueryInfo.getFilterInfo()).thenReturn(filterInfo);
         filterInfos.add(filterInfo);
 
-        //info.hasSort
-        //init info.hasSort=true
-        when(info.hasSort()).thenReturn(true);
+        //mock fetchSize
+        int fetchSize = 1;
+        when(detailQueryInfo.getFetchSize()).thenReturn(fetchSize);
 
-        //mock SwiftDetailFilterInfo
-        SwiftDetailFilterInfo swiftDetailFilterInfo = mock(SwiftDetailFilterInfo.class);
-        whenNew(SwiftDetailFilterInfo.class).withArguments(null, null, SwiftDetailFilterType.ALL_SHOW).thenReturn(swiftDetailFilterInfo);
-        filterInfos.add(swiftDetailFilterInfo);
+        //mock query getSegmentQuery的返回值
+        Query query = mock(Query.class);
+        DetailQueryBuilder detailQueryBuilder = spy(new DetailQueryBuilder(detailQueryInfo));
+        doReturn(query).when(detailQueryBuilder).getSegmentQuery(segment, columns, filterInfos);
+        queries.add(query);
 
-        //mock sort init sorts  info.getSorts() = sorts
-        Sort sort = PowerMockito.mock(Sort.class);
-        List<Sort> sorts = Arrays.asList(sort);
-        when(info.getSorts()).thenReturn(sorts);
+        //mock query getResultQuery的返回值
+        Query queryResult = mock(Query.class);
+        doReturn(queryResult).when(detailQueryBuilder).getResultQuery(queries);
 
-        //mock info.getFetchSize()
-        int fetchSize = 2;
-        when(info.getFetchSize()).thenReturn(fetchSize);
+        assertThat(detailQueryBuilder.buildQuery()).isEqualTo(queryResult);
+    }
 
-        //mock new GeneralFilterInfo
+    @Test
+    public void getSegmentQuery() throws Exception {
+        Segment seg = mock(Segment.class);
+        List columns = mock(List.class);
+        List filterInfos = mock(List.class);
+
+        //mock detailQueryInfo.getFetchSize()
+        DetailQueryInfo detailQueryInfo = mock(DetailQueryInfo.class);
+        int fetchSize = 1;
+        when(detailQueryInfo.getFetchSize()).thenReturn(fetchSize);
+
+        //mock new GeneralFilterInfo()
         GeneralFilterInfo generalFilterInfo = mock(GeneralFilterInfo.class);
         whenNew(GeneralFilterInfo.class).withArguments(filterInfos, GeneralFilterInfo.AND).thenReturn(generalFilterInfo);
 
-        //mock detailFilter = FilterBuilder.buildDetailFilter
-        DetailFilter detailFilter = PowerMockito.mock(DetailFilter.class);
+        //mock FilterBuilder.buildDetailFilter
         mockStatic(FilterBuilder.class);
-        when(FilterBuilder.buildDetailFilter(segment, generalFilterInfo)).thenReturn(detailFilter);
+        DetailFilter detailFilter = mock(DetailFilter.class);
+        when(FilterBuilder.buildDetailFilter(seg, generalFilterInfo)).thenReturn(detailFilter);
 
-        //mock sortedDetailSegmentQuery = new SortedDetailSegmentQuery()
-        SortedDetailSegmentQuery sortedDetailSegmentQuery = PowerMockito.mock(SortedDetailSegmentQuery.class);
-        whenNew(SortedDetailSegmentQuery.class)
-                .withArguments(info.getFetchSize(), columns, FilterBuilder.buildDetailFilter(segment, generalFilterInfo), sorts)
-                .thenReturn(sortedDetailSegmentQuery);
-        queries.add(sortedDetailSegmentQuery);
-
-        //getComparators getClassType
-        //mock columnKey columnName
-        ColumnKey columnKey = PowerMockito.mock(ColumnKey.class);
-        when(sort.getColumnKey()).thenReturn(columnKey);
-        String columnName = "fange";
-        when(columnKey.getName()).thenReturn(columnName);
-
-        //getClassType
-        //mock column
-        SwiftMetaDataColumn column = PowerMockito.mock(SwiftMetaDataColumn.class);
-        mockStatic(SwiftDatabase.class);
-        SwiftDatabase swiftDatabase = mock(SwiftDatabase.class, Mockito.RETURNS_DEEP_STUBS);
-        when(SwiftDatabase.getInstance()).thenReturn(swiftDatabase);
-        when(swiftDatabase.getTable(table).getMetadata().getColumn(columnName)).thenReturn(column);
-
-        //mock classtype = ColumnTypeUtils.getClassType
-        mockStatic(ColumnTypeUtils.class);
-        ColumnTypeConstants.ClassType classType = ColumnTypeConstants.ClassType.INTEGER;
-        when(ColumnTypeUtils.getClassType(column)).thenReturn(classType);
-
-        //mock comparators
-        List<Pair<Sort, ColumnTypeConstants.ClassType>> comparators = new ArrayList<Pair<Sort, ColumnTypeConstants.ClassType>>();
-        comparators.add(Pair.of(sort, classType));
-
-        //mock new SortedDetailSegment
-        SortedDetailResultQuery sortedDetailResultQuery = mock(SortedDetailResultQuery.class);
-        whenNew(SortedDetailResultQuery.class)
-//                .withAnyArguments().thenReturn(sortedDetailResultQuery);
-                .withArguments(2, queries, comparators).thenReturn(sortedDetailResultQuery);
-        DetailQueryBuilder detailQueryBuilder = DetailQueryBuilder.get();
-        assertEquals(sortedDetailResultQuery, detailQueryBuilder.buildQuery(bean));
-
-
-        //info.hasSort() = false
-        when(info.hasSort()).thenReturn(false);
-        List<FilterInfo> filterInfos1 = new ArrayList<FilterInfo>();
-        filterInfos1.add(filterInfo);
-        GeneralFilterInfo generalFilterInfo1 = mock(GeneralFilterInfo.class);
-        whenNew(GeneralFilterInfo.class).withArguments(filterInfos1, GeneralFilterInfo.AND).thenReturn(generalFilterInfo1);
-        DetailFilter detailFilter1 = mock(DetailFilter.class);
-        when(FilterBuilder.buildDetailFilter(segment, generalFilterInfo1)).thenReturn(detailFilter1);
-
-        List<Query<DetailQueryResultSet>> queries1 = new ArrayList<Query<DetailQueryResultSet>>();
+        //mock new DetailSegmentQuery
         DetailSegmentQuery detailSegmentQuery = mock(DetailSegmentQuery.class);
-        whenNew(DetailSegmentQuery.class).withArguments(fetchSize, columns, detailFilter1).thenReturn(detailSegmentQuery);
-        queries1.add(detailSegmentQuery);
+        whenNew(DetailSegmentQuery.class).withArguments(fetchSize, columns, detailFilter).thenReturn(detailSegmentQuery);
+
+        assertThat(new DetailQueryBuilder(detailQueryInfo).getSegmentQuery(seg, columns, filterInfos)).isEqualTo(detailSegmentQuery);
+    }
+
+    @Test
+    public void getResultQuery() throws Exception {
+        List queris = mock(List.class);
+
+        //mock detailQueryInfo.getFetchSize()
+        DetailQueryInfo detailQueryInfo = mock(DetailQueryInfo.class);
+        int fetchSize = 1;
+        when(detailQueryInfo.getFetchSize()).thenReturn(fetchSize);
 
         DetailResultQuery detailResultQuery = mock(DetailResultQuery.class);
-        whenNew(DetailResultQuery.class).withArguments(fetchSize, queries1).thenReturn(detailResultQuery);
-        Assert.assertEquals(detailResultQuery, detailQueryBuilder.buildQuery(bean));
+        whenNew(DetailResultQuery.class).withArguments(fetchSize, queris).thenReturn(detailResultQuery);
 
+        assertThat(new DetailQueryBuilder(detailQueryInfo).getResultQuery(queris)).isEqualTo(detailResultQuery);
 
     }
+
 
 }
