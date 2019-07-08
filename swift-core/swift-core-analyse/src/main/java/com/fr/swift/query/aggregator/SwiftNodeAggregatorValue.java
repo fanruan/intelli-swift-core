6feb7ee6a0d36510f78f4fbad4642a9d7dd07e28
Yee <yee@fanruan.com>
2019-07-08 17:24:44 +0800

    调整

--- swift-core/swift-core-analyse/src/main/java/com/fr/swift/query/aggregator/SwiftNodeAggregatorValue.java
+++ swift-core/swift-core-analyse/src/main/java/com/fr/swift/query/aggregator/SwiftNodeAggregatorValue.java
@@ -6,7 +6,7 @@ import com.fr.swift.result.SwiftNode;
  * @author yee
  * @date 2019-07-07
  */
-public interface SwiftNodeNodeAggregatorValue extends AggregatorValue<SwiftNode> {
+public interface SwiftNodeAggregatorValue extends AggregatorValue<SwiftNode> {
     SwiftNode calculateValue(int depth);
 
     SwiftNode getLeafNode();
