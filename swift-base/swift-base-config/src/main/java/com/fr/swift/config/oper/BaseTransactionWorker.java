d43237d0dfb1bb5faa762a8232d4907206c93e0a
Yee <yee@fanruan.com>
2019-08-05 15:24:45 +0800

    KERNEL-1020 配置新接口设计 不需要TransactionManager了，已经封装到CommandBus里了

--- swift-base/swift-base-config/src/main/java/com/fr/swift/config/oper/BaseTransactionWorker.java
+++ swift-base/swift-base-config/src/main/java/com/fr/swift/config/oper/BaseTransactionWorker.java
@@ -1,9 +1,11 @@
 package com.fr.swift.config.oper;
 
 /**
+ * @deprecated 弃用 去掉service后删
  * @author yee
  * @date 2018-11-27
  */
+@Deprecated
 public abstract class BaseTransactionWorker<T> implements ConfigSessionCreator.TransactionWorker<T> {
     private boolean needTx = true;
 
--- swift-base/swift-base-config/src/main/java/com/fr/swift/config/oper/ConfigSessionCreator.java
+++ swift-base/swift-base-config/src/main/java/com/fr/swift/config/oper/ConfigSessionCreator.java
@@ -7,10 +7,28 @@ import java.sql.SQLException;
  * @date 2018-11-27
  */
 public interface ConfigSessionCreator {
-    <T> T doTransactionIfNeed(TransactionWorker<T> worker) throws SQLException;
-
+    /**
+     * 创建session
+     *
+     * @return
+     * @throws ClassNotFoundException
+     */
     ConfigSession createSession() throws ClassNotFoundException;
 
+    /**
+     * @param worker
+     * @param <T>
+     * @return
+     * @throws SQLException
+     * @deprecated 弃用，去掉service后删
+     */
+    @Deprecated
+    <T> T doTransactionIfNeed(TransactionWorker<T> worker) throws SQLException;
+
+    /**
+     * @deprecated 弃用  去掉service后删除
+     * @param <T>
+     */
     interface TransactionWorker<T> {
         boolean needTransaction();
 
