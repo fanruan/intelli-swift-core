98fd55ed1cd724764e5b8040ce5ce78cb1f9c267
Yee <yee@fanruan.com>
2019-08-09 12:55:25 +0800

    KERNEL-1039 Swift新配置替换旧配置 configEntity等 revert

---
+++ swift-base/swift-base-config/src/main/java/com/fr/swift/config/service/DataSyncRuleService.java
@@ -0,0 +1,11 @@
+package com.fr.swift.config.service;
+
+
+import com.fr.swift.config.DataSyncRule;
+
+/**
+ * @author yee
+ * @date 2018/7/16
+ */
+public interface DataSyncRuleService extends IRuleService<DataSyncRule> {
+}
