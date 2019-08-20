5c60fb156104f70bb656685525ba06a81dfb5e95
Yee <yee@fanruan.com>
2019-08-20 15:19:41 +0800

    DEC-9339 【jdbc】【swift】sql数据集，模糊查询无法查出符合条件的所有数据 BugFix 数值字段模糊查询有问题，解析bugfix

--- swift-core/swift-core-analyse/src/main/java/com/fr/swift/query/filter/detail/impl/number/NumberStartsWithFilter.java
+++ swift-core/swift-core-analyse/src/main/java/com/fr/swift/query/filter/detail/impl/number/NumberStartsWithFilter.java
@@ -1,7 +1,6 @@
-package com.fr.swift.query.filter.detail.impl.string;
+package com.fr.swift.query.filter.detail.impl.number;
 
 import com.fr.swift.query.filter.detail.impl.AbstractDetailFilter;
-import com.fr.swift.query.filter.detail.impl.util.LookupFactory;
 import com.fr.swift.query.filter.match.MatchConverter;
 import com.fr.swift.result.SwiftNode;
 import com.fr.swift.segment.column.Column;
@@ -10,32 +9,32 @@ import com.fr.swift.structure.array.IntList;
 import com.fr.swift.structure.array.IntListFactory;
 import com.fr.swift.structure.iterator.IntListRowTraversal;
 import com.fr.swift.structure.iterator.RowTraversal;
-import com.fr.swift.util.ArrayLookupHelper;
 import com.fr.swift.util.Strings;
 import com.fr.swift.util.Util;
 
 /**
- * Created by Lyon on 2017/11/27.
+ * @author yee
+ * @date 2019-08-20
  */
-public class StringStartsWithFilter extends AbstractDetailFilter<String> {
+public class NumberStartsWithFilter extends AbstractDetailFilter {
     private String startsWith;
 
-    public StringStartsWithFilter(String startsWith, Column<String> column) {
+    public NumberStartsWithFilter(String startsWith, Column column) {
         Util.requireNonNull(Strings.isEmpty(startsWith) ? null : startsWith);
         this.startsWith = startsWith;
         this.column = column;
     }
 
     @Override
-    protected RowTraversal getIntIterator(final DictionaryEncodedColumn<String> dict) {
-        ArrayLookupHelper.Lookup<String> lookup = LookupFactory.create(dict);
-        int start = ArrayLookupHelper.getStartIndex4StartWith(lookup, startsWith);
-        int end = ArrayLookupHelper.getEndIndex4StartWith(lookup, startsWith);
-        start = start < 0 ? 0 : start;
-        if (start >= dict.size() || end < 0) {
-            return new IntListRowTraversal(IntListFactory.createEmptyIntList());
+    protected RowTraversal getIntIterator(DictionaryEncodedColumn dict) {
+        IntList intList = IntListFactory.createIntList();
+        for (int i = 0, size = dict.size(); i < size; i++) {
+            final Object value = dict.getValue(i);
+            String data = null == value ? null : value.toString();
+            if (data != null && data.startsWith(startsWith)) {
+                intList.add(i);
+            }
         }
-        IntList intList = IntListFactory.createRangeIntList(start, end);
         return new IntListRowTraversal(intList);
     }
 
