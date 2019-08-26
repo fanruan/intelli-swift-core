5dca6e6891fe0067bd993ee42189d492e80f491d
Marvin.Zhao <marvin.zhao@fanruan.com>
2019-08-26 10:32:37 +0800

    KERNEL-1100 下载seg失败的处理策略 1、将异常处理流程接入SwiftUploadService 2、编写上传失败的异常处理handler和对应test

---
+++ swift-exception-repair/src/main/java/com/fr/swift/exception/UploadExceptionContext.java
@@ -0,0 +1,27 @@
+package com.fr.swift.exception;
+
+import com.fr.swift.segment.SegmentKey;
+
+/**
+ * @author Marvin
+ * @date 8/22/2019
+ * @description
+ * @since swift 1.1
+ */
+public class UploadExceptionContext implements ExceptionContext {
+    SegmentKey context;
+    boolean isAllShow;
+
+    public UploadExceptionContext(SegmentKey context, boolean isAllShow) {
+        this.context = context;
+        this.isAllShow = isAllShow;
+    }
+
+    public SegmentKey getSegmentKey() {
+        return context;
+    }
+
+    public boolean isAllShow() {
+        return isAllShow;
+    }
+}
---
+++ swift-history/src/main/java/com/fr/swift/exception/handler/UploadSegExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.fr.swift.exception.handler;
+
+import com.fr.swift.SwiftContext;
+import com.fr.swift.beans.annotation.SwiftBean;
+import com.fr.swift.exception.ExceptionInfo;
+import com.fr.swift.exception.ExceptionInfoType;
+import com.fr.swift.exception.UploadExceptionContext;
+import com.fr.swift.segment.SegmentKey;
+import com.fr.swift.service.SwiftUploadService;
+
+import java.util.Collections;
+
+/**
+ * @author Marvin
+ * @date 8/22/2019
+ * @description
+ * @since swift 1.1
+ */
+@SwiftBean
+@RegisterExceptionHandler
+public class UploadSegExceptionHandler implements ExceptionHandler {
+
+    private SwiftUploadService uploadService = SwiftContext.get().getBean(SwiftUploadService.class);
+
+    @Override
+    public boolean handleException(ExceptionInfo info) {
+        if (SwiftRepositoryAccessibleTester.testAccessible()) {
+            SegmentKey key = ((UploadExceptionContext) info.getContext()).getSegmentKey();
+            if (!((UploadExceptionContext) info.getContext()).isAllShow()) {
+                uploadService.upload(Collections.singleton(key));
+                return true;
+            } else {
+                uploadService.uploadAllShow(Collections.singleton(key));
+                return true;
+            }
+        }
+        //需要上传的seg只有本节点有，通知master也无法处理。因此考虑直接过掉异常，手动去数据库里查找FAILED的task重新上传
+        return true;
+    }
+
+
+    @Override
+    public ExceptionInfo.Type getExceptionInfoType() {
+        return ExceptionInfoType.UPLOAD_SEGMENT;
+    }
+
+}
