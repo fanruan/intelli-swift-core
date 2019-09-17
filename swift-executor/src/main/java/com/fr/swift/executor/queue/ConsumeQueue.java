94278f2902be95e38a8f97e85257877c954fb2b6
Bellman <Bellman@fanruan.com>
2019-09-17 09:55:26 +0800

    REPORT-21866 队列修改：给 task 增加优先级属性，把消费队列改成优先队列，具体任务的优先级需要进一步设定

--- swift-executor/src/main/java/com/fr/swift/executor/queue/ConsumeQueue.java
+++ swift-executor/src/main/java/com/fr/swift/executor/queue/ConsumeQueue.java
@@ -3,9 +3,9 @@ package com.fr.swift.executor.queue;
 import com.fr.swift.executor.task.ExecutorTask;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
-import java.util.concurrent.BlockingDeque;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
 
 /**
  * This class created on 2019/2/21
@@ -15,15 +15,24 @@ import java.util.concurrent.LinkedBlockingDeque;
  */
 public class ConsumeQueue {
 
+    private static final int DEFAULT_INITIAL_CAPACITY = 100;
     private static ConsumeQueue INSTANCE = new ConsumeQueue();
 
     public static ConsumeQueue getInstance() {
         return INSTANCE;
     }
 
-    private BlockingDeque<ExecutorTask> queue = new LinkedBlockingDeque<ExecutorTask>();
+    /**
+     * 使用 PriorityBlockingQueue 实现拥有更高优先级(数字更大)的任务优先 take
+     */
+    private PriorityBlockingQueue<ExecutorTask> queue = new PriorityBlockingQueue<>(DEFAULT_INITIAL_CAPACITY, new Comparator<ExecutorTask>() {
+        @Override
+        public int compare(ExecutorTask o1, ExecutorTask o2) {
+            return Integer.compare(o1.getPriority(), o2.getPriority());
+        }
+    });
 
-    private List<ExecutorTask> taskList = new ArrayList<ExecutorTask>();
+    private List<ExecutorTask> taskList = new ArrayList<>();
 
     private ConsumeQueue() {
     }
