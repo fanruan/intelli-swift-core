6d308c155cf26e389b7d6e7684e045ef6d892ac6
HengJiang <Heng.Jiang@fanruan.com>
2019-10-14 15:22:34 +0800

    更改修改位置为ContexProvider

--- swift-config/src/main/java/com/fr/swift/config/service/impl/SwiftCubePathServiceImpl.java
+++ swift-config/src/main/java/com/fr/swift/config/service/impl/SwiftCubePathServiceImpl.java
@@ -9,6 +9,7 @@ import com.fr.swift.config.entity.SwiftConfigEntity;
 import com.fr.swift.config.oper.ConfigSession;
 import com.fr.swift.config.service.SwiftConfigService;
 import com.fr.swift.config.service.SwiftCubePathService;
+import com.fr.swift.context.ContextProvider;
 import com.fr.swift.property.SwiftProperty;
 import com.fr.swift.util.Strings;
 
@@ -34,7 +35,7 @@ public class SwiftCubePathServiceImpl implements SwiftCubePathService {
                 }
             } catch (Exception ignore) {
             }
-            String path = SwiftProperty.getProperty().getCubesPath();
+            String path = SwiftContext.get().getBean(ContextProvider.class).getContextPath();
             for (SwiftConfigEntity swiftConfigEntity : toEntity(path)) {
                 dao.saveOrUpdate(session, swiftConfigEntity);
             }
