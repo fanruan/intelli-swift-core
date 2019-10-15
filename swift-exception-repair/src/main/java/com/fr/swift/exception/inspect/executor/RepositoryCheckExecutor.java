f028163d78ccf654dbbe8c166fc233e865e33543
Marvin.Zhao <marvin.zhao@fanruan.com>
2019-10-15 15:41:24 +0800

    DEC-10294 共享存储FTP不可用时产生的日志，恢复可用后没有重新上传 1、创建一个健康检查线程，一个重试任务队列和重试任务消费者；当检查到共享存储可用时从重试队列中取出重试任务执行重试

---
+++ swift-exception-repair/src/main/java/com/fr/swift/exception/inspect/executor/RepositoryCheckExecutor.java
@@ -0,0 +1,44 @@
+package com.fr.swift.exception.inspect.executor;
+
+import com.fr.swift.exception.inspect.ComponentHealthCheck;
+import com.fr.swift.exception.inspect.SwiftRepositoryHealthInspector;
+import com.fr.swift.exception.inspect.executor.retry.RetryTaskConsumer;
+import com.fr.swift.log.SwiftLoggers;
+import com.fr.swift.util.concurrent.PoolThreadFactory;
+import com.fr.swift.util.concurrent.SwiftExecutors;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Marvin
+ * @version 1.1
+ * Created by Marvin on 10/14/2019
+ */
+public class RepositoryCheckExecutor implements Runnable {
+
+    ExecutorService taskConsumerService = SwiftExecutors.newSingleThreadExecutor(new PoolThreadFactory(RetryTaskConsumer.class));
+    long timeOut = 8L;
+    private ComponentHealthCheck repositoryChecker = new ComponentHealthCheck(SwiftRepositoryHealthInspector.getInstance(), 30000);
+
+    @Override
+    public void run() {
+        if (repositoryChecker.isHealthy()) {
+            Future<Boolean> future = taskConsumerService.submit(new RetryTaskConsumer());
+            try {
+                //重试任务时间过长可能是线程被阻塞，取消阻塞线程
+                future.get(timeOut, TimeUnit.MINUTES);
+            } catch (InterruptedException e) {
+                SwiftLoggers.getLogger().error(e);
+            } catch (ExecutionException e) {
+                SwiftLoggers.getLogger().error(e);
+            } catch (TimeoutException e) {
+                future.cancel(true);
+                SwiftLoggers.getLogger().info("Retry Task Timeout, Cancel Task !");
+            }
+        }
+    }
+}
