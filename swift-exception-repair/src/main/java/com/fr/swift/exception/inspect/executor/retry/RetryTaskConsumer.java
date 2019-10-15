e61872693b2e0ecf9d670a7a4e1ebb1a069bc3e1
Marvin.Zhao <marvin.zhao@fanruan.com>
2019-10-15 15:41:24 +0800

    DEC-10294 共享存储FTP不可用时产生的日志，恢复可用后没有重新上传 1、创建一个健康检查线程，一个重试任务队列和重试任务消费者；当检查到共享存储可用时从重试队列中取出重试任务执行重试

---
+++ swift-exception-repair/src/main/java/com/fr/swift/exception/inspect/executor/retry/RetryTaskConsumer.java
@@ -0,0 +1,32 @@
+package com.fr.swift.exception.inspect.executor.retry;
+
+import com.fr.swift.log.SwiftLoggers;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Marvin
+ * @version 1.1
+ * Created by Marvin on 10/15/2019
+ */
+public class RetryTaskConsumer implements Callable<Boolean> {
+
+    private RetryTask task;
+
+    @Override
+    public Boolean call() throws Exception {
+        //最多执行100个重试任务，防止一次调度重试太多任务影响到下次调度
+        //多余的任务和在RetryTaskConsumer运行期间加入重试队列的任务放到下一次调度中执行
+        int taskCount = Math.min(RepositoryRetryTaskQueue.getInstance().getTaskCount(), 100);
+        for (int i = 0; i < taskCount; i++) {
+            try {
+                task = RepositoryRetryTaskQueue.getInstance().take();
+                task.retry();
+            } catch (InterruptedException e) {
+                SwiftLoggers.getLogger().error(e);
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ swift-history/src/main/java/com/fr/swift/exception/UploadSegRetryTask.java
@@ -0,0 +1,34 @@
+package com.fr.swift.exception;
+
+import com.fr.swift.beans.annotation.SwiftAutoWired;
+import com.fr.swift.exception.inspect.executor.retry.RetryTask;
+import com.fr.swift.segment.SegmentKey;
+import com.fr.swift.service.SwiftUploadService;
+
+import java.util.Collections;
+
+/**
+ * @author Marvin
+ * @version 1.1
+ * Created by Marvin on 10/14/2019
+ */
+public class UploadSegRetryTask implements RetryTask {
+
+    ExceptionInfo info;
+    @SwiftAutoWired
+    private SwiftUploadService uploadService;
+
+    public UploadSegRetryTask(ExceptionInfo info) {
+        this.info = info;
+    }
+
+    @Override
+    public void retry() {
+        SegmentKey key = ((UploadExceptionContext) info.getContext()).getSegmentKey();
+        if (!((UploadExceptionContext) info.getContext()).isAllShow()) {
+            uploadService.upload(Collections.singleton(key));
+        } else {
+            uploadService.uploadAllShow(Collections.singleton(key));
+        }
+    }
+}
