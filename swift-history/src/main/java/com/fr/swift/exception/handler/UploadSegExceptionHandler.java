7449a6e1b7e22781c6dc8142bbfb3a0ed2b0d425
Marvin.Zhao <marvin.zhao@fanruan.com>
2019-10-15 15:39:59 +0800

    DEC-10294 共享存储FTP不可用时产生的日志，恢复可用后没有重新上传 1、创建一个健康检查线程，一个重试任务队列和重试任务消费者；当检查到共享存储可用时从重试队列中取出重试任务执行重试

--- swift-history/src/main/java/com/fr/swift/exception/handler/UploadSegExceptionHandler.java
+++ swift-history/src/main/java/com/fr/swift/exception/handler/UploadSegExceptionHandler.java
@@ -1,16 +1,11 @@
 package com.fr.swift.exception.handler;
 
-import com.fr.swift.beans.annotation.SwiftAutoWired;
 import com.fr.swift.beans.annotation.SwiftBean;
 import com.fr.swift.exception.ExceptionInfo;
 import com.fr.swift.exception.ExceptionInfoType;
-import com.fr.swift.exception.RetryUploadSegScheduledWorker;
-import com.fr.swift.exception.inspect.ComponentHealthCheck;
-import com.fr.swift.exception.inspect.SwiftRepositoryHealthInspector;
-import com.fr.swift.service.SwiftUploadService;
-import com.fr.swift.util.concurrent.SwiftExecutors;
-
-import java.util.concurrent.ExecutorService;
+import com.fr.swift.exception.UploadSegRetryTask;
+import com.fr.swift.exception.inspect.executor.retry.RepositoryRetryTaskQueue;
+import com.fr.swift.exception.inspect.executor.retry.RetryTask;
 
 /**
  * @author Marvin
@@ -22,23 +17,10 @@ import java.util.concurrent.ExecutorService;
 @RegisterExceptionHandler
 public class UploadSegExceptionHandler implements ExceptionHandler {
 
-    @SwiftAutoWired
-    private SwiftUploadService uploadService;
-
-    private ComponentHealthCheck repositoryChecker = new ComponentHealthCheck(SwiftRepositoryHealthInspector.getInstance(), 30000);
-
-    private RetryUploadSegScheduledWorker executor = new RetryUploadSegScheduledWorker();
-
     @Override
     public boolean handleException(final ExceptionInfo info) {
-        ExecutorService service= SwiftExecutors.newSingleThreadExecutor();
-        service.execute(new Runnable() {
-            @Override
-            public void run() {
-                executor.work(info);
-            }
-        });
-        service.shutdown();
+        RetryTask task = new UploadSegRetryTask(info);
+        RepositoryRetryTaskQueue.getInstance().offer(task);
 
         //需要上传的seg只有本节点有，通知master也无法处理。因此考虑直接过掉异常，手动去数据库里查找FAILED的task重新上传
         return true;
