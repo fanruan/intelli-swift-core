87ab898f4bf01630404f8f6870b2b70bd86ca683
Marvin.Zhao <Marvin.Zhao@fanruan.com>
2019-10-16 18:17:55 +0800

    KERNEL-1557 swift模块拆分-marvin 1、swift-base-io拆到swift-io、api 2、swift-base-repository拆到swift-repo、api 3、swift-base-common/com.fr.swift.basic拆到swift-rpc-api 4、swift-base-common/com.fr.swift.cluste拆到rswift-cluster-api

--- swift-history/src/main/java/com/fr/swift/exception/handler/UploadSegExceptionHandler.java
+++ swift-history/src/main/java/com/fr/swift/exception/handler/UploadSegExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.fr.swift.exception.handler;
 
+import com.fr.swift.beans.annotation.SwiftAutoWired;
 import com.fr.swift.beans.annotation.SwiftBean;
 import com.fr.swift.exception.ExceptionInfo;
 import com.fr.swift.exception.ExceptionInfoType;
-import com.fr.swift.exception.UploadSegRetryTask;
-import com.fr.swift.exception.inspect.executor.retry.RepositoryRetryTaskQueue;
-import com.fr.swift.exception.inspect.executor.retry.RetryTask;
+import com.fr.swift.exception.UploadExceptionContext;
+import com.fr.swift.exception.inspect.ComponentHealthCheck;
+import com.fr.swift.exception.inspect.SwiftRepositoryHealthInspector;
+import com.fr.swift.segment.SegmentKey;
+import com.fr.swift.service.SwiftUploadService;
+
+import java.util.Collections;
 
 /**
  * @author Marvin
@@ -17,12 +22,23 @@ import com.fr.swift.exception.inspect.executor.retry.RetryTask;
 @RegisterExceptionHandler
 public class UploadSegExceptionHandler implements ExceptionHandler {
 
+    @SwiftAutoWired
+    private SwiftUploadService uploadService;
+
+    private ComponentHealthCheck repositoryChecker = new ComponentHealthCheck(SwiftRepositoryHealthInspector.getInstance(), 30000);
+
     @Override
-    public boolean handleException(final ExceptionInfo info) {
-
-        RetryTask task = new UploadSegRetryTask(info);
-        RepositoryRetryTaskQueue.getInstance().offer(task);
-
+    public boolean handleException(ExceptionInfo info) {
+        if (repositoryChecker.isHealthy()) {
+            SegmentKey key = ((UploadExceptionContext) info.getContext()).getSegmentKey();
+            if (!((UploadExceptionContext) info.getContext()).isAllShow()) {
+                uploadService.upload(Collections.singleton(key));
+                return true;
+            } else {
+                uploadService.uploadAllShow(Collections.singleton(key));
+                return true;
+            }
+        }
         //需要上传的seg只有本节点有，通知master也无法处理。因此考虑直接过掉异常，手动去数据库里查找FAILED的task重新上传
         return true;
     }
