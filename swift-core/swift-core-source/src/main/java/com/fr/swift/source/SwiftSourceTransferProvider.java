d149cebeb3bb5515541af91699cd7836424967aa
Marvin.Zhao <marvin.zhao@fanruan.com>
2019-11-07 18:28:10 +0800

    KERNEL-1785 整理jdbc和privacy对core的依赖 KERNEL-1786 整理cloud和public对core的依赖 1、将jdbc和privacy对core原有模块的依赖移动到拆分后的新模块上 1、将cloud和public对core原有模块的依赖移动到拆分后的新模块上

--- swift-core/swift-core-source/src/main/java/com/fr/swift/source/SwiftSourceTransferProvider.java
+++
@@ -1,7 +0,0 @@
-package com.fr.swift.source;
-
-import com.fr.swift.exception.SegmentAbsentException;
-
-public interface SwiftSourceTransferProvider {
-    SwiftSourceTransfer createSourceTransfer(DataSource dataSource) throws SegmentAbsentException;
-}
