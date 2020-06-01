c9f2897e62185d4930c8fef22f3ec317918c44d8
HengJiang <Heng.Jiang@fanruan.com>
2020-06-01 10:20:16 +0800

    KERNEL-3969 查询过滤条件根据hash查询预处理

---
+++ swift-base/swift-base-common/src/main/java/com/fr/swift/source/alloter/impl/hash/function/DateAppIdType.java
@@ -0,0 +1,11 @@
+package com.fr.swift.source.alloter.impl.hash.function;
+
+/**
+ * @author Heng.J
+ * @date 2020/4/29
+ * @description
+ * @since swift 1.1
+ */
+public enum DateAppIdType {
+    APPID, YEAR_MONTH, ALL
+}
