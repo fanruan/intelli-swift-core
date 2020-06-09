9b0a51078f1278a5fb1771a7be0f9a39f12324d7
lucifer <lucifer@fanruan.com>
2020-06-09 15:37:37 +0800

    QFX-143 埋点的片段提取需求开发 1、多余import去掉

--- swift-base/swift-base-common/src/main/java/com/fr/swift/config/ConfigInputUtil.java
+++ swift-base/swift-base-common/src/main/java/com/fr/swift/config/ConfigInputUtil.java
@@ -7,7 +7,6 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.io.Reader;
 
 /**
  * @author Heng.J
