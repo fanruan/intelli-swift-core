8f87096d3ddff023e2d0e6a5edc425c6bed57871
lucifer <lucifer@fanruan.com>
2020-09-14 13:46:32 +0800

    KERNEL-5286 自定义任务 1、接口设计 2、定时任务、同步异步自定义任务执行实现

---
+++ swift-schedule/src/main/java/com/fr/swift/quartz/service/ScheduleTaskService.java
@@ -0,0 +1,46 @@
+package com.fr.swift.quartz.service;
+
+import org.quartz.SchedulerException;
+
+/**
+ * @Author: lucifer
+ * @Description:
+ * @Date: Created in 2020/9/3
+ */
+public interface ScheduleTaskService extends LifeCycle {
+
+    /**
+     * get groovy execute job
+     * TODO: 2020/9/4  consider it can be split
+     *
+     * @param fileName
+     * @return
+     */
+    Runnable getGroovyExecuteJob(String fileName) throws Exception;
+
+    /**
+     * add or update groovy jobs
+     * TODO: 2020/9/3  consider it can be split
+     *
+     * @param fileNames
+     * @throws SchedulerException
+     */
+    void addOrUpdateGroovyJobs(String... fileNames) throws Exception;
+
+    /**
+     * delete groovy jobs
+     * TODO: 2020/9/3  consider it can be split
+     *
+     * @param fileNames
+     * @throws SchedulerException
+     */
+    void deleteGroovyJobs(String... fileNames) throws Exception;
+
+    /**
+     * init groovy schedule job
+     * TODO: 2020/9/3  consider abstract this method
+     *
+     * @throws Exception
+     */
+    void initGroovyScheduleJob() throws Exception;
+}
