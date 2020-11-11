a8d9d1e59f9e31aa94c9248e502e1563c37afc74
Hoky.He <303455184@qq.com>
2020-11-11 15:57:22 +0800

    KERNEL-5917 数据按表压缩后迁移 1.修改cube之前不必要的方法

--- swift-core/swift-core-common/src/main/java/com/fr/swift/cube/CubeUtil.java
+++ swift-core/swift-core-common/src/main/java/com/fr/swift/cube/CubeUtil.java
@@ -1,30 +1,15 @@
 package com.fr.swift.cube;
 
 import com.fr.swift.segment.Segment;
-import com.fr.swift.source.SourceKey;
-
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author anchore
  * @date 2018/6/5
  */
 public class CubeUtil {
-    private static ConcurrentHashMap<String, Integer> yearMonthMap = new ConcurrentHashMap<>();
 
     public static boolean isReadable(Segment seg) {
         return seg.isReadable();
     }
 
-    public static int getCurrentDir(SourceKey tableKey) {
-        return 0;
-    }
-
-    public static void put(String segId, int yearMonth) {
-        yearMonthMap.put(segId, yearMonth);
-    }
-
-    public static int getYearMonth(String segId) {
-        return yearMonthMap.getOrDefault(segId, 0);
-    }
 }
